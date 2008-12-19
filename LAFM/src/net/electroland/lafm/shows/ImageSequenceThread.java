d2e82959dcef2553f263a04d30c7e1105075efe4
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2008-12-19 06:02:02 +0000

    first checkin.  scaffold only.

---
+++ LAFM/src/net/electroland/lafm/shows/ImageSequenceThread.java
@@ -0,0 +1,34 @@
+package net.electroland.lafm.shows;
+
+import processing.core.PGraphics;
+import net.electroland.detector.DMXLightingFixture;
+import net.electroland.lafm.core.ShowThread;
+import net.electroland.lafm.core.SoundManager;
+
+public class ImageSequenceThread extends ShowThread {
+
+	// constructor should take an array of Images.
+	
+	public ImageSequenceThread(DMXLightingFixture flower,
+			SoundManager soundManager, int lifespan, int fps, PGraphics raster) {
+		super(flower, soundManager, lifespan, fps, raster);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void complete(PGraphics raster) {
+		// TODO Auto-generated method stub
+		
+		// paint black.
+		
+	}
+
+	@Override
+	public void doWork(PGraphics raster) {
+		// TODO Auto-generated method stub
+
+		// paint the image on the raster
+		
+	}
+
+}
