257f3ef92eeecdbc8d44505b9e5df05086a408c2
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2008-12-22 00:44:44 +0000

    works.

--- LAFM/src/net/electroland/lafm/shows/ImageSequenceThread.java
+++ LAFM/src/net/electroland/lafm/shows/ImageSequenceThread.java
@@ -1,34 +1,60 @@
 package net.electroland.lafm.shows;
 
-import processing.core.PGraphics;
 import net.electroland.detector.DMXLightingFixture;
 import net.electroland.lafm.core.ShowThread;
 import net.electroland.lafm.core.SoundManager;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+import processing.core.PImage;
 
 public class ImageSequenceThread extends ShowThread {
 
-	// constructor should take an array of Images.
+	private int index = 0;
+	private PImage[] sequence;
+	private boolean resize = true;
 	
 	public ImageSequenceThread(DMXLightingFixture flower,
-			SoundManager soundManager, int lifespan, int fps, PGraphics raster, String ID) {
+			SoundManager soundManager, int lifespan, int fps, PGraphics raster, String ID, PImage[] sequence, boolean resize) {
 		super(flower, soundManager, lifespan, fps, raster, ID);
-		// TODO find the appropriate image
+		if (sequence != null){			
+			this.sequence = sequence;
+		}else{
+			System.out.println("WARNING: IMAGE SEQUENCE WAS A NULL POINTER");
+		}
+		this.resize = resize;
+	}
+
+	public ImageSequenceThread(DMXLightingFixture[] flower,
+			SoundManager soundManager, int lifespan, int fps, PGraphics raster, String ID, PImage[] sequence, boolean resize) {
+		super(flower, soundManager, lifespan, fps, raster, ID);
+		if (sequence != null){			
+			this.sequence = sequence;
+		}else{
+			System.out.println("WARNING: IMAGE SEQUENCE WAS A NULL POINTER");
+		}
+		this.resize = resize;
 	}
 
 	@Override
 	public void complete(PGraphics raster) {
-		// TODO Auto-generated method stub
-		
-		// paint black.
-		
+
+		raster.colorMode(PConstants.RGB, 255, 255, 255);
+		raster.background(0);	// paint it black.
 	}
 
 	@Override
 	public void doWork(PGraphics raster) {
-		// TODO Auto-generated method stub
-
-		// paint the image on the raster
-		
+		if (resize){
+			raster.beginDraw();
+			raster.image(sequence[index++], 0, 0, raster.width, raster.height);			
+			raster.endDraw();
+		}else{
+			raster.beginDraw();
+			raster.image(sequence[index++], 0, 0);
+			raster.endDraw();
+		}
+		if (index == sequence.length){
+			index = 0;
+		}
 	}
-
 }
