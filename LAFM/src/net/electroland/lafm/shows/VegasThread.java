a1b70a023c3ed4f594c942bc1aea8cfb369f5ba6
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2008-12-31 22:49:02 +0000

    vegas running much quicker, much more "sparkly"

--- LAFM/src/net/electroland/lafm/shows/VegasThread.java
+++ LAFM/src/net/electroland/lafm/shows/VegasThread.java
@@ -13,6 +13,7 @@ public class VegasThread extends ShowThread {
 	
 	ColorScheme spectrum;
 	float speed;
+	int age = 0;
 
 	public VegasThread(DMXLightingFixture flower, SoundManager soundManager,
 			int lifespan, int fps, PGraphics raster, String ID, int showPriority,
@@ -42,10 +43,18 @@ public class VegasThread extends ShowThread {
 		raster.colorMode(PConstants.RGB, 255, 255, 255, 100);
 		raster.rectMode(PConstants.CENTER);
 		raster.beginDraw();
-		float[] color = spectrum.getColor((float)Math.random());
-		raster.noStroke();
-		raster.fill(color[0], color[1], color[2]);
-		raster.rect((float)(Math.random()*255), (float)(Math.random()*255), 40, 40);
+		if(age > speed){
+			raster.background(0);
+			raster.noStroke();
+			for(int i=0; i<25; i++){
+				float[] color = spectrum.getColor((float)Math.random());
+				raster.fill(color[0], color[1], color[2]);
+				raster.rect((float)(Math.random()*255), (float)(Math.random()*255), 50, 50);
+			}
+			age = 0;
+		} else {
+			age++;
+		}
 		raster.endDraw();
 	}
 
