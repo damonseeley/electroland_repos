4a0dd5ad6943cc2c6aae291836e3918cd8807055
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-02-14 23:53:28 +0000

    deleting tag

--- IndyControllerDiagnostic/tags/src/net/electroland/indy/test/IPAddressParseException.java
+++
@@ -1,8 +0,0 @@
-package net.electroland.indy.test;
-
-@SuppressWarnings("serial")
-public class IPAddressParseException extends Exception{
-	public IPAddressParseException(String mssg){
-		super(mssg);
-	}
-}
