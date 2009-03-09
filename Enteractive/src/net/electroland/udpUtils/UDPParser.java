6dc2beed75c7eec54d5b5b17f5bfbe7ec61c31c7
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-03-09 01:47:30 +0000

    parsing of incoming HaleUDP packets by command byte values

--- Enteractive/src/net/electroland/udpUtils/UDPParser.java
+++ Enteractive/src/net/electroland/udpUtils/UDPParser.java
@@ -3,6 +3,8 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
+import net.electroland.enteractive.utils.HexUtils;
+
 import org.apache.log4j.Logger;
 
 
@@ -13,19 +15,49 @@ public class UDPParser extends Thread {
 	
 	UDPReceiver receiver;
 	boolean isRunning = true;
+	TCUtil tcUtils;
 
-	public UDPParser(int port) throws SocketException, UnknownHostException {
+	public UDPParser(int port, TCUtil tcUtils) throws SocketException, UnknownHostException {
 		receiver = new UDPReceiver(port);
+		this.tcUtils = tcUtils;
 	}
 
 	public void parseMsg(String msg) {
 		//logger.debug("msg: " + msg);
-		logger.info(msg);
-		//String[] line = msg.split(",");
-		// TODO check command byte against TCUtils command bytes
-		//if((byte)Integer.parseInt(line[1]) == 0x00){
-			
-		//}
+		byte[] line = HexUtils.hexToBytes(msg);
+		int offset = (int)(line[line.length-3] & 0xFF) + (int)(line[line.length-2] & 0xFF);	// determines starting position of payload values
+		//System.out.println(offset);
+		if(tcUtils != null){
+			if(line[1] == Byte.valueOf(tcUtils.tileProps.getProperty("TileUpdateCmd")).byteValue()){
+				// direct response of light values
+				lightValues(offset);
+			} else if(line[1] == Byte.valueOf(tcUtils.tileProps.getProperty("TileFeedbackCmd")).byteValue()){
+				// direct response of sensor states
+				sensorValues(offset);
+			} else if(line[1] == Byte.valueOf(tcUtils.tileProps.getProperty("TileOnOffCmd")).byteValue()){
+				// direct response from tiles being powered on/off
+				tilePowerState(offset);
+			} else if(line[1] == Byte.valueOf(tcUtils.tileProps.getProperty("TileReportCmd")).byteValue()){
+				// continuous sensor state reporting on change
+				sensorValues(offset);
+			} else if(line[1] == Byte.valueOf(tcUtils.tileProps.getProperty("TileOffsetCmd")).byteValue()){
+				// TODO direct response from setting offset value
+			} else if(line[1] == Byte.valueOf(tcUtils.tileProps.getProperty("TileMCResetCmd")).byteValue()){
+				// TODO direct response from resetting microcontroller on tile controller
+			} 
+		} 
+	}
+	
+	public void lightValues(int offset){
+		
+	}
+	
+	public void sensorValues(int offset){
+		
+	}
+	
+	public void tilePowerState(int offset){
+		
 	}
 
 	public void stopRunning() {
@@ -52,7 +84,7 @@ public class UDPParser extends Thread {
 
 	//just for testing
 	public static void main(String[] args) throws SocketException, UnknownHostException {
-		UDPParser parser = new UDPParser(10011) ;
+		UDPParser parser = new UDPParser(10011, null) ;
 		parser.start();
 	}
 
