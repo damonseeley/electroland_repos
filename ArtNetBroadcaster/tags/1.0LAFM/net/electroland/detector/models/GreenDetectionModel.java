c9a78b4ad6f69427debdbec21561ee11fca8b746
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-03-11 22:26:08 +0000

    tagged version of artnetbroadcaster from LAFM install.

---
+++ ArtNetBroadcaster/tags/1.0LAFM/net/electroland/detector/models/GreenDetectionModel.java
@@ -0,0 +1,22 @@
+package net.electroland.detector.models;
+
+import net.electroland.detector.DetectionModel;
+
+/**
+ * This class determines the average green intensity of an array of pixels, and
+ * returns that value as a byte from 0-255.  It returns the same value for
+ * getColor as getBrightness.
+ * 
+ * @author geilfuss
+ *
+ */
+public class GreenDetectionModel implements DetectionModel {
+
+	final public byte getValue(int[] pixels) {
+		float g = 0;
+		for (int i = 0; i < pixels.length; i++){
+			g += (pixels[i] >> 8 & 0xFF);
+		}
+		return (byte)(g / pixels.length);
+	}
+}
