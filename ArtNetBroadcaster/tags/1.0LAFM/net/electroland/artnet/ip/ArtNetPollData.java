cadea17858921621e0c09a63be7e5c1a4335995a
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-03-15 05:04:05 +0000

    removing tag created by DS that was causing problems.

--- ArtNetBroadcaster/tags/1.0LAFM/net/electroland/artnet/ip/ArtNetPollData.java
+++
@@ -1,7 +0,0 @@
-package net.electroland.artnet.ip;
-
-public class ArtNetPollData extends ArtNetData {
-
-	//	OpOutput. Transmitted low byte first. 
-	short OpCode = ArtNetOpCodes.OpPoll;
-}
--- ArtNetBroadcaster/tags/1.0LAFM/net/electroland/artnet/ip/ArtNetPollReplyData.java
+++
@@ -1,7 +0,0 @@
-package net.electroland.artnet.ip;
-
-public class ArtNetPollReplyData extends ArtNetData {
-
-	//	OpOutput. Transmitted low byte first. 
-	short OpCode = ArtNetOpCodes.OpPollReply;
-}
