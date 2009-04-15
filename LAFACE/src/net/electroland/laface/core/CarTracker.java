46e5fdddc456e85450f40387099cc059b06d4e0b
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-04-15 00:48:57 +0000

    bug when switching to show other than WaveShow fixed.

--- LAFACE/src/net/electroland/laface/core/CarTracker.java
+++ LAFACE/src/net/electroland/laface/core/CarTracker.java
@@ -33,34 +33,37 @@ public class CarTracker extends Thread implements TrackListener{
 				
 				TrackResults result = resultsQueue.take(); // will block until something is on the que
 				//System.out.println(result.created.size() + " tracks created");
-				if(result.created.size() > 0){
-					Iterator<Track> iter = result.created.iterator();
-					while(iter.hasNext()){
-						Track newtrack = iter.next();
-						// REMEMBER: the camera is mirroring the display
-						if(newtrack.x < Integer.parseInt(ElProps.THE_PROPS.get("srcWidth").toString())/2){
-							Impulse impulse = new Impulse(main, 0, 300, false);	// left side
-							impulse.start();
-						} else {
-							Impulse impulse = new Impulse(main, 0, 300, true);		// right side
-							impulse.start();
+				if(main.getCurrentAnimation() instanceof WaveShow){
+					if(result.created.size() > 0){
+						Iterator<Track> iter = result.created.iterator();
+						while(iter.hasNext()){
+							Track newtrack = iter.next();
+							// REMEMBER: the camera is mirroring the display
+							if(newtrack.x < Integer.parseInt(ElProps.THE_PROPS.get("srcWidth").toString())/2){
+								Impulse impulse = new Impulse(main, 0, 300, false);	// left side
+								impulse.start();
+							} else {
+								Impulse impulse = new Impulse(main, 0, 300, true);		// right side
+								impulse.start();
+							}
 						}
 					}
-				}
 				
-				if(result.existing.size() > 0){
-					// set damping based on population size
-					Wave wave = ((WaveShow)(main.getCurrentAnimation())).getWaves().get(0);	// set specifically for single wave sprite instance
-					// TODO set a min/max damping value in properties based on population sizes
-					if(result.existing.size() < 9){
-						wave.setDamping(result.existing.size()/40);
+				
+					if(result.existing.size() > 0){
+						// set damping based on population size
+						Wave wave = ((WaveShow)(main.getCurrentAnimation())).getWaves().get(0);	// set specifically for single wave sprite instance
+						// TODO set a min/max damping value in properties based on population sizes
+						if(result.existing.size() < 9){
+							wave.setDamping(result.existing.size()/40);
+						} else {
+							wave.setDamping(0.2);	// max damping cap
+						}
 					} else {
-						wave.setDamping(0.2);	// max damping cap
+						// set damping to 0
+						Wave wave = ((WaveShow)(main.getCurrentAnimation())).getWaves().get(0);	// set specifically for single wave sprite instance
+						wave.setDamping(0);	// allows the existing wave action to continue infinitely if no new traffic comes
 					}
-				} else {
-					// set damping to 0
-					Wave wave = ((WaveShow)(main.getCurrentAnimation())).getWaves().get(0);	// set specifically for single wave sprite instance
-					wave.setDamping(0);	// allows the existing wave action to continue infinitely if no new traffic comes
 				}
 				//System.out.println(result.existing.size() + " tracks");
 			} catch (InterruptedException e) {
