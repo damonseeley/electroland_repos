e5c8d3b0a7b800a9af48aab78294eb82854ed929
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-05-10 06:54:41 +0000

    updated to ignore only a max of half the pixels, so that black pixels within one or the other zone of a two pixel zone will be ignored, but the others will not.

--- ElectrolandLightingUtils/net/electroland/lighting/detector/models/BrightRedDetectionModel.java
+++ ElectrolandLightingUtils/net/electroland/lighting/detector/models/BrightRedDetectionModel.java
@@ -5,14 +5,21 @@ import net.electroland.lighting.detector.DetectionModel;
 public class BrightRedDetectionModel implements DetectionModel {
 
 	public byte getValue(int[] pixels) {
+		// we'll ignore up to half of the pixels if they are black.
+		int ignore = pixels.length / 2;
 		int points = 0;
 		float r = 0;
 		for (int i = 0; i < pixels.length; i++){
 			int intensity = (pixels[i] >> 16) & 0xFF;
-			if (intensity > 0)
+			// if we surpassed the max number of ignored pixels or this isn't
+			// a black pixel, average it in.
+			if (ignore == 0 || intensity > 0)
 			{
 				r += intensity;
 				points++;
+			}else{
+				// ignore this pixel.
+				ignore--;
 			}
 		}
 		return points == 0 ? (byte)0 : (byte)(r / points);
