a72007032ca18de41eee814322427287904b15e9
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-05-11 04:47:21 +0000

    actually, properly set it as a log4j debug option.

--- ElectrolandLightingUtils/net/electroland/lighting/detector/FlexXMLRecipient.java
+++ ElectrolandLightingUtils/net/electroland/lighting/detector/FlexXMLRecipient.java
@@ -85,7 +85,7 @@ public class FlexXMLRecipient extends Recipient {
 		sb.append("</xml>");
 
 		String message = sb.toString();
-		//System.out.println(message);
+		logger.debug(message);
 		xmlsb.send(new FlexRecipientXMLSocketMessage(message));
 	}
 }
