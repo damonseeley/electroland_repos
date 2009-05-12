cece5f7abf7fac2b41ac0a14723aea7f238c5029
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-05-12 18:48:08 +0000

    nevermind, blackout is part of ELU now

--- Enteractive/src/net/electroland/enteractive/core/EnteractiveMain.java
+++ Enteractive/src/net/electroland/enteractive/core/EnteractiveMain.java
@@ -113,7 +113,6 @@ public class EnteractiveMain extends JFrame implements AnimationListener, Action
 			}
 		});
 		
-		Runtime.getRuntime().addShutdownHook(new Thread(){public void run(){dmr.blackOutAll();}});
 
 		lights3D = new Lights3D(600,600, dmr.getRecipient("floor"),  dmr.getRecipient("face"), ptr.getModel());
 		lights3D.setMinimumSize(new Dimension(600,600));
