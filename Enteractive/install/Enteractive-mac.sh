c9aa70ecc391d73c5bb8f3a0dc39aa5feeb703ba
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-08-24 00:43:46 +0000

    works.  make sure to chmod a+x

--- Enteractive/install/Enteractive-mac.sh
+++ Enteractive/install/Enteractive-mac.sh
@@ -1,2 +1,2 @@
-# start project on Mac OS
-java -classpath Enteractive.jar:libraries/ELU.jar:libraries/core.jar net.electroland.enteractive.core.EnteractiveMain
+#! /bin/sh
+java -classpath ./depends:Enteractive.jar:libraries/ELU.jar:libraries/core.jar:libraries/javaosc_ELmod.jar:libraries/junit-4.6.jar:libraries/log4j-1.2.15.jar:libraries/miglayout15-swing.jar:libraries/SCSoundControl.jar net.electroland.enteractive.core.EnteractiveMain
