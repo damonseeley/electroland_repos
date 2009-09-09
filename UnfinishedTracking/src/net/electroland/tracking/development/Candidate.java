4e9d9a2f81f9f53421ee503da62c8d04d455e9eb
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-09-09 19:54:57 +0000

    Initial commit of UnfinishedTracking code from LAFACE. Will NOT run in its current condition. See description.txt for more details.

---
+++ UnfinishedTracking/src/net/electroland/tracking/development/Candidate.java
@@ -0,0 +1,125 @@
+package net.electroland.tracking.development;
+
+import java.util.Vector;
+
+import net.electroland.blobDetection.match.Track;
+
+/**
+ * A Candidate is associated with a Track, and is used to retain a history of the
+ * tracks location in order to assess the velocity and direction of the Track, as
+ * well as whether it's accelerating or decelerating.
+ * 
+ * If a Candidate survives the number of required samples and maintains a velocity
+ * above the minimum, a new Mover object will be created with the Candidates properties
+ * and the Candidate will be destroyed.
+ * 
+ * @author Aaron Siegel
+ */
+
+public class Candidate {
+	
+	private Track track;
+	private int id;
+	public float x, y;							// current location (normalized)
+	public float width, height;
+	private Vector<Vector<Float>> locations;	// past locations (normalized)
+	private Vector<Long> times;				// sample times (milliseconds)
+	private float minimumXSpeed = 0.05f;
+	private float endZoneMargin = 0.2f;
+
+	public Candidate(Track track){
+		this.track = track;
+		this.id = track.id;
+		locations = new Vector<Vector<Float>>();
+		times = new Vector<Long>();
+	}
+	
+	public void addLocation(float x, float y){
+		this.x = x;
+		this.y = y;
+		Vector<Float> loc = new Vector<Float>();
+		loc.add(this.x);
+		loc.add(this.y);
+		locations.add(loc);
+		times.add(System.currentTimeMillis());
+	}
+	
+	public float getX(){
+		return x = track.x;
+	}
+	
+	public float getY(){
+		return y = track.y;
+	}
+	
+	public float getWidth(){
+		return width = track.width;
+	}
+	
+	public float getHeight(){
+		return height = track.height;
+	}
+	
+	public boolean isStatic(){
+		float xdiff = locations.get(locations.size()-1).get(0) - locations.get(0).get(0);	// distance between first and last sample.
+		//float ydiff = locations.get(locations.size()-1).get(1) - locations.get(0).get(1);
+		if(Math.abs(xdiff) < minimumXSpeed){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean endZone(){
+		Vector<Float> speed = getSpeed();
+		if(speed.get(0) < 0 && x > 1-endZoneMargin){
+			return true;
+		} else if(speed.get(0) > 0 && x < endZoneMargin) {
+			return true;
+		}
+		return false;
+	}
+	
+	// this function returns [0] X unit vector, [1] Y unit vector, [2] X duration to edge, [3] Y duration to edge
+	public Vector<Float> getSpeed(){
+		// TODO evaluate all sample points for acceleration/deceleration trend and rate
+		long samplediff = System.currentTimeMillis() - times.get(0);						// time in between first and last sample.
+		float xdiff = locations.get(locations.size()-1).get(0) - locations.get(0).get(0);	// distance between first and last sample.
+		float ydiff = locations.get(locations.size()-1).get(1) - locations.get(0).get(1);
+		//System.out.println(xdiff);
+		if(xdiff == 0){
+			return null;
+		}
+		float hypodiff = (float)Math.sqrt(xdiff*xdiff + ydiff*ydiff);
+		
+		// TODO check how long it will take before the mover is outside the raster at the current speed on this vector
+		float xscale, yscale;
+		if(xdiff >= 0){
+			xscale = (1-Math.abs(locations.get(locations.size()-1).get(0)))/xdiff;		// multiple of how much longer it will take to 
+		} else {																		// complete the remaining distance at this speed.
+			xscale = Math.abs(locations.get(locations.size()-1).get(0)/xdiff);
+			//System.out.println("xduration: "+ (samplediff * xscale) +" "+xscale);
+			//System.out.println(Math.abs(locations.get(locations.size()-1).get(0)) +" "+ xdiff);
+		}
+		if(ydiff >= 0){
+			yscale = (1-Math.abs(locations.get(locations.size()-1).get(1)))/ydiff;		
+		} else {
+			yscale = Math.abs(locations.get(locations.size()-1).get(1)/ydiff);
+		}
+		
+		Vector<Float> moverVec = new Vector<Float>();
+		moverVec.add(xdiff/hypodiff);					// unit vector
+		moverVec.add(ydiff/hypodiff);	
+		moverVec.add(samplediff * xscale);				// time between first and last sample multiplied by the ratio 
+		moverVec.add(samplediff * yscale);				// of remaining distance to what was covered between sample points.
+		return moverVec;
+	}
+	
+	public Vector<Vector<Float>> getLocations(){
+		return locations;
+	}
+	
+	public int getID(){
+		return id;
+	}
+	
+}
---
+++ UnfinishedTracking/src/net/electroland/tracking/development/CarTracker.java
@@ -0,0 +1,96 @@
+package net.electroland.tracking.development;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import net.electroland.blobDetection.match.Track;
+import net.electroland.blobDetection.match.TrackListener;
+import net.electroland.blobDetection.match.TrackResults;
+import net.electroland.blobTracker.core.BlobTrackerServer;
+import net.electroland.blobTracker.util.ElProps;
+import net.electroland.laface.shows.Reflection;
+import net.electroland.laface.shows.WaveShow;
+import net.electroland.laface.sprites.Wave;
+
+public class CarTracker extends Thread implements TrackListener{
+	LinkedBlockingQueue<TrackResults> resultsQueue;
+	ConcurrentHashMap<Integer,Track> cars;
+	BlobTrackerServer bts;
+	LAFACEMain main;
+	
+	public CarTracker(LAFACEMain main){
+		this.main = main;
+		resultsQueue = new LinkedBlockingQueue<TrackResults>();		// used to get info on active blobs
+		cars = new ConcurrentHashMap<Integer,Track>();				// stores active blob data (position history, speed, etc)
+		ElProps.init("depends//blobTracker.props");
+		bts = new BlobTrackerServer(ElProps.THE_PROPS);
+		bts.addTrackListener(0, this);
+	}
+
+	public void run(){
+		while(true) {
+			try {
+				
+				TrackResults result = resultsQueue.take(); // will block until something is on the que
+				//System.out.println(result.created.size() + " tracks created");
+				if(main.getCurrentAnimation() instanceof WaveShow){
+					if(result.created.size() > 0){
+						Iterator<Track> iter = result.created.iterator();
+						while(iter.hasNext()){
+							Track newtrack = iter.next();
+							// REMEMBER: the camera is mirroring the display
+							if(newtrack.x < Integer.parseInt(ElProps.THE_PROPS.get("srcWidth").toString())/2){
+								Impulse impulse = new Impulse(main, 0, 2000, false);	// left side
+								impulse.start();
+							} else {
+								Impulse impulse = new Impulse(main, 0, 2000, true);	// right side
+								impulse.start();
+							}
+						}
+					}
+				
+				
+					if(result.existing.size() > 0){
+						// set damping based on population size
+						Wave wave = ((WaveShow)(main.getCurrentAnimation())).getWaves().get(0);	// set specifically for single wave sprite instance
+						// TODO set a min/max damping value in properties based on population sizes
+						if(result.existing.size() < 9){
+							wave.setDamping(result.existing.size()/40);
+						} else {
+							wave.setDamping(0.2);	// max damping cap
+						}
+					} else {
+						// set damping to 0
+						Wave wave = ((WaveShow)(main.getCurrentAnimation())).getWaves().get(0);	// set specifically for single wave sprite instance
+						wave.setDamping(0);	// allows the existing wave action to continue infinitely if no new traffic comes
+					}
+				} else if(main.getCurrentAnimation() instanceof Reflection){
+					// TODO do anything in here?
+					
+				}
+				//System.out.println(result.existing.size() + " tracks");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			/*
+			try {
+				sleep(33);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/
+		}
+	}
+	
+	public void addTrackListener(TrackListener listener){
+		bts.addTrackListener(0, listener);
+	}
+	
+	public void updateTracks(TrackResults results) {
+		resultsQueue.offer(results);	
+	}
+
+}
