1a97fc00e315225cb3f9c5a99aea751a1fdba819
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-09-09 19:51:37 +0000

    Removed dependencies on ElVisBlobTracker and moved them into the UnfinishedTracking project.

--- LAFACE/src/net/electroland/laface/tracking/MoverListener.java
+++
@@ -1,5 +0,0 @@
-package net.electroland.laface.tracking;
-
-public interface MoverListener {
-	abstract void moverEvent(Mover mover);
-}
--- LAFACE/src/net/electroland/laface/tracking/TargetListener.java
+++
@@ -1,5 +0,0 @@
-package net.electroland.laface.tracking;
-
-public interface TargetListener {
-	abstract void targetEvent(Target target);
-}
