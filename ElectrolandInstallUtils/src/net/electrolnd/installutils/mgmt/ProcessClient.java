e84a0ae1051a1c06b5388facf7c418744260ee17
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2009-11-21 21:31:49 +0000

    client works and is a little friendly.

--- ElectrolandInstallUtils/src/net/electrolnd/installutils/mgmt/ProcessClient.java
+++ ElectrolandInstallUtils/src/net/electrolnd/installutils/mgmt/ProcessClient.java
@@ -1,76 +1,121 @@
 package net.electrolnd.installutils.mgmt;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
 public class ProcessClient {
 
+//	final static String startCmd = "C:\\Documents and Settings\\Electroland\\Desktop\\FIL\\Projection2\\FILMain";
+//	final static String stopCmd = "taskkill /f /fi \"WINDOWTITLE eq FIL*\"";
+	
 	public static void main(String args[])
 	{
-		int port = 8181; // default port.
-		ServerSocket me = null;
-
 		try 
 		{
-			if (args.length == 1)
-				port = Integer.parseInt(args[0]);
+			// load props
+			Properties p = new Properties();
+			p.load(new FileInputStream(new File("directives.properties")));
 
-			me = new ServerSocket(port);
-			
-		} catch (IOException e) {
-		    System.out.println("Could not listen on port: " + port);
-		    System.exit(-1);
-		} catch (NumberFormatException e){
-		    System.out.println("Invalid port: " + args[0]);			
-		    System.exit(-1);
-		}
+			// command to be run on 'start' requests
+			String startCmd = p.getProperty("startDirective");
+			// command to be run on 'stop' requests (and before 'start')
+			String stopCmd = p.getProperty("stopDirective");
+			// port to listen on.
+			String portStr = p.getProperty("port", "8181");
 
-		try 
-		{
-			Process process = null;
-			Socket master = me.accept();
-
-			System.out.println("Accepting connection from " + master.getInetAddress());
+			int port = Integer.parseInt(portStr);
+			ServerSocket me = new ServerSocket(port);
+			System.out.println("Waiting for Director connection...");
 
 			while (true)
 			{
-				BufferedReader in = new BufferedReader(
-	                      new InputStreamReader(
-	                    		  master.getInputStream()));
-
-				String command = in.readLine();
-
-				if ("start".equalsIgnoreCase(command))
+				try 
 				{
-					if (process != null)
+					Socket master = me.accept();
+					System.out.println("Director connected from " + master.getInetAddress());
+
+					PrintWriter out = new PrintWriter(
+		                      new OutputStreamWriter(
+		                    		  master.getOutputStream()));					
+					out.println("Welcome Director!");
+					out.println("=================");
+					out.println(" Valid commands are:");
+					out.println("   start");
+					out.println("   stop");
+					out.println("   quit (or just 'q')");
+					out.println("");
+					out.flush();
+
+					BufferedReader in = new BufferedReader(
+		                      new InputStreamReader(
+		                    		  master.getInputStream()));
+					while (true)
 					{
-						process.destroy();
-						process.waitFor();
-						System.out.println("killed existing process.");
+						String directive = in.readLine();
+
+						if (directive.equalsIgnoreCase("start"))
+						{
+							runCmd(stopCmd,  ">> services stopped.", out);
+							runCmd(startCmd, ">> services started.", out);
+		
+						}else if (directive.equalsIgnoreCase("stop"))
+						{
+							runCmd(stopCmd, ">> services stopped.", out);
+
+						}else if (directive.toLowerCase().startsWith("q"))
+						{
+							System.out.println("Director disconnected.");
+							out.println(">> good bye!.");
+							out.flush();
+							master.close();
+							break;
+						}else {
+							System.out.println(">> unknown directive: " + directive);							
+							out.println(">> unknown directive: " + directive);
+							out.flush();
+						}
 					}
-					process = Runtime.getRuntime().exec("ls -al");
-					System.out.println("started new process");
 					
-				}else if ("stop".equalsIgnoreCase(command))
-				{
-					if (process != null)
-					{
-						process.destroy();
-						process.waitFor();
-						System.out.println("killed existing process.");
-					}					
-				}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}			
 			}
 			
 		} catch (IOException e) {
-			System.out.println("Connection failure: " + e);
-		    System.exit(-1);
-		} catch (InterruptedException e) {
-			System.out.println("InterruptedException: " + e);
 			e.printStackTrace();
+		    System.exit(-1);
+		} catch (NumberFormatException e){
+			e.printStackTrace();
+		    System.exit(-1);
+		}
+	}
+
+	private static Process runCmd(String cmd, String successMsg, PrintWriter out)
+	{
+		System.out.println("issuing command:");
+		System.out.println("\t" + cmd);
+		System.out.println("");
+
+		try{			
+			Process p = Runtime.getRuntime().exec(cmd);
+			out.println(successMsg);
+			out.flush();
+			return p;
+			
+		}catch (IOException e){
+			e.printStackTrace();
+			e.printStackTrace(out);
+			out.flush();
+			return null;
 		}
 	}
 }
