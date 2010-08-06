91577ad6db4c4025224ebd82b0813b1732991367
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-06 05:19:18 +0000

    check in scaffolding

---
+++ ElectrolandLightingUtils/src/net/electroland/lighting/tools/VisualAnimationRecipientFrame.java
@@ -0,0 +1,11 @@
+package net.electroland.lighting.tools;
+
+import javax.swing.JFrame;
+import org.apache.log4j.Logger;
+
+public class VisualAnimationRecipientFrame extends JFrame {
+
+	static Logger logger = Logger.getLogger(VisualAnimationRecipientFrame.class);
+
+	
+}
---
+++ ElectrolandLightingUtils/src/net/electroland/lighting/tools/VisualLightingManager.java
@@ -0,0 +1,54 @@
+package net.electroland.lighting.tools;
+
+public class VisualLightingManager implements Runnable{
+	
+	public VisualLightingManager()
+	{
+		
+	}
+	
+	final public void run()
+	{
+		
+	}	
+	
+	final public void start()
+	{
+		
+	}
+
+	final public void stop()
+	{
+
+	}
+
+	final public void pause()
+	{
+		
+	}
+
+	final public void reloadProps()
+	{
+		
+	}
+
+	final public void saveProps()
+	{
+		
+	}
+
+	final public void allOn()
+	{
+		
+	}
+
+	final public void allOff()
+	{
+		
+	}
+
+	final public void killAllShows()
+	{
+		
+	}
+}
