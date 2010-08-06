08faf3c156235be48e2470cefbb20c7f4161050f
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-06 05:19:48 +0000

    check in scaffolding

---
+++ ElectrolandLightingUtils/src/net/electroland/lighting/tools/views/RawPackets.java
@@ -0,0 +1,29 @@
+package net.electroland.lighting.tools.views;
+
+import javax.swing.JPanel;
+
+import net.electroland.lighting.detector.Recipient;
+import net.electroland.lighting.detector.animation.Animation;
+import net.electroland.lighting.tools.VisualAnimationRecipientViewable;
+
+public class RawPackets extends VisualAnimationRecipientViewable {
+
+	@Override
+	public JPanel getPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Recipient getRecipient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Animation getAnimation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
