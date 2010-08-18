01069ce99fb5986fb699bb824a48c0520b991ece
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-18 23:33:43 +0000

    detectors reflect the proper values.  clicks toggle detectors on and off.

--- ElectrolandLightingUtils/src/net/electroland/lighting/tools/views/DetectorStates.java
+++ ElectrolandLightingUtils/src/net/electroland/lighting/tools/views/DetectorStates.java
@@ -2,50 +2,95 @@ package net.electroland.lighting.tools.views;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.util.Iterator;
 
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
 
 import net.electroland.lighting.detector.Detector;
 import net.electroland.lighting.detector.Recipient;
 import net.electroland.lighting.tools.RecipientRepresentation;
+import net.electroland.util.Util;
 
-public class DetectorStates extends RecipientRepresentation {
+public class DetectorStates extends RecipientRepresentation implements MouseInputListener {
 
+	boolean showDetectors = true;
+	
 	public DetectorStates(Recipient r)
 	{
 		super(r);
+		this.addMouseListener(this);
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-		g.setColor(Color.BLACK);
+		super.repaint();
+		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
 		// draw raster
-		if (this.getRaster() != null)
+		if (getRaster() != null)
 		{
-			BufferedImage raster = (BufferedImage)((this.getRaster().getRaster()));
-			g.drawImage(raster, 0, 0, raster.getWidth((JPanel)this), 
-										raster.getHeight((JPanel)this), this);
+			BufferedImage image = (BufferedImage)((this.getRaster().getRaster()));
+			g.drawImage(image, 0, 0, image.getWidth((JPanel)this), 
+					image.getHeight((JPanel)this), this);			
+		}
+		if (showDetectors){
+			// draw each detector
+			Recipient fixture = getRecipient();
 			
+			Iterator<Detector> i = fixture.getDetectors().iterator();
+			while (i.hasNext()){
+				Detector d = i.next();			
+				int rgb = Util.unsignedByteToInt(fixture.getLastEvaluatedValue(d));
+				g.setColor(new Color(rgb,rgb,rgb));
+				g.fillRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());
+				g.setColor(Color.GRAY);
+				g.drawRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());
+			}			
 		}
-		// draw each detector
-		Recipient fixture = getRecipient();
-		
-		Iterator<Detector> i = fixture.getDetectors().iterator();
-		while (i.hasNext()){
-			Detector d = i.next();
-			int rgb = (int)(fixture.getLastEvaluatedValue(d));
-			g.setColor(new Color(rgb,rgb,rgb));
-			g.fillRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());
-			g.setColor(Color.GRAY);
-			g.drawRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());
-		}
+	}
 
-		// draw the border of the fixture
-		g.setColor(Color.DARK_GRAY);
-		g.drawRect(0, 0, fixture.getPreferredDimensions().width, fixture.getPreferredDimensions().height);					
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		showDetectors = !showDetectors;
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
