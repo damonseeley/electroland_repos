7b218394dd1a8f0c0ad691a4b0680810047551a9
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-18 23:39:03 +0000

    using simpleVLM

--- MemphisJava/src/net/electroland/memphis/core/MemphisCore.java
+++ MemphisJava/src/net/electroland/memphis/core/MemphisCore.java
@@ -6,24 +6,20 @@ import net.electroland.memphis.behavior.TestBehavior;
 public class MemphisCore extends Conductor {
 
 	final int MAX_PACKET = 2048;
-	final int LISTEN_PORT = 8080;
+	final int LISTEN_PORT = 7474;
 	
 	public MemphisCore()
 	{
 		// enable HaldUDP and animation
-		// listen port and max packet should probably come in through
-		// either lights.properties or the args in main.
-		this.initHaleUDPSensor(LISTEN_PORT, MAX_PACKET);
+		// listen port and max packet should come from a props file or args
 		this.initAnimation("depends\\lights.properties");
+		this.initHaleUDPSensor(LISTEN_PORT, MAX_PACKET);
 		
 		// add a behavior (for now, just one)
 		this.addBehavior(new TestBehavior());
-		
-		// use the VLM
-		this.showVLM();
 
-		// start everything
-		this.startSystem();
+		// use the VLM
+		this.showSimpleVLM();
 	}
 	
 	public static void main(String args[])
