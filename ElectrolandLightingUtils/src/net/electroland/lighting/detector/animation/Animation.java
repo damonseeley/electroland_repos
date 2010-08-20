9984d0467f74672dd1ce4bb8397cee96669b608f
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-20 07:19:29 +0000

    added init(Properties)

--- ElectrolandLightingUtils/src/net/electroland/lighting/detector/animation/Animation.java
+++ ElectrolandLightingUtils/src/net/electroland/lighting/detector/animation/Animation.java
@@ -1,7 +1,29 @@
 package net.electroland.lighting.detector.animation;
 
+import java.util.Properties;
+
 public interface Animation
 {
+	/**
+	 * Called by the AnimationManager to pass in initialization
+	 * properties when the animation is called.  This will generally contain
+	 * the contains of animation.properties.
+	 * 
+	 * @param props
+	 */
+	abstract public void init(Properties props);
+	
+	/**
+	 * Return 'true' to notify the AnimationManager that your animation has
+	 * completed it's lifecycle.
+	 * @return
+	 */
 	abstract public boolean isDone();
+	
+	/**
+	 * Returns the Raster to be returned for the latest frame of Animation
+	 * to the AnimationManager.
+	 * @return
+	 */
 	abstract public Raster getFrame();
 }
