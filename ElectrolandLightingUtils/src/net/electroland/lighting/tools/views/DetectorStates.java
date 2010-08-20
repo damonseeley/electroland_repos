0e9702fb3c89d2de4df2bfb0745538dae062b07f
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-20 07:17:42 +0000

    paintComponent -> paint so that frames only refresh as often as the frame rate.

--- ElectrolandLightingUtils/src/net/electroland/lighting/tools/views/DetectorStates.java
+++ ElectrolandLightingUtils/src/net/electroland/lighting/tools/views/DetectorStates.java
@@ -14,10 +14,11 @@ import net.electroland.lighting.detector.Recipient;
 import net.electroland.lighting.tools.RecipientRepresentation;
 import net.electroland.util.Util;
 
+@SuppressWarnings("serial")
 public class DetectorStates extends RecipientRepresentation implements MouseInputListener {
 
 	boolean showDetectors = true;
-	
+
 	public DetectorStates(Recipient r)
 	{
 		super(r);
@@ -25,57 +26,39 @@ public class DetectorStates extends RecipientRepresentation implements MouseInpu
 	}
 	
 	@Override
-	protected void paintComponent(Graphics g) {
-		super.repaint();
+	public void paint(Graphics g) {
+
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
-		// draw raster
-		if (getRaster() != null)
-		{
-			BufferedImage image = (BufferedImage)((this.getRaster().getRaster()));
-			g.drawImage(image, 0, 0, image.getWidth((JPanel)this), 
-					image.getHeight((JPanel)this), this);			
-		}
+
+			if (getRaster() != null){
+				// seriously: rename getRaster() in the parent.
+				BufferedImage image = (BufferedImage)getRaster().getRaster();
+				g.drawImage(image, 0, 0, 
+						image.getWidth((JPanel)this), 
+						image.getHeight((JPanel)this), this);				
+			}
+			
 		if (showDetectors){
 			// draw each detector
 			Recipient fixture = getRecipient();
 			
 			Iterator<Detector> i = fixture.getDetectors().iterator();
 			while (i.hasNext()){
-				Detector d = i.next();			
-				int rgb = Util.unsignedByteToInt(fixture.getLastEvaluatedValue(d));
-				g.setColor(new Color(rgb,rgb,rgb));
-				g.fillRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());
-				g.setColor(Color.GRAY);
-				g.drawRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());
-			}			
+				Detector d = i.next();
+				Byte b = fixture.getLastEvaluatedValue(d);
+				if (b != null){
+					int rgb = Util.unsignedByteToInt(fixture.getLastEvaluatedValue(d));
+					g.setColor(new Color(rgb,rgb,rgb));
+					g.fillRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());					
+					g.setColor(Color.GRAY);
+					g.drawRect(d.getX(), d.getY(), d.getWidth(), d.getHeight());					
+				}
+			}
 		}
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
@@ -83,14 +66,20 @@ public class DetectorStates extends RecipientRepresentation implements MouseInpu
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseClicked(MouseEvent arg0) {}
 
 	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseEntered(MouseEvent arg0) {}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {}
 }
