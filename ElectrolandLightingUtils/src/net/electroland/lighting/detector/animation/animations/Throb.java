a97379e5b07e082d430a23a29851c763d5e78b15
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-20 07:20:01 +0000

    works with Java2d proeprly

--- ElectrolandLightingUtils/src/net/electroland/lighting/detector/animation/animations/Throb.java
+++ ElectrolandLightingUtils/src/net/electroland/lighting/detector/animation/animations/Throb.java
@@ -1,27 +1,35 @@
 package net.electroland.lighting.detector.animation.animations;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.Properties;
 
 import net.electroland.lighting.detector.animation.Animation;
 import net.electroland.lighting.detector.animation.Raster;
 
 public class Throb implements Animation {
 
-	private int i, d;
+	private int i, d, c;
 	private boolean isDone = false;
 	private Raster raster;
+	private Properties props;
 
 	public Throb(BufferedImage image)
 	{
 		raster = new Raster(image);
+		c = 2;
 		i = 0;
 		d = 20;
 	}
 
+	public void init(Properties props){
+		this.props = props;
+	}
+
 	public boolean isDone() 
 	{
-		return isDone;
+		return isDone || c < 0;
 	}
 
 	public void stop()
@@ -40,13 +48,14 @@ public class Throb implements Animation {
 			d = -20;
 		}else if (i < 0)
 		{
+			c--;
 			i = 0;
 			d = 20;
 		}
 
-		b.getGraphics().setColor(new Color(i,i,i));
-		b.getGraphics().fillRect(0, 0 , b.getWidth(), b.getHeight());
-		
+		Graphics2D g = b.createGraphics();
+		g.setColor(new Color(i,i,i));
+		g.fillRect(0, 0 , b.getWidth(), b.getHeight());
 		return raster;
 	}
 }
