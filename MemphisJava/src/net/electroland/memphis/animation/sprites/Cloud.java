d6ab356e7b63f9d55710909a7e920f7d11f398bc
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-25 21:36:28 +0000

    changed haleudp listen port to 1001

--- MemphisJava/src/net/electroland/memphis/animation/sprites/Cloud.java
+++ MemphisJava/src/net/electroland/memphis/animation/sprites/Cloud.java
@@ -4,6 +4,10 @@ import processing.core.PImage;
 import net.electroland.lighting.detector.animation.Raster;
 
 public class Cloud extends Sprite {
+	
+	private PImage image;
+	private int duration;
+	private long startTime;
 
 	public Cloud(int id, Raster raster, float x, float y, PImage image, float duration) {
 		super(id, raster, x, y);
