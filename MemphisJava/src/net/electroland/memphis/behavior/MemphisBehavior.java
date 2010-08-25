102b41b1718f876fa34b3802ff70cc65517295dc
asiegel <asiegel@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-25 17:31:38 +0000

    Renamed Shooters animation to MemphisAnimation, since we plan on using only one. Added Ticker sprite for horizontal action when sensor is tripped. Added Wave sprite for big long sweep of lights when either far end sensor is tripped.

--- MemphisJava/src/net/electroland/memphis/behavior/MemphisBehavior.java
+++ MemphisJava/src/net/electroland/memphis/behavior/MemphisBehavior.java
@@ -11,9 +11,8 @@ import net.electroland.lighting.detector.DetectorManager;
 import net.electroland.lighting.detector.Recipient;
 import net.electroland.lighting.detector.animation.Animation;
 import net.electroland.lighting.detector.animation.AnimationManager;
-import net.electroland.memphis.animation.Shooters;
+import net.electroland.memphis.animation.MemphisAnimation;
 import net.electroland.memphis.animation.Throb;
-import net.electroland.memphis.animation.Wave;
 import net.electroland.memphis.core.BridgeState;
 
 public class MemphisBehavior extends MemphisProcessingBehavior {
@@ -49,7 +48,7 @@ public class MemphisBehavior extends MemphisProcessingBehavior {
 				//PGraphics pg = p5.createGraphics(width, height, PConstants.P3D);
 				//am.startAnimation(new Wave(p5, "depends/wave.properties"), bridge);
 				// BRADLEY: Modifed to pass the bridge state in to the Animation 8/25
-				am.startAnimation(new Shooters(p5, "depends/shooters.properties", bs), bridge);
+				am.startAnimation(new MemphisAnimation(p5, "depends/memphisanimation.properties", bs), bridge);
 				//am.startAnimation(new Throb(new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)), bridge);
 			}
 		}
