257f0ca39c6d44b25a302128c3428583821b094e
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-08-27 03:20:26 +0000

    updated ByteListener to perform logging

--- MemphisJava/src/net/electroland/memphis/utils/bytelistener/ByteListenerMain.java
+++ MemphisJava/src/net/electroland/memphis/utils/bytelistener/ByteListenerMain.java
@@ -1,14 +1,19 @@
 package net.electroland.memphis.utils.bytelistener;
 
+import java.io.IOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ByteListenerMain {
 	
 	UDPParser parser;
+	UDPLogger logger;
 	
 	public ByteListenerMain(int port){
 		try {
+			System.out.println("Attempting bind on port " + port);
 			parser = new UDPParser(port);
 			parser.run();
 		} catch (SocketException e) {
@@ -18,12 +23,37 @@ public class ByteListenerMain {
 		}
 	}
 	
-	public static void main(String[] args) {
-		int port = 1001;
-		if(args.length > 0){
-			port = Integer.parseInt(args[0]);
+	public ByteListenerMain(int port, String logFileName){
+		try {
+			System.out.println("Started up with filename " + logFileName);
+			logger = new UDPLogger(logFileName, port);
+			logger.run();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		ByteListenerMain main = new ByteListenerMain(port);
+	}
+	
+	public static void main(String[] args) {
+		//System.out.println(args[0] + " : " + args[1]);
+		int port;
+		String fileName;
+		if(args.length == 1){
+			port = Integer.parseInt(args[0]);
+			ByteListenerMain main = new ByteListenerMain(port);
+		} else if(args.length > 1) {
+			Date dateNow = new Date ();
+	        SimpleDateFormat dateformatYYYYMMDD = new SimpleDateFormat("E_yyyy_MM_dd-HHmmss_a"); 
+	        StringBuilder nowYYYYMMDD = new StringBuilder( dateformatYYYYMMDD.format( dateNow ) );
+
+			port = Integer.parseInt(args[0]);
+			fileName = args[1];
+			ByteListenerMain main = new ByteListenerMain(port, fileName+"_"+nowYYYYMMDD+".txt");
+		}
+		
 		
 	}
 
