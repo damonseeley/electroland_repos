20bcf87540e0fe3a90d020e8cbb7e6d29a5e81ca
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2010-09-14 02:26:18 +0000

    reference ELU.jar in dist

--- MemphisJava/install/run.bat
+++ MemphisJava/install/run.bat
@@ -1,1 +1,1 @@
-java -classpath ELU.jar;depends/;libraries/core.jar;libraries/ELU.jar;libraries/log4j-1.2.15.jar;libraries/miglayout-3.7.3.1-swing.jar net.electroland.memphis.core.MemphisCore
+java -classpath dist/ELU.jar;depends/;libraries/core.jar;libraries/ELU.jar;libraries/log4j-1.2.15.jar;libraries/miglayout-3.7.3.1-swing.jar net.electroland.memphis.core.MemphisCore
