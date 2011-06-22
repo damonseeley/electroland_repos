31b1e167e9dc88f51d489abb57822be941b6db0c
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-22 23:25:48 +0000

    scaffolding done.

---
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/canvas/ELUCanvas2D.java
@@ -0,0 +1,14 @@
+package net.electroland.utils.lighting.canvas;
+
+import net.electroland.utils.lighting.ELUCanvas;
+import net.electroland.utils.lighting.CanvasDetector;
+import net.electroland.utils.lighting.InvalidPixelGrabException;
+
+public class ELUCanvas2D extends ELUCanvas {
+
+	@Override
+	public CanvasDetector[] sync(int[] pixels) throws InvalidPixelGrabException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
