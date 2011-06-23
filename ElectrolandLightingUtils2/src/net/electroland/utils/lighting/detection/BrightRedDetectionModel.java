a3f74203b958ecac4eb08d3599a8928b9126ce23
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-23 00:56:54 +0000

    import from old library

---
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/BrightRedDetectionModel.java
@@ -0,0 +1,27 @@
+package net.electroland.utils.lighting.detection;
+
+import net.electroland.utils.lighting.DetectionModel;
+
+public class BrightRedDetectionModel implements DetectionModel {
+
+	public byte getValue(int[] pixels) {
+		// we'll ignore up to half of the pixels if they are black.
+		int ignore = pixels.length / 2;
+		int points = 0;
+		float r = 0;
+		for (int i = 0; i < pixels.length; i++){
+			int intensity = (pixels[i] >> 16) & 0xFF;
+			// if we surpassed the max number of ignored pixels or this isn't
+			// a black pixel, average it in.
+			if (ignore == 0 || intensity > 0)
+			{
+				r += intensity;
+				points++;
+			}else{
+				// ignore this pixel.
+				ignore--;
+			}
+		}
+		return points == 0 ? (byte)0 : (byte)(r / points);
+	}
+}
---
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/RedDetectionModel.java
@@ -0,0 +1,22 @@
+package net.electroland.utils.lighting.detection;
+
+import net.electroland.utils.lighting.DetectionModel;
+
+/**
+ * This class determines the average red intensity of an array of pixels, and
+ * returns that value as a byte from 0-255.  It returns the same value for
+ * getColor as getBrightness.
+ * 
+ * @author geilfuss
+ *
+ */
+public class RedDetectionModel implements DetectionModel {
+
+	final public byte getValue(int[] pixels) {
+		float r = 0;
+		for (int i = 0; i < pixels.length; i++){
+			r += (pixels[i] >> 16) & 0xFF;
+		}
+		return (byte)(r / pixels.length);
+	}
+}
