ba06b6331914ae9f9e4072b9f661bedcad996b84
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-23 23:32:33 +0000

    props file question

--- SkateAD/src/net/electroland/skate/core/SkateMain.java
+++ SkateAD/src/net/electroland/skate/core/SkateMain.java
@@ -4,11 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
 
 import net.electroland.skate.ui.GUIFrame;
 import net.electroland.skate.ui.GUIPanel;
-import net.electroland.utils.lighting.*;
-import net.electroland.utils.lighting.canvas.*;
+import net.electroland.utils.Util;
+import net.electroland.utils.lighting.ELUManager;
+import net.electroland.utils.lighting.canvas.ELUCanvas2D;
 
 import org.apache.log4j.Logger;
 
@@ -24,27 +29,27 @@ public class SkateMain extends Thread {
 
 	private ELUManager elu;
 	private ELUCanvas2D c;
-	
+
 	public static boolean SHOWUI;
 	public static GUIFrame guiFrame;
 	public static GUIPanel guiPanel;
 	int GUIWidth, GUIHeight;
-	
+
 	int canvasHeight, canvasWidth; 
 	int viewHeight, viewWidth;
 	float viewScale;
-	
-	
+
+
 	public static boolean isRunning;
 	private static float framerate;
 	private static Timer timer;
-	
+
 	public static long curTime = System.currentTimeMillis(); //  time of frame start to aviod lots of calls to System.getcurentTime()
 	public static long elapsedTime = -1; //  time between start of cur frame and last frame to avoid re calculating passage of time allover the place
 
 
 	public SkateMain() {
-		
+
 		// create lighting utils
 		//elu = new ELUManager("lights.properties");
 		//c = elu.getCanvas();
@@ -56,19 +61,29 @@ public class SkateMain extends Thread {
 		guiPanel = new GUIPanel(GUIWidth,GUIHeight);
 		//add the panel to the top of the window
 		guiFrame.add(guiPanel);
-		
+
 		viewHeight = GUIHeight - (int)(GUIHeight*0.1);
 		viewWidth = GUIWidth - (int)(GUIWidth*0.1);
 		viewScale = 1.0f;
-		
+
 		//init space and lights
 		//init skaters
 		//init sound controller and speakers
-		
+
 		// start everything (e.g., start the threads for each of these subsystems)
 		//elu.start();
-		
-		// thread stuff
+
+		Skater sx = new Skater("depends\\180f_sample.xaf");
+		try {
+			loadSkaterProps();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+		/////////////// THREAD STUFF
 		framerate = 30;
 		isRunning = true;
 		timer = new Timer(framerate);
@@ -86,65 +101,126 @@ public class SkateMain extends Thread {
 
 
 			//figure out whether to add or subtract skaters
-			
+
 			//update sound locations
-			
+
 			//draw skater sprites on an image at native size
 			//flop sand scale skater image to canvas-size
 			//extract a pixel array from the canvas-sized sprite image and sync with ELU
 			//draw detectors and skater info on the local canvas image post sync
-			
-			
-			
-			
-			
+
+
+
+
+
 			//test
 			//BufferedImage i = new BufferedImage(400,400,ColorSpace.TYPE_RGB);
 			Graphics g = guiPanel.getGraphics();
 			//g.setColor(new Color(255,0,0));
 			//g.fillRect((int)(20),(int)(20),20,20);
 			//logger.info("Drew something?");
-			
+
 			BufferedImage i = new BufferedImage(400,400,ColorSpace.TYPE_RGB);
 			Graphics gi = i.getGraphics();
 			gi.setColor(new Color(255,150,255));
 			gi.fillRect(0,0,i.getWidth(),i.getHeight());
 			gi.setColor(new Color(255,0,0));
 			gi.fillRect((int)(60),(int)(60),20,20);
-			
+
 			g.drawImage(i, 0, 0, null);
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
+
+
+
+
+
+
+
+
 			//logger.info(timer.sleepTime);
 			timer.block();
 		}
 
+	}
 
+	public void loadSkaterProps() throws IOException
+	{
+		// find lights.properties
+		Properties props = new Properties();
+		InputStream is = new Util().getClass().getClassLoader().getResourceAsStream("depends\\Skaters.properties");
+		if (is != null)
+		{
+			props.load(is);
+		} else {
+			throw new IOException("Skaters file not found!");
+		}
+
+
+		// parse recipients
+		Enumeration <Object> g = props.keys();
+
+		while (g.hasMoreElements())
+		{
+			String key = ("" + g.nextElement()).trim();
+			System.out.println(key);
+
+			/*
+			if (key.toLowerCase().startsWith("recipient."))
+			{
+				// validate that it has an ID
+				int idStart = key.indexOf('.');
+				if (idStart == -1 || idStart == key.length() - 1)
+				{
+					throw new OptionException("no id specified in property " + key);
+				}else{
+					// get the ID
+					String id = key.substring(idStart + 1, key.length());
+
+					// get the props
+					Map<String,String> m = OptionParser.parse("" + props.get(key));
+
+					// load the protocol-appropriate Recipient Class.
+					try {
+						Recipient r = (Recipient)(new Util().getClass().getClassLoader().loadClass("" + m.get("-class")).newInstance());
+
+						// name, configure, store
+						r.setName(id);
+						r.configure(m);
+						recipients.put(id, r);
+
+					// TODO: friendler error handling here.
+					} catch (InstantiationException e) {
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			 */
+		}
 
 	}
 
-	
+
+
+
+
 	public static void killTheads() {
 		stopRunning();	
 	}
-	
+
 	public static void stopRunning() { // it is good to have a way to stop a thread explicitly (besides System.exit(0) ) EGM
 		isRunning = false;
 		timer.stopRunning();
 	}
-	
+
 	public static void restart() {
 		isRunning = true;
 		timer.start();
 	}
-	
+
 	public static void shutdown() {
 		try { // surround w/try catch block to make sure System.exit(0) gets call no matter what
 			SkateMain.killTheads();
---
+++ SkateAD/src/net/electroland/skate/core/Skater.java
@@ -0,0 +1,151 @@
+package net.electroland.skate.core;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class Skater {
+	
+	public String fileName;
+	public int startTick;
+	public int endTick;
+	public int frameRate;
+	public int ticksPerFrame;
+	public double lengthSeconds;
+	public int lengthFrames;
+	
+	public double cmConversion;
+	
+	public HashMap[] frameData;
+
+	
+	public Skater(String XMLfile) {
+		
+		cmConversion = 2.54; //incoming files should have inch units, by default do
+
+		try {
+
+			File fXmlFile = new File(XMLfile);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+
+			
+			// SETUP SCENE INFO
+			//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			System.out.println("---------- SCENE INFO ----------");
+			
+			// Parse out SceneInfo attribs into object vars
+			NodeList SIlist = doc.getElementsByTagName("SceneInfo");
+			//System.out.println(SIlist.getLength());
+			//System.out.println(SIlist.item(0).getNodeName());
+			
+			Element SIel = (Element)SIlist.item(0);
+			fileName = SIel.getAttribute("fileName");
+			startTick = Integer.parseInt(SIel.getAttribute("startTick"));
+			endTick = Integer.parseInt(SIel.getAttribute("endTick"));
+			frameRate = Integer.parseInt(SIel.getAttribute("frameRate"));
+			ticksPerFrame = Integer.parseInt(SIel.getAttribute("ticksPerFrame"));
+
+			//use number of samples(frames) to determine length in seconds
+			NodeList nl = doc.getElementsByTagName("S");		
+			lengthFrames = nl.getLength();
+			lengthSeconds = (double)nl.getLength()/(double)frameRate;
+			
+			System.out.println("origin fileName: " + fileName);
+			System.out.println("startTick: " + startTick);
+			System.out.println("endTick: " + endTick);
+			System.out.println("frameRate: " + frameRate);
+			System.out.println("ticksPerFrame: " + ticksPerFrame);
+			System.out.println("length (in frames): " + lengthFrames);
+			System.out.println("length (in seconds): " + lengthSeconds);
+
+			System.out.println("-------- END SCENE INFO --------");
+			
+			
+
+			System.out.println("---------- ANIM DATA ----------");
+			
+
+			//create an array of hashtables to store frame data
+			frameData  = new HashMap[nl.getLength()];
+			
+			//get the sample data from XML - note we already created the NodeList above for "S"
+			if(nl != null && nl.getLength() > 0) {
+				for(int i = 0 ; i < nl.getLength();i++) {
+					
+					//create a new frame hashtable
+					HashMap<String,Double> ht = new HashMap<String,Double>();
+
+					//get the sample element
+					Element el = (Element)nl.item(i);
+
+					//store t as the first hash pair
+					ht.put("ticks", Double.parseDouble(el.getAttribute("t")));
+					
+					//get the v string and do stringops on it
+					String v = el.getAttribute("v");
+					String[] vs = v.split(" ");
+			
+					//put the v values into our hashmap
+					ht.put("z", Double.parseDouble(vs[11]));
+					ht.put("y", Double.parseDouble(vs[10]));
+					ht.put("x", Double.parseDouble(vs[9]));
+					
+					frameData[i] = ht;
+					//System.out.println(i);
+				}
+			}
+			
+			System.out.println(frameData[96]);
+			
+			System.out.println("-------- END ANIM DATA --------");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(getMetricPosAtTime(3.2)[0] + " " + getMetricPosAtTime(3.2)[1] + " " + getMetricPosAtTime(3.2)[2] + " ");
+		
+	}
+	
+	/* Return the pos value in centimeters for given frame */
+	public double[] getMetricPosAtFrame(int index){
+		double[] pos = new double[3];
+		pos[0] = (Double)frameData[index].get("x") * cmConversion;
+		pos[1] = (Double)frameData[index].get("y") * cmConversion;
+		pos[2] = (Double)frameData[index].get("z") * cmConversion;
+		return pos;
+	}
+	
+	
+	/* Return the pos value in centimeters for given time */
+	public double[] getMetricPosAtTime(double time){
+		double[] pos = new double[3];
+		// quantize to individual frames by using int for frame value
+		int frame = (int)((time/lengthSeconds) * lengthFrames);
+		pos[0] = (Double)frameData[frame].get("x") * cmConversion;
+		pos[1] = (Double)frameData[frame].get("y") * cmConversion;
+		pos[2] = (Double)frameData[frame].get("z") * cmConversion;
+		return pos;
+	}
+	
+
+	public static void main(String argv[]) {
+		@SuppressWarnings("unused")
+		Skater sx = new Skater("depends\\180f_pos.xaf");
+	}
+	
+
+}
+
+
+
