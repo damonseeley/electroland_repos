93bf6d58738045fc1a280f18a8a0b2b1e7cb3bf7
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-23 00:57:13 +0000

    stub implementation.

---
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/protocols/ARTNetRecipient.java
@@ -0,0 +1,36 @@
+package net.electroland.utils.lighting.protocols;
+
+import java.util.Hashtable;
+
+import net.electroland.utils.OptionException;
+import net.electroland.utils.lighting.Recipient;
+
+
+public class ARTNetRecipient extends Recipient {
+
+	@Override
+	public void configure(Hashtable<String, String> properties)
+			throws OptionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void allOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void allOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void send(byte[] b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
