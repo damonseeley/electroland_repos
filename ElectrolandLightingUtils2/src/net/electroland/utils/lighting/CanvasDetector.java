b96c5daf8090e82bbff3f75866dacd5c61730021
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-23 00:57:46 +0000

    full stub.

--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/CanvasDetector.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/CanvasDetector.java
@@ -1,13 +1,33 @@
 package net.electroland.utils.lighting;
 
+import java.awt.Shape;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class CanvasDetector {
 
-	/*
-		  array of Canvas image array index locations that should be part of the evaluation process
-		  detector model
-		  geometry
-		  latest evaluated state
-		  tags
-	 */
+	protected Shape boundary;
+	protected DetectionModel detectorModel;
+	protected byte latestState;
+	protected Vector tags;
+	protected CopyOnWriteArrayList<Integer>indices;
 	
+	public Shape getBoundary() {
+		return boundary;
+	}
+	public DetectionModel getDetectorModel() {
+		return detectorModel;
+	}
+	public byte getLatestState() {
+		return latestState;
+	}
+	public void setEvaluatedValue(byte b){
+		latestState = b;
+	}
+	public Vector getTags() {
+		return tags;
+	}
+	public CopyOnWriteArrayList<Integer> getIndices() {
+		return indices;
+	}
 }
