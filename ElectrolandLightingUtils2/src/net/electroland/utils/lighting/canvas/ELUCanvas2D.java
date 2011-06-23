8ea0622ced9a00bcc1d71541ec7e01901ba6812c
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-23 01:02:02 +0000

    added configure() and concrete implementation of dimensions to 2d

--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/canvas/ELUCanvas2D.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/canvas/ELUCanvas2D.java
@@ -1,14 +1,31 @@
 package net.electroland.utils.lighting.canvas;
 
-import net.electroland.utils.lighting.ELUCanvas;
+import java.awt.Dimension;
+import java.util.Hashtable;
+
+import net.electroland.utils.OptionException;
 import net.electroland.utils.lighting.CanvasDetector;
+import net.electroland.utils.lighting.ELUCanvas;
 import net.electroland.utils.lighting.InvalidPixelGrabException;
 
 public class ELUCanvas2D extends ELUCanvas {
+
+	protected Dimension d;
+	
+	@Override
+	public void Configure(Hashtable<String, String> properties)
+			throws OptionException {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
 	public CanvasDetector[] sync(int[] pixels) throws InvalidPixelGrabException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Dimension getDimensions() {
+		return d;
 	}
 }
