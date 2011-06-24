c0bacafcd8529b098f0b0bbe59a78b6212f002ce
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-24 00:39:08 +0000

    added parsing of objectType.objectName to params

--- ElectrolandLightingUtils2/src/net/electroland/utils/OptionParser.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/OptionParser.java
@@ -1,12 +1,181 @@
 package net.electroland.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 public class OptionParser {
 
 	private static String ARG_MARKER = " -";
 
+	
+	public static void main(String args[]){
+		Properties p = new Properties();
+		p.put("cat.1", "-class goober -width 700");
+		p.put("cat.name with pace", "-height 600");
+		p.put("dog.mydog", "-weight 600");
+		p.put("dog.mydog.2", "-weight 400 -foo phi");
+		p.put("dog.fixture[0]", "-weight 400");
+
+		try {
+			OptionParser op = new OptionParser(p);
+		} catch (OptionException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//  objectType -> named_object -> parameters (key value pairs)
+	private Map<String,Map<String,Map<String,String>>> objects;
+
+	/**
+	 * Take properties file provided by client
+	 * @param p
+	 */
+	public OptionParser(Properties p) throws OptionException
+	{
+		this.objects = init(p);
+	}
+
+	/**
+	 * load resourceName from classpath
+	 * @param resourceName
+	 */
+	public OptionParser(String resourceName) throws OptionException
+	{
+		Properties p = new Properties();
+
+		InputStream is = new Util().getClass().getClassLoader().getResourceAsStream(resourceName);
+		if (is != null)
+		{
+			try {
+				p.load(is);
+			} catch (IOException e) {
+				throw new OptionException("Please make sure " + resourceName + " is in your classpath.");
+			}
+		}else{
+			throw new OptionException("Please make sure " + resourceName + " is in your classpath.");
+		}
+		this.objects = init(p);
+	}
+
+	public static Map<String,Map<String,Map<String,String>>> init(Properties p) throws OptionException
+	{
+		// java generics syntax is bullshit and a half!
+		Hashtable<String,Map<String,Map<String,String>>> objects 
+				= new Hashtable<String,Map<String,Map<String,String>>>();
+		
+		// get every named properties from the props file
+		Enumeration <Object> g = p.keys();
+		while (g.hasMoreElements())
+		{
+			// parse out the namespace from "objectType.name"
+			String key = ("" + g.nextElement()).trim();
+			
+			// make sure there is only one '.'?
+			int endOfName = key.indexOf('.');
+			if (endOfName == -1 || endOfName == key.length() - 1){
+				throw new OptionException("object '" + key + "' requires a name.");
+			}
+
+			String objectType = key.substring(0,endOfName);
+			System.out.print("objectType: '" + objectType);
+			String objectName = key.substring(endOfName + 1, key.length());
+			System.out.print("', objectName: '" + objectName);
+			Map<String,String> params = parse("" + p.get(key));
+			System.out.println("', params: " + params);
+			
+			// see if the objectType exists or not:
+			Map<String,Map<String,String>> names = objects.get(objectType);
+			if (names == null)
+			{
+				names = new Hashtable<String,Map<String,String>>();
+				objects.put(objectType, names);
+			}
+
+			names.put(objectName, params);
+		}
+
+		return objects;
+	}
+	
+	/**
+	 * Ask for a type of object, and get the names of all instances of that
+	 * object.
+	 * 
+	 * @param objectType
+	 * @return
+	 * @throws OptionException
+	 */
+	public Set<String> getObjectnames(String objectType) throws OptionException
+	{
+		Map<String,Map<String,String>> type = objects.get(objectType);
+		if (type == null){
+			throw new OptionException("no object of type '" + objectType + "' was found.");
+		}else{
+			return type.keySet();
+		}
+	}
+	
+	/**
+	 * Ask for an object by name of a particular type, and receive all parameters
+	 * defined for that object.
+	 * 
+	 * @param objectType
+	 * @param objectName
+	 * @return
+	 * @throws OptionException
+	 */
+	public Map<String,String> getParams(String objectType, String objectName) throws OptionException
+	{
+		Map<String,Map<String,String>> type = objects.get(objectType);
+		if (type == null){
+			throw new OptionException("no object of type '" + objectType + "' was found.");
+		}else{
+			Map<String,String> params = type.get(objectName);
+			if (params == null)
+			{
+				throw new OptionException("no object named '" + objectName + "' of type '" + objectType + "' was found.");
+			}else
+			{
+				return params;
+			}
+		}		
+	}
+
+	/**
+	 * Ask for a parameter belonging to an object of a particular type and
+	 * get it's current value.
+	 * 
+	 * @param objectType
+	 * @param objectName
+	 * @param paramName
+	 * @return
+	 * @throws OptionException
+	 */
+	public String getParam(String objectType, String objectName, String paramName) throws OptionException
+	{
+		Map<String,String> params = getParams(objectType, objectName);
+		if (params == null)
+		{
+			throw new OptionException("no parameter '" + paramName + "' in object named '" + objectName + "' of type '" + objectType + "' was found.");
+		}else
+		{
+			return params.get(paramName);
+		}
+	}
+	
+	// TODO: make this work.
+	public void save()
+	{
+		
+	}
+	
 	/**
 	 * This is a primitive parse for [-key val] style option variables in Strings.
 	 * For example, for the input:
