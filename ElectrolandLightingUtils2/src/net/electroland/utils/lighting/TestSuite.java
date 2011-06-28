70b743826db23cdf8135769528b63bf46d05579a
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-28 18:20:58 +0000

    testSuite properly notifies it is done and can restart.  also, testSuite has debug() and shows up in ELUManager.debug().

--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/TestSuite.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/TestSuite.java
@@ -2,27 +2,37 @@ package net.electroland.utils.lighting;
 
 import java.util.List;
 
+import net.electroland.utils.Util;
+
+import org.apache.log4j.Logger;
+
 public class TestSuite implements Runnable {
 
-	private int loopsLeft;
+	private static Logger logger = Logger.getLogger(TestSuite.class);
+	
+	
+	private int loops;
 	private int fps;
 	private Test[] tests;
 	private ELUManager elu;
 	private byte color;
 	private Thread thread;
+	private String name;
 	
-	public TestSuite(ELUManager elu, int fps, List<Test>tests, int loops, byte color)
+	public TestSuite(String name, ELUManager elu, int fps, List<Test>tests, int loops, byte color)
 	{
+		this.name = name;
 		this.elu = elu;
 		this.fps = fps;
 		this.tests = new Test[tests.size()];
 		tests.toArray(this.tests);
-		this.loopsLeft = loops;
+		this.loops = loops;
 		this.color = color;
 	}
 	
 	@Override
 	public void run() {
+		int loopsLeft = loops;
 		while (loopsLeft-- > 0)
 		{
 			for (Test t : tests)
@@ -39,9 +49,29 @@ public class TestSuite implements Runnable {
 				}
 			}
 		}
+		
+		// need to tell ELU that you are done.
+		elu.testDone();
+		thread = null;
 	}
 	public void start(){
 		thread = new Thread(this);
 		thread.start();
+	}
+
+	public void debug()
+	{
+		StringBuffer sb = new StringBuffer("TestSuite '");
+		sb.append(name).append("'[fps=").append(fps).append(",loops=");
+		sb.append(loops).append(",color=").append(Util.bytesToHex(color));
+		sb.append(",tests[");
+		for (Test t : tests)
+		{
+			sb.append(t.name);
+			sb.append(',');
+		}
+		sb.append("todo: remove last comma");
+		sb.append("]]");
+		logger.debug(sb.toString());
 	}
 }
