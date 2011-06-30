cc0a34e7ea5d881cf047184d4a9cfa6c0e529750
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-06-30 13:42:00 +0000

    added unit test.  and note that this object might really not be that useful yet

--- SkateAD/src/net/electroland/skate/core/ChannelPool.java
+++ SkateAD/src/net/electroland/skate/core/ChannelPool.java
@@ -5,8 +5,24 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ChannelPool {
 
+	// this would be a more useful class if it store Channel objects that
+	// had some kind of broadly applicable information.  Otherwise, could
+	// just put a Queue in SoundController.  Or List for that matter.
 	Queue<Integer> availableChannels;
 
+	public static void main(String args[]){
+		
+		// unit tests
+		ChannelPool p = new ChannelPool(3);
+		assert p.getFirstAvailable() != -1; // should succeed
+		assert p.getFirstAvailable() != -1; // should succeed
+		assert p.getFirstAvailable() != -1; // should succeed
+		assert p.getFirstAvailable() == -1; // should fail
+		p.releaseChannel(2);
+		assert p.getFirstAvailable() != -1; // should succeed
+		System.out.println("test succeeded.");
+	}
+	
 	public ChannelPool(int channels)
 	{
 		availableChannels = new ConcurrentLinkedQueue<Integer>();
