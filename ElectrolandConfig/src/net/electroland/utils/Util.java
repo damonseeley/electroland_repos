45fcf2f7ffba43fc0ede29c08abb197ed2a10bf7
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-09-11 01:17:11 +0000

    Moved config classes from ELU2.

---
+++ ElectrolandConfig/src/net/electroland/utils/Util.java
@@ -0,0 +1,28 @@
+package net.electroland.utils;
+
+public class Util {
+
+	public static byte getHiByte(short s){
+		return (byte)(s >> 8);
+	}
+	
+	public static byte getLoByte(short s){
+		return (byte)s;
+	}
+	public static String bytesToHex(byte b)
+	{
+		return Integer.toHexString((b&0xFF) | 0x100).substring(1,3) + " ";
+	}
+	public static String bytesToHex(byte[] b, int length)
+	{
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i< length; i++){
+			sb.append(Integer.toHexString((b[i]&0xFF) | 0x100).substring(1,3) + " ");
+		}
+		return sb.toString();
+	}
+	public static int unsignedByteToInt(byte b) 
+	{
+		return (int) b & 0xFF;
+	}
+}
