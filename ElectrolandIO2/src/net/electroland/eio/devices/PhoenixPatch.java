901cea75b8da51d49c3145fa35d6a25a604270ed
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-11 06:50:22 +0000

    first test complete.

---
+++ ElectrolandIO2/src/net/electroland/eio/devices/PhoenixPatch.java
@@ -0,0 +1,12 @@
+package net.electroland.eio.devices;
+
+public class PhoenixPatch {
+
+    int startRef, bit;
+
+    public PhoenixPatch(int startRef, int bit)
+    {
+        this.startRef = startRef;
+        this.bit = bit;
+    }
+}
