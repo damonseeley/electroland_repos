344293b72f2fdc862a2ea23e5d87e50b663f8d2f
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-18 04:30:12 +0000

    ionodes to iodevices

--- ElectrolandIO2/src/net/electroland/eio/IOThread.java
+++ ElectrolandIO2/src/net/electroland/eio/IOThread.java
@@ -6,69 +6,69 @@ import net.electroland.eio.devices.IODevice;
 
 public class IOThread implements Runnable {
 
-	protected int rate = 1;
-	private Thread thread = null;
-	private Collection<IODevice> devices;
-	private boolean isRunning = false;
-	
-	public IOThread(Collection<IODevice> devices, int rate)
-	{
-		this.devices = devices;
-		this.rate = rate;
-	}
-	
-	@Override
-	public void run() {
+    protected int rate = 1;
+    private Thread thread = null;
+    private Collection<IODevice> devices;
+    private boolean isRunning = false;
 
-		// connect all
-		for (IODevice device: devices)
-		{
-			device.connect();
-		}
+    public IOThread(Collection<IODevice> devices, int rate)
+    {
+        this.devices = devices;
+        this.rate = rate;
+    }
 
-		while (isRunning){
+    @Override
+    public void run() {
+    
+    	// connect all
+        for (IODevice device: devices)
+        {
+            device.connect();
+        }
+    
+        while (isRunning){
 
-			long start = System.currentTimeMillis();
-			
-			// sync states
-			for (IODevice device: devices)
-			{
-				device.readInput();
-				device.sendOutput();
-			}
-			
-			long duration = System.currentTimeMillis() - start;
-			long delay = (long)(1000.0/rate);
+            long start = System.currentTimeMillis();
 
-			if (duration < delay)
-				delay -= duration;
-			else
-				delay = 0;
-			
-			try {
-				Thread.sleep(delay);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+            // sync states
+            for (IODevice device: devices)
+            {
+                device.readInput();
+                device.sendOutput();
+            }
+            // TODO: add code to get measured FPS
+            long duration = System.currentTimeMillis() - start;
+            long delay = (long)(1000.0/rate);
 
-		// close all
-		for (IODevice device: devices)
-		{
-			device.close();
-		}
-		thread = null;
-	}
-	
-	public void start()
-	{
-		isRunning = true;
-		thread = new Thread(this);
-		thread.start();
-	}
+            if (duration < delay)
+                delay -= duration;
+            else
+                delay = 0;
 
-	public void stop()
-	{
-		isRunning = false;
-	}
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // close all
+        for (IODevice device: devices)
+        {
+            device.close();
+        }
+        thread = null;
+    }
+
+    public void start()
+    {
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    
+    public void stop()
+    {
+        isRunning = false;
+    }
 }
