10629dca1908296faaf9be8725914e7e4eb0cae6
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-18 04:26:52 +0000

    added a couple more utils.

--- ElectrolandConfig/src/net/electroland/utils/Util.java
+++ ElectrolandConfig/src/net/electroland/utils/Util.java
@@ -2,27 +2,51 @@ package net.electroland.utils;
 
 public class Util {
 
-	public static byte getHiByte(short s){
-		return (byte)(s >> 8);
-	}
-	
-	public static byte getLoByte(short s){
-		return (byte)s;
-	}
-	public static String bytesToHex(byte b)
-	{
-		return Integer.toHexString((b&0xFF) | 0x100).substring(1,3) + " ";
-	}
-	public static String bytesToHex(byte[] b, int length)
-	{
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i< length; i++){
-			sb.append(Integer.toHexString((b[i]&0xFF) | 0x100).substring(1,3) + " ");
-		}
-		return sb.toString();
-	}
-	public static int unsignedByteToInt(byte b) 
-	{
-		return (int) b & 0xFF;
-	}
+    public static byte getHiByte(short s)
+    {
+        return (byte)(s >> 8);
+    }
+
+    public static byte getLoByte(short s)
+    {
+        return (byte)s;
+    }
+
+    public static String bytesToHex(byte b)
+    {
+        return Integer.toHexString((b&0xFF) | 0x100).substring(1,3) + " ";
+    }
+
+    public static String bytesToHex(byte[] b, int length)
+    {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i< length; i++){
+            sb.append(Integer.toHexString((b[i]&0xFF) | 0x100).substring(1,3) + " ");
+        }
+        return sb.toString();
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                                 + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+    public static byte[] intToByteArray(int value) {
+        byte[] b = new byte[4];
+        for (int i = 0; i < 4; i++) {
+            int offset = (b.length - 1 - i) * 8;
+            b[i] = (byte) ((value >>> offset) & 0xFF);
+        }
+        return b;
+    }
+
+    public static int unsignedByteToInt(byte b) 
+    {
+        return (int) b & 0xFF;
+    }
 }
