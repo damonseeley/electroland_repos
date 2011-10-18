e398b9f6dfc153eaf9a8030f8428c47539f5adbf
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-18 04:27:10 +0000

    tabs to spaces.

--- ElectrolandConfig/src/net/electroland/utils/OptionException.java
+++ ElectrolandConfig/src/net/electroland/utils/OptionException.java
@@ -3,17 +3,17 @@ package net.electroland.utils;
 @SuppressWarnings("serial")
 public class OptionException extends Exception
 {
-	public OptionException(){
-		super();
-	}
+    public OptionException(){
+        super();
+    }
 
-	public OptionException(String mssg)
-	{
-		super(mssg);
-	}
+    public OptionException(String mssg)
+    {
+        super(mssg);
+    }
 
-	public OptionException(Exception e)
-	{
-		super(e.getMessage());
-	}
+    public OptionException(Exception e)
+    {
+        super(e.getMessage());
+    }
 }
