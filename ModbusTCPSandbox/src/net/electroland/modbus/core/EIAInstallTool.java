4412567903a583bd1d0c40a9e52a2851f1ef3072
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-18 01:07:31 +0000

    updated UI added code for checking states

--- ModbusTCPSandbox/src/net/electroland/modbus/core/EIAInstallTool.java
+++ ModbusTCPSandbox/src/net/electroland/modbus/core/EIAInstallTool.java
@@ -1,58 +1,65 @@
 package net.electroland.modbus.core;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.color.ColorSpace;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JSeparator;
-import javax.swing.JSlider;
 import javax.swing.JTextField;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 import net.miginfocom.swing.MigLayout;
 import net.wimpi.modbus.util.BitVector;
 
 import org.apache.log4j.Logger;
 
-public class EIAInstallTool extends JFrame{
+public class EIAInstallTool extends JFrame implements Runnable  {
 
-protected JButton startButton, stopButton;
-protected JTextField ipAddressInput;
-protected ButtonGroup buttonGroup;
-protected JLabel ipCmds, sensorOutput;
+	protected JButton startButton, stopButton;
+	protected JTextField ipAddressInput;
+	protected ButtonGroup buttonGroup;
+	protected JLabel ipCmds, sensorOutput;
 
-private MTMThread mtmt;
-private String startIP;
-private int startFramerate;
-	
+	SensorPanel sp;
+
+	private MTMThread mtmt;
+	private String startIP;
+	private int startFramerate;
+
+	private int windowWidth,windowHeight;
+	private int sensorWidth,sensorHeight;
+
 	static Logger logger = Logger.getLogger(EIAInstallTool.class);
 
-	public EIAInstallTool(int width, int height) {
-		
+	public EIAInstallTool() {
+
 		super("EIA Setup Tool");
-		
-		startIP = "192.168.247.22";
-		startFramerate = 60;
-		
+
+		windowWidth = 1024;
+		windowHeight = 768;
+		sensorWidth = windowWidth;
+		sensorHeight = 600;
+
+		startIP = "192.168.247.61";
+		startFramerate = 30;
+
 		JPanel ipPanel = new JPanel();
 		ipPanel.setBorder(BorderFactory.createTitledBorder("IP Address Info"));
 		this.setLayout(new MigLayout(""));
 		this.add(ipPanel, "center, growx, wrap");
-		
+
 		ipPanel.setLayout(new MigLayout(""));
 
-		
+
 		ipAddressInput = new JTextField();
 		ipPanel.add(ipAddressInput, "span2 ,growx, wrap");
 		ipPanel.add(new JSeparator(), "span, growx, wrap");
@@ -60,45 +67,58 @@ private int startFramerate;
 		ipPanel.add(startButton, "center");
 		stopButton = new JButton("Stop");
 		ipPanel.add(stopButton, "center");
-		
+
 		startButton.addActionListener(new ActionListener(){
-	        public void actionPerformed(ActionEvent e) {
-	        	resetMTMT("0.0.0.0");
-	        }
+			public void actionPerformed(ActionEvent e) {
+				resetMTMT("0.0.0.0");
+			}
 		});
-		
+
 		stopButton.addActionListener(new ActionListener(){
-	        public void actionPerformed(ActionEvent e) {
-	        	//logger.info(e);
-	        	killMTMT();
-	        }
+			public void actionPerformed(ActionEvent e) {
+				//logger.info(e);
+				killMTMT();
+			}
 		});
-		
+
+		sp = new SensorPanel(sensorWidth,sensorHeight);
+		this.add(sp);
+
+
 		//setup window
 		this.setVisible(true);
-		this.setSize(width, height);
+		this.setSize(windowWidth, windowHeight);
 
 		this.addWindowListener(
-			new java.awt.event.WindowAdapter() {
-			    public void windowClosing(WindowEvent winEvt) {
-			    	//fix
-			    	//close();
-			    }
-		});
+				new java.awt.event.WindowAdapter() {
+					public void windowClosing(WindowEvent winEvt) {
+						//fix
+						close();
+					}
+				});
+
+		mtmt = new MTMThread(startIP,startFramerate,sp);
 		
-		mtmt = new MTMThread(startIP,startFramerate);
 		
-		
-		
+		Thread t=new Thread (this);
+		t. start();
 
 	}
-	
+
+	public void run() {
+
+		while(true) {
+			//logger.info("I'm running");
+		}
+
+	}
+
 	private void resetMTMT(String newip){
 		logger.info("Resetting MTMT");
 		mtmt.stopClean();
 		//mtmt = new MTMThread(newip,startFramerate);
 	}
-	
+
 	private void killMTMT() {
 		logger.info("Killing MTMT");
 		mtmt.stopClean();
@@ -106,16 +126,16 @@ private int startFramerate;
 
 
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 	public static void printOutput(byte[] bytes, String label)
 	{
 		BitVector bv = BitVector.createBitVector(bytes);
@@ -130,17 +150,17 @@ private int startFramerate;
 		System.out.println("<-- us on " + label);
 
 	}
-	
+
 
 	private void close(){
 		mtmt.stopClean();
 		System.exit(0);
 	}
-	
-	
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EIAInstallTool eiait = new EIAInstallTool(1024, 768);
+		EIAInstallTool eiait = new EIAInstallTool();
 
 	}
 
