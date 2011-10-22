608daea825d910c7f1e1c38c91a30b12e15958b0
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-22 18:23:02 +0000

    this project is corrupted.

--- ElectrolandIO/src/net/electroland/eio/OState.java
+++
@@ -1,9 +0,0 @@
-package net.electroland.eio;
-
-public class OState extends IOState{
-
-    public OState(String id, int x, int y, int z, String units) {
-        super(id, x, y, z, units);
-    }
-
-}
--- ElectrolandIO/src/net/electroland/utils/sensors/OState.java
+++
@@ -1,5 +0,0 @@
-package net.electroland.utils.sensors;
-
-public class OState extends IOState{
-
-}
