2a811d567fef8167175daa6db2e052f96ffb461c
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-22 07:24:36 +0000

    first checkin

---
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/GrayScaleDetectionModel.java
@@ -0,0 +1,24 @@
+package net.electroland.utils.lighting.detection;
+
+import net.electroland.utils.lighting.DetectionModel;
+
+public class GrayScaleDetectionModel implements DetectionModel {
+
+    @Override
+    public byte getValue(int[] pixels) {
+        float total = 0;
+        for (int i = 0; i < pixels.length; i++){
+
+            // separate rgb vals
+            int r = (pixels[i] >> 16) & 0xFF;
+            int g = (pixels[i] >> 8 & 0xFF);
+            int b = (pixels[i] & 0xFF);
+
+            // http://en.wikipedia.org/wiki/Grayscale
+            int gy = (int)((.3 * r) + (.59 * g) + (.11 * b));
+
+            total += gy;
+        }
+        return (byte)(total / pixels.length);
+    }
+}
