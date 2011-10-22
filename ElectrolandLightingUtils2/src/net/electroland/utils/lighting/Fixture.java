f90b056bcb3e1e556ee44760b824ee3b9dbc297a
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-22 23:19:07 +0000

    added support for dimension and location of fixture

--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/Fixture.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/Fixture.java
@@ -3,31 +3,42 @@ package net.electroland.utils.lighting;
 import java.util.List;
 import java.util.Vector;
 
+import javax.vecmath.Point3d;
+
 public class Fixture
 {
-	protected String name;
-	protected FixtureType type;
-	protected int startAddress;
-	protected Vector<String> tags = new Vector<String>();
-	protected Recipient recipient;
-	
-	
-	public Fixture(String name, FixtureType type, int startAddress, Recipient recipient, List<String> tags){
-		this.name = name;
-		this.type = type;
-		this.startAddress = startAddress;
-		this.tags.addAll(tags);
-		this.recipient = recipient;
-	}
+    protected String name;
+    protected FixtureType type;
+    protected int startAddress;
+    protected Vector<String> tags = new Vector<String>();
+    protected Recipient recipient;
+    protected Point3d location;
 
-	public String toString()
-	{
-		StringBuffer sb = new StringBuffer("FixtureType").append(name).append("[");
-		sb.append("type=").append(type);
-		sb.append(",startAddress=").append(startAddress);
-		sb.append(",recipient=").append(recipient);
-		sb.append("tags=").append(tags);
-		sb.append("]");
-		return sb.toString();
-	}
+    public Fixture(String name, FixtureType type, int startAddress, Recipient recipient, List<String> tags){
+        this.name = name;
+        this.type = type;
+        this.startAddress = startAddress;
+        this.tags.addAll(tags);
+        this.recipient = recipient;
+    }
+
+    public Point3d getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point3d location) {
+        this.location = location;
+    }
+
+    public String toString()
+    {
+        StringBuffer sb = new StringBuffer("FixtureType").append(name).append("[");
+        sb.append("type=").append(type);
+        sb.append(",startAddress=").append(startAddress);
+        sb.append(",recipient=").append(recipient);
+        sb.append("tags=").append(tags);
+        sb.append("location=").append(location);
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/FixtureType.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/FixtureType.java
@@ -1,16 +1,27 @@
 package net.electroland.utils.lighting;
 
+import java.awt.Dimension;
 import java.util.Vector;
 
 public class FixtureType
 {
 	protected String name;
 	protected Vector<Detector> detectors;
-	
+	protected Dimension size;
+
 	public FixtureType(String name, int channels)
 	{
 		this.name = name;
 		detectors = new Vector<Detector>();
 		detectors.setSize(channels);
 	}
+
+    public Dimension getSize() {
+        return size;
+    }
+
+    public void setSize(Dimension size) {
+        this.size = size;
+    }
+
 }
