4ce0964d25eaac5162b27f242619adbb240ec6b9
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-10-23 03:30:27 +0000

    first checkin.

--- ElectrolandIO/src/net/electroland/eio/filters/SimpleLowPass.java
+++ ElectrolandIO/src/net/electroland/eio/filters/SimpleLowPass.java
@@ -7,21 +7,74 @@ import net.electroland.utils.ParameterMap;
 public class SimpleLowPass implements IOFilter {
 
     private static Logger logger = Logger.getLogger(IOFilter.class);
-    private int millis;
+
+    private int data[];
+    private int index = 0;
+    private boolean firstpass = true;
+    private boolean lastBool = false; // for breaking ties with bools.
+
+    public static void main(String args[]){
+        SimpleLowPass lo = new SimpleLowPass();
+        lo.data = new int[10];
+
+//        boolean[] samples = {true,true,true,true,true,true,false,false,true,true,false,true,false,true};
+//        boolean[] samples = {true,false,true,false,true,false,true,false,true,true,false,false,false,false};
+        byte[] samples = {(byte)100,(byte)100,(byte)100,(byte)0,(byte)-1};
+        
+        for (int i = 0; i < samples.length; i++)
+        {
+            System.out.println(lo.filter(samples[i]));
+        }
+    }
 
     @Override
     public void configure(ParameterMap params) {
-        millis = params.getRequiredInt("millis");
-        logger.info("\t\tconfigured SimpleLowPass filter for " + millis + " millis.");
+        data = new int[params.getRequiredInt("samples")];
+        logger.info("\t\tconfigured SimpleLowPass filter for " + data.length + " samples.");
     }
 
     @Override
     public byte filter(byte b) {
-        return b;
+        // a lot of casting on behalf of generalizing this. maybe don't?
+        int average = (int)add((int)b);
+        return (byte)average;
     }
 
     @Override
     public boolean filter(boolean b) {
+        // maybe more efficient not to generalize?  for booleans, a simple up
+        // down count is enough. 
+        // (if the new value is true, add one. otherwise, subtract. then
+        //  do the same thing with the erased value from the ring buffer)
+        // would avoid calculating an average plus all decimal conversions.
+        double average = add(b ? 1 : 0);
+        System.out.println(average);
+        if (average ==.5)
+        {
+            return lastBool;
+        }else if (average < .5){
+            lastBool = false;
+            return false;
+        }else if (average > .5){
+            lastBool = true;
+            return true;
+        }
+
         return b;
+    }
+
+    // ring buffered average
+    private double add(int sample){
+        data[index++] = sample;
+        if (index == data.length){
+            index = 0;
+            firstpass = false;
+        }
+        int total = 0;
+        int samples = firstpass ? index : data.length;
+        for (int i = 0; i < samples; i++){
+            total += data[i];
+        }
+        return total / ((double)samples);
     }
 }
