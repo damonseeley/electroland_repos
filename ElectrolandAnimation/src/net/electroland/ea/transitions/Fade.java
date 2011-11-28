2ef1856cb19ac3a7aa52a9ac30c13d929327b2cc
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-11-28 06:57:06 +0000

    first checkin.

---
+++ ElectrolandAnimation/src/net/electroland/ea/transitions/Fade.java
@@ -0,0 +1,35 @@
+package net.electroland.ea.transitions;
+
+import java.awt.Image;
+import java.util.Map;
+
+import net.electroland.ea.Transition;
+import net.electroland.utils.ParameterMap;
+
+public class Fade implements Transition {
+
+    @Override
+    public void config(ParameterMap config) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void init(Map<String, Object> context) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isDone() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Image getFrame(Image primary, Image secondary) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
