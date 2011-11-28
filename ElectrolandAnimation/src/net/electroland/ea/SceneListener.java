1452f46aec6f9b9b00b922a4baf9aa301f3f1f00
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-11-28 07:11:49 +0000

    keep Scene more opaque.

--- ElectrolandAnimation/src/net/electroland/ea/SceneListener.java
+++ ElectrolandAnimation/src/net/electroland/ea/SceneListener.java
@@ -1,6 +1,6 @@
 package net.electroland.ea;
 
 public interface SceneListener {
-    public void sceneComplete(int id, Scene s);
-    public void sceneStarted(int id, Scene s);
+    public void sceneComplete(int id, String scene);
+    public void sceneStarted(int id, String s);
 }
