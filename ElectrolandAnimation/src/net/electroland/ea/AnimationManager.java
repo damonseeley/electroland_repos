6acec5a4f43582143ede59ff1bdfdf3d888b3dc9
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-11-29 05:52:27 +0000

    config is complete.

--- ElectrolandAnimation/src/net/electroland/ea/AnimationManager.java
+++ ElectrolandAnimation/src/net/electroland/ea/AnimationManager.java
@@ -2,16 +2,18 @@ package net.electroland.ea;
 
 import java.awt.Dimension;
 import java.awt.Image;
+import java.util.Hashtable;
 import java.util.Map;
+
+import net.electroland.utils.ElectrolandProperties;
+import net.electroland.utils.ParameterMap;
 
 public class AnimationManager {
 
-    private Image one, two;
-
-    public void config(String filename)
-    {
-        
-    }
+    private Dimension stageDim;
+    private int fps = 33;
+    Map<String, Scene> scenes;
+    Map<String, Transition> transitions;
 
     public void setContext(Map<String, Object> context)
     {
@@ -88,5 +90,47 @@ public class AnimationManager {
     public int[] getStagePixels(int x, int y, int width, int height)
     {
         return null;
+    }
+
+    /************************ parse props *************************************/
+    public void config(String filename)
+    {
+        ElectrolandProperties p = new ElectrolandProperties(filename);
+
+        // fps, stage width & height
+        Integer fpsProp = p.getOptionalInt("settings", "global", "fps");
+        this.fps = fpsProp == null ? this.fps : fpsProp;
+
+        Integer widthProp = p.getOptionalInt("settings", "global", "width");
+        if (widthProp != null)
+            stageDim.width = widthProp;
+
+        Integer heightProp = p.getOptionalInt("settings", "global", "height");
+        if (heightProp != null)
+            stageDim.height = heightProp;
+
+        // scenes
+        scenes = new Hashtable<String,Scene>();
+        Map<String, ParameterMap> sceneParams = p.getObjects("scene");
+        for (String s : sceneParams.keySet()){
+            ParameterMap universalParams = sceneParams.get(s);
+            Map<String, ParameterMap> extendedParams = p.getObjects(s);
+
+            Scene scene = (Scene)(universalParams.getRequiredClass("class"));
+            scene.config(universalParams, extendedParams);
+            scenes.put(s, scene);
+        }
+
+        // transitions
+        transitions = new Hashtable<String, Transition>();
+        Map<String, ParameterMap> transitionParams = p.getObjects("transition");
+        for (String s : transitionParams.keySet()){
+            ParameterMap universalParams = transitionParams.get(s);
+            Map<String, ParameterMap> extendedParams = p.getObjects(s);
+
+            Transition transition = (Transition)(universalParams.getRequiredClass("class"));
+            transition.config(universalParams, extendedParams);
+            transitions.put(s, transition);
+        }
     }
 }
