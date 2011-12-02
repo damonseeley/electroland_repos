02f74c4ebc01497a819e3e7a25555f8ed29b7c3e
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-12-02 04:45:57 +0000

    clip implementation, first pass.

--- ElectrolandAnimation/src/net/electroland/ea/Clip.java
+++ ElectrolandAnimation/src/net/electroland/ea/Clip.java
@@ -1,23 +1,140 @@
 package net.electroland.ea;
 
+import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import net.electroland.utils.ParameterMap;
 
 public abstract class Clip {
 
-    int x, width, height;
+    // values used to add this clip to the scene
+    protected Rectangle area;
+    protected int alpha;
+    protected Dimension baseSize;
+    private Queue<Change> changes;
+    private Change currentChange;
+    private boolean isDeleted = false;
 
     // configuration parameters from the properties file
     abstract public void config(ParameterMap primaryParams, Map<String, ParameterMap> extendedParams);
 
     // whatever the Conductor decided you needed when you start an instance
-    abstract public void init(Map<String,Object> context);
+    abstract public void init(Map<String, Object> context);
 
     // return false when it is time for this scene to die.
     abstract public boolean isDone();
 
-    // return an Image for the current frame.
-    abstract public Image getFrame(int width, int height);
+    // Will be passed a baseSize.width x baseSize.height image.  
+    // manager is expecting to receive the same back (should properly implement
+    // manager as an image observer instead)
+    abstract public Image getFrame(Image image);
+
+    public boolean isDeleted(){
+        return isDeleted;
+    }
+
+    public void processChanges()
+    {
+        if (currentChange != null)
+        {
+            if (currentChange.isComplete()){
+                
+                if (currentChange.deleteWhenDone){
+                    isDeleted = true;
+                }
+                
+                if (currentChange.started){
+                    // apply the final area and alpha
+                    this.alpha = currentChange.targetAlpha;
+                    this.area = currentChange.targetArea;
+                }
+                currentChange = null;
+            }else{
+                if (currentChange.started){
+                    // percent complete
+                    double pc = (System.currentTimeMillis() - currentChange.start)
+                                    /(double)currentChange.duration;
+                    
+                    if (currentChange.initArea != currentChange.targetArea){
+                        alpha = Change.d(currentChange.initAlpha, currentChange.targetAlpha, pc);
+                    }
+                    if (currentChange.initAlpha != currentChange.targetAlpha){
+                        area.x = Change.d(currentChange.initArea.x, currentChange.targetArea.x, pc);
+                        area.y = Change.d(currentChange.initArea.y, currentChange.targetArea.y, pc);
+                        area.width = Change.d(currentChange.initArea.width, currentChange.targetArea.width, pc);
+                        area.height = Change.d(currentChange.initArea.height, currentChange.targetArea.height, pc);
+                         
+                    }
+                }else{
+                    if (System.currentTimeMillis() > currentChange.start){
+                        // why don't you start me up! oh yeeeah
+                        currentChange.initArea = new Rectangle(area);
+                        currentChange.initAlpha = new Integer(alpha);
+                        if (currentChange.targetArea == null)
+                            currentChange.targetArea = currentChange.initArea;
+                        if (currentChange.targetAlpha == null)
+                            currentChange.targetAlpha = currentChange.initAlpha;
+                    }
+                }
+            }
+        }
+        if (currentChange == null && !changes.isEmpty()){
+            // get the next change
+            currentChange = changes.remove();
+            // configure it
+            currentChange.start = System.currentTimeMillis() + currentChange.delay;
+            currentChange.finish = currentChange.start + currentChange.duration;
+        }
+    }
+
+
+    public Dimension getBaseSize()
+    {
+        return baseSize;
+    }
+
+    public void queueChange(Rectangle area, Integer alpha, int durationMillis, int delayMillis, boolean deleteWhenDone)
+    {
+        if (changes == null)
+        {
+            resetQueue();
+        }
+        changes.add(new Change(area, alpha, durationMillis, delayMillis, deleteWhenDone));
+    }
+    public void resetQueue()
+    {
+        changes = new ConcurrentLinkedQueue<Change>();
+    }
+}
+
+class Change{
+
+    long start, finish;
+    int delay;
+    int duration;
+    Rectangle initArea, targetArea;
+    int initAlpha;
+    Integer targetAlpha;
+    boolean started = false;
+    boolean deleteWhenDone = false;
+
+    public Change(Rectangle area, Integer alpha, int duration, int delay, boolean deleteWhenDone){
+        this.targetArea = area;
+        this.targetAlpha = alpha;
+        this.duration = duration;
+        this.delay = delay;
+        this.deleteWhenDone = deleteWhenDone;
+    }
+
+    public boolean isComplete(){
+        return System.currentTimeMillis() > finish;
+    }
+    
+    public static int d(int init, int target, double complete){
+        return (int)((complete * (target - init)) + init);
+    }
 }
