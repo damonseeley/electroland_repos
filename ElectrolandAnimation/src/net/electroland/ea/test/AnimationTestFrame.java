2821c6d918cc3906faa12e973ab1cc8434e7a895
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-12-03 05:12:35 +0000

    test with three images

--- ElectrolandAnimation/src/net/electroland/ea/test/AnimationTestFrame.java
+++ ElectrolandAnimation/src/net/electroland/ea/test/AnimationTestFrame.java
@@ -45,6 +45,9 @@ public class AnimationTestFrame extends JFrame implements ClipListener{
         // simultaneouslyplay a clip in the bottom/left quadrant
         int clipId2 = anim.startClip("testClip", new Rectangle(0,h,w,h), 1.0);
 
+        // simultaneouslyplay a clip in the top/right quadrant
+        int clipId3 = anim.startClip("testClip", new Rectangle(w,0,w,h), 1.0);
+
         // delay 2 seconds, and then slowly expand clip1 to full screen
         anim.queueClipChange(clipId1, new Rectangle(0,0,w*2,h*2), null, null, 1500, 1000, false);
 
@@ -59,7 +62,8 @@ public class AnimationTestFrame extends JFrame implements ClipListener{
         anim.queueClipChange(clipId2, null, null, 1.0, 500, 0, false);
         anim.queueClipChange(clipId2, null, null, .25, 500, 0, false);
         anim.queueClipChange(clipId2, null, null, 1.0, 500, 0, false);
-        anim.queueClipChange(clipId2, null, null, 0.0, 500, 0, true);
+        anim.queueClipChange(clipId2, new Rectangle(w,0,w,h), null, 0.0, 3000, 0, true);
+        anim.queueClipChange(clipId3, new Rectangle(w,h,0,0), null, 0.0, 3000, 6000, true);
 
         while (true){
             f.getGraphics().drawImage(anim.getStage(), 0, 0, f.getWidth(), f.getHeight(), null);
