fd1bfa6d60826747199c451d57462ee554147e61
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-12-03 01:14:14 +0000

    done.

--- ElectrolandAnimation/src/net/electroland/ea/test/AnimationTestFrame.java
+++ ElectrolandAnimation/src/net/electroland/ea/test/AnimationTestFrame.java
@@ -28,9 +28,19 @@ public class AnimationTestFrame extends JFrame{
         anim.config(args.length > 0 ? args[1] : "animation.properties");
         int w = anim.getStageDimensions().width / 2;
         int h = anim.getStageDimensions().height / 2;
+
         // play a clip in the top/left quadrant
-        anim.startClip("testClip", new Rectangle(0,0,w,h), 1.0);
+        int clipId = anim.startClip("testClip", new Rectangle(0,0,w,h), 1.0);
+
+        // delay 2 seconds, and then slowly expand it to full screen
+        anim.queueClipChange(clipId, new Rectangle(0,0,w*2,h*2), null, null, 3000, 2000, false);
+
+        // delay 2 seconds, and then slowly make it the bottom left quadrant
+        anim.queueClipChange(clipId, new Rectangle(w,h,w,h), null, null, 3000, 2000, false);
         
+        // delay 2 seconds, and then fade it out
+        anim.queueClipChange(clipId, null, null, 0.0, 3000, 2000, true);
+
         AnimationTestFrame f = new AnimationTestFrame();
         f.setSize(anim.getStageDimensions());
         f.setVisible(true);
