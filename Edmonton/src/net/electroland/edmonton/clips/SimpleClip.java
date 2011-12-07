14f3259736d971c3705d16dd838b9fee31a420e4
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-12-07 03:09:17 +0000

    remove flickr

--- Edmonton/src/net/electroland/edmonton/clips/SimpleClip.java
+++ Edmonton/src/net/electroland/edmonton/clips/SimpleClip.java
@@ -2,6 +2,7 @@ package net.electroland.edmonton.clips;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.util.Map;
@@ -42,9 +43,13 @@ public class SimpleClip extends Clip {
 
 		//only renders if the delay time (rate) has been exceeded
 		// this should work if Clip.image is not cleared during each getFrame, but it appears that it does not
-		if (System.currentTimeMillis() - lastRender > delay)
-		{
-			//Double buffer this to prevent flickering
+//		if (System.currentTimeMillis() - lastRender > delay)
+//		{
+	        // BELOW IS UNNECESSARY. you could just do:
+	        // Graphics g = image.getGraphics();
+	        // g.setColor(Color.WHITE);
+	        // g.fillRect(0, 0, 15,8);
+        //Double buffer this to prevent flickering // BRADLEY: THIS IS UNNECESARRY.
 			BufferedImage bi = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
 			Graphics g2 = bi.getGraphics();
 			g2.setColor(Color.WHITE);
@@ -53,7 +58,13 @@ public class SimpleClip extends Clip {
 			Graphics g = image.getGraphics();
 			g.drawImage(bi, 0, 0, null);
 			lastRender = System.currentTimeMillis();
-		}
+
+//		}else{
+	        // THEN NOTHING HAPPENS HERE.  E.g., nothing is painted so the
+		    // image is returned same as it was passed into getFrame, which is
+		    // to say blank.
+		    // That's causing the flicker.
+//		}
 
 		return image;
 	}
