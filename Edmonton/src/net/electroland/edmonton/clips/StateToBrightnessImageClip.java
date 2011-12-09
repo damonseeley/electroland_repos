9142d72a2be5f1a2563a4e798e1ad2a9c1f8e231
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2011-12-09 23:01:20 +0000

    changed state to brightness Clip to be image based with alpha to prevent clip/draw order problems

--- Edmonton/src/net/electroland/edmonton/clips/StateToBrightnessImageClip.java
+++ Edmonton/src/net/electroland/edmonton/clips/StateToBrightnessImageClip.java
@@ -1,20 +1,26 @@
 package net.electroland.edmonton.clips;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 import net.electroland.ea.Clip;
 import net.electroland.utils.ParameterMap;
 
 import org.apache.log4j.Logger;
 
-public class StateToBrightnessClip extends Clip {
+public class StateToBrightnessImageClip extends Clip {
 
-	private static Logger logger = Logger.getLogger(StateToBrightnessClip.class);
+	private static Logger logger = Logger.getLogger(StateToBrightnessImageClip.class);
+	private BufferedImage spriteImage;
 	int w,h;
 	Map<String, Object> bvals = new HashMap<String, Object>();
 
@@ -23,6 +29,16 @@ public class StateToBrightnessClip extends Clip {
 
 		w = primaryParams.getRequiredInt("width");
 		h = primaryParams.getRequiredInt("height");
+
+		String imageFile = primaryParams.getRequired("file");
+		try {
+			spriteImage = ImageIO.read(new File(imageFile));
+		} catch (IOException e) {
+			logger.info(this + " could not load image file " + imageFile);
+			//return;
+			//throw new OptionException(e);
+
+		}
 	}
 
 	@Override
@@ -46,18 +62,22 @@ public class StateToBrightnessClip extends Clip {
 
 
 		BufferedImage bi = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
-		Graphics g2 = bi.getGraphics();
-
+		Graphics2D g2 = (Graphics2D)bi.getGraphics();
+		
 		//g2.fillRect(0, 0, w, h);
 		for (String key : bvals.keySet()) {
 			BrightPoint bp = (BrightPoint) bvals.get(key);
 			//need to draw with alpha here...
 			if (bp.brightness > 0){
-				//logger.info(bp.x);
-				g2.setColor(new Color(bp.brightness, bp.brightness, bp.brightness));
-				g2.fillRect((int)bp.x-6, 0, 11, 16);
+				float fbright = bp.brightness/255.0f;
+				float[] scales = { 1f, 1f, 1f, fbright };
+				float[] offsets = new float[4];
+				RescaleOp rop = new RescaleOp(scales, offsets, null);
+				/* Draw the image, applying the filter */
+				g2.drawImage(spriteImage,rop,(int)bp.x-5,0);
 			}
 		}
+		g2.dispose();
 
 		Graphics g = image.getGraphics();
 		g.drawImage(bi, 0, 0, null);
