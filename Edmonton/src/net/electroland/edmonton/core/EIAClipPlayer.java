ebb4d5565128476518405f53cb5288ffcd270caf
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-11 23:28:39 +0000

    clip PER_TRACK working all the way through to anim and canvas

--- Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer.java
+++ Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer.java
@@ -1,18 +1,30 @@
 package net.electroland.edmonton.core;
 
+import java.awt.Rectangle;
+
 import net.electroland.ea.AnimationManager;
 
 public class EIAClipPlayer {
 
-	private AnimationManager am;
+	private AnimationManager anim;
 	
 	public EIAClipPlayer(AnimationManager am)
 	{
-		this.am = am;
+		this.anim = am;
 	}
 
 	public void testClip(double x)
 	{
 		System.out.println("PLAY AT " + x);
 	}
+	
+	public void sparkleClip32(double x){
+		int clip = anim.startClip("sparkleClip32", new Rectangle((int)x-16,0,32,16), 0.0);
+		//anim.queueClipChange(clip, null, null, 1.0, 1700, 0, false); //make it smaller
+		anim.queueClipChange(clip, null, null, 1.0, 500, 0, false); //fadein
+		anim.queueClipChange(clip, null, null, 0.0, 500, 500, true); //fadeout
+		//logger.info("Created Egg Sparkle at x = " + x);
+	}
+	
+	
 }
