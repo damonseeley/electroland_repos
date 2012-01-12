c486b40f91e7a4b8f0fcf381aaa6a02c5b53afc2
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-12 02:35:45 +0000

    slight refactoring

--- Edmonton/src/net/electroland/edmonton/core/sequencing/SoundCue.java
+++ Edmonton/src/net/electroland/edmonton/core/sequencing/SoundCue.java
@@ -6,7 +6,11 @@ import net.electroland.edmonton.core.SoundController;
 import net.electroland.utils.OptionException;
 import net.electroland.utils.ParameterMap;
 
+import org.apache.log4j.Logger;
+
 public class SoundCue extends Cue{
+
+    static Logger logger = Logger.getLogger(SoundCue.class);
 
     final static int PLAY_GLOBAL = 0;
     final static int PLAY_SINGLE_CHANNEL = 1;
@@ -40,25 +44,23 @@ public class SoundCue extends Cue{
     @Override
     public void play(Map<String, Object> context) {
 
-        if (context != null)
+        if (context != null && context.get("soundController") instanceof SoundController)
         {
-            Object o = context.get("soundController");
-            if (o instanceof SoundController)
-            {
-                SoundController sc = (SoundController)o;
+            SoundController sc = (SoundController)context.get("soundController");
 
-                switch(mode){
-                case(PLAY_GLOBAL):
-                    sc.playGlobal(filename, false, gain);
-                    break;
-                case(PLAY_LOCAL):
-                    sc.playLocal(filename, x, gain);
-                    break;
-                case(PLAY_SINGLE_CHANNEL):
-                    sc.playSingleChannel(filename, x, gain);
-                    break;
-                }
+            switch(mode){
+            case(PLAY_GLOBAL):
+                sc.playGlobal(filename, false, gain);
+                break;
+            case(PLAY_LOCAL):
+                sc.playLocal(filename, x, gain);
+                break;
+            case(PLAY_SINGLE_CHANNEL):
+                sc.playSingleChannel(filename, x, gain);
+                break;
             }
+        } else {
+            logger.warn("Context is missing 'soundController'");
         }
     }
 }
