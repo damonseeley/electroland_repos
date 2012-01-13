63131e9c57317c4523100725600bb70d6a102107
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-13 06:17:37 +0000

    more programming added logger

--- Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer.java
+++ Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer.java
@@ -2,11 +2,16 @@ package net.electroland.edmonton.core;
 
 import java.awt.Rectangle;
 
+import org.apache.log4j.Logger;
+
 import net.electroland.ea.AnimationManager;
+import net.electroland.edmonton.core.sequencing.ClipCue;
 
 public class EIAClipPlayer {
 
 	private AnimationManager anim;
+	
+    static Logger logger = Logger.getLogger(EIAClipPlayer.class);
 	
 	public EIAClipPlayer(AnimationManager am)
 	{
@@ -19,6 +24,7 @@ public class EIAClipPlayer {
 	}
 	
 	public void sparkleClip32(double x){
+		logger.debug("sparkleClip32 started at x=" + x);
 		int clip = anim.startClip("sparkleClip32", new Rectangle((int)x-16,0,32,16), 0.0);
 		//anim.queueClipChange(clip, null, null, 1.0, 1700, 0, false); //make it smaller
 		anim.queueClipChange(clip, null, null, 1.0, 500, 0, false); //fadein
