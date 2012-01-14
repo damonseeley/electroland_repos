0110a01a47549b167252ac6235a32ee469c62575
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-14 00:03:29 +0000

    updated pcode from Eitan

--- Edmonton/src/net/electroland/edmonton/core/TrackingSolver.java
+++ Edmonton/src/net/electroland/edmonton/core/TrackingSolver.java
@@ -1,34 +1,42 @@
 package net.electroland.edmonton.core;
 
 public class TrackingSolver {
-	
+
 	/*
-	sort sensors and track spatially rtol or ltor before calling this.
-	
-	public Solution solve(Sensors sensors, Tracks tracks, Solution bestScoreSoFar) {
-		if sensors.isEmpty() {
-			// for each unamtched track add to solution and update score with some penalty
-			return new Solution();
+	// sort sensors and tracks spatially RtoL or LtoR before calling this.
+
+	public Solution solve(Solution bestSolutionSoFar, Solution partialSolution, Sensors remainingSensors, Tracks remainingTracks) {
+		if partialSolution.getsScore() >= bestSolutionSoFar.getScore() {
+			return bestSolutionSoFar;
+		} else if (remainingSensors == null) {
+			// all done
+			add penalty for unmatched tracks to bestSolutionSoFar's score
+			return bestSolutionSoFar;
 		} else {
-			Sensor s = sensors.removeFirst();
-			for(Track t:tracks) {
-				Match m = new Match(s,t)
-				//is match valid? ie too far apart,etc
-			Solution partialSolution = solve(sensors, tracks, bestScoreSoFar);
-			if(partialSolution != null) {
-				paritalSolution.addMatch(m)
-				if(paritalSolution.getScore() < bestSoFar.getScore()) {
-					bestScoreSoFar =  paritalSolution;
-				} else {
-					return null;
-				}
+			Sensor s = remainingSensors.pop(); // remove and return first element
+			for(Track t : tracks) {
+				Tracks tracksWithoutT = remainingTracks.remove(t); // use a non destructive remove, should create a new list
+				//optionally disallow s and t as a match because they are too far apart
+				Solution newPartial = partialSolution.addMatch(new Match(s,t)); // non destructive, should create a new solution
+				Solution newSolution = solve(bestSolutionSoFar, newPartial, remainingSensors, tracksWithoutT)
+						if(newSolution.getScore() < bestSolutionSoFar.getScore()) {
+							bestSolutionSoFar = newSolution;
+						}
 			}
-			// create one more solution with a new track and compare
-			return bestScoreSoFar;
+			// see if we are better off with creating a new match
+			Solution newPartial = partialSolution.addMatch(new Match(s, new TrackAtLocationOfS());
+			add new track penalty to newPartial's score
+			Solution newSolution = solve(bestSolutionSoFar, newPartial, remainingSensors, remainingTracks)
+			if(newSolution.getScore() < bestSolutionSoFar.getScore()) {
+				bestSolutionSoFar = newSolution;
 			}
-			
+			return bestSolutionSoFar;
+
 		}
+
 	}
-	*/
+	 */
+
+
 
 }
