a619d76c379bd0818dc4460bde2b440ed667db37
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-16 07:42:48 +0000

    latest anim stuff

--- Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer.java
+++ Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer.java
@@ -32,6 +32,10 @@ public class EIAClipPlayer {
 		//logger.info("Created Egg Sparkle at x = " + x);
 	}
 	
+	/*
+	 * EIA Show methods
+	 */
+	
 	public void introLocalTrill4Up(double x) {
 		logger.info("localTrill4Up@ " + x);
 		//8 px wide, ms times 15 176 322 510, where 15 is effectively 0 in cue time
@@ -39,26 +43,22 @@ public class EIAClipPlayer {
 		//create the first bar and have it appear immediately
 		int trill1 = anim.startClip("simpleClip2", new Rectangle((int)x-4,0,2,16), 1.0); //2px wide rect at left of area
 		//create the other bars, but at 0.0 alpha to popin later
-		int trill2 = anim.startClip("simpleClip2", new Rectangle((int)x-2,0,2,16), 0.0);
-		int trill3 = anim.startClip("simpleClip2", new Rectangle((int)x,0,2,16), 0.0); 
-		int trill4 = anim.startClip("simpleClip2", new Rectangle((int)x+2,0,2,16), 0.0); 
+		int trill2 = anim.startClip("simpleClip2", new Rectangle((int)x-2,0,2,16), 0.1);
+		int trill3 = anim.startClip("simpleClip2", new Rectangle((int)x,0,2,16), 0.2); 
+		int trill4 = anim.startClip("simpleClip2", new Rectangle((int)x+2,0,2,16), 0.3); 
 		
 		//now pop in trills2-4 on time
-		anim.queueClipChange(trill2, null, null, 1.0, 1, 500, false); //popin
-		anim.queueClipChange(trill3, null, null, 1.0, 1, 1500, false); //popin
-		anim.queueClipChange(trill4, null, null, 1.0, 1, 2500, false); //popin
-		
-		/*
-		 * 	anim.queueClipChange(trill2, null, null, 1.0, 0, 161, false); //popin
-		anim.queueClipChange(trill3, null, null, 1.0, 0, 307, false); //popin
-		anim.queueClipChange(trill4, null, null, 1.0, 0, 495, false); //popin
-		 */
+		anim.queueClipChange(trill2, null, null, 1.0, 0, 500, false); //popin 161
+		anim.queueClipChange(trill3, null, null, 1.0, 0, 1500, false); //popin 307
+		anim.queueClipChange(trill4, null, null, 1.0, 0, 3000, false); //popin 495
+ 
 		
 		//fade em all out
 		anim.queueClipChange(trill1, null, null, 0.0, 500, 5000, true); //fadeout
 		anim.queueClipChange(trill2, null, null, 0.0, 500, 5000, true); //fadeout
 		anim.queueClipChange(trill3, null, null, 0.0, 500, 5000, true); //fadeout
 		anim.queueClipChange(trill4, null, null, 0.0, 500, 5000, true); //fadeout
+		
 		
 	}
 	
