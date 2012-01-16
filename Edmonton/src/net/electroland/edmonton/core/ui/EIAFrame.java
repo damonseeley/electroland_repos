00b225bca10e0e75356d19b9a7b6f57a40b18e02
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-16 02:47:26 +0000

    show buttson

--- Edmonton/src/net/electroland/edmonton/core/ui/EIAFrame.java
+++ Edmonton/src/net/electroland/edmonton/core/ui/EIAFrame.java
@@ -39,7 +39,7 @@ public class EIAFrame extends JFrame implements ActionListener {
 	private int panelWidth,panelHeight;
 
 	private JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b10;
-	private JButton startSeq,stopSeq;
+	private JButton startShow1,startShow2,stopSeq;
 	private DoubleJSlider js1;
 	private ArrayList<JButton> buttons;
 
@@ -62,52 +62,47 @@ public class EIAFrame extends JFrame implements ActionListener {
 
 		b2 = new JButton("Big Fill");
 		b2.setActionCommand("bigfill");
-		buttons.add(b2);
-		//b2.addActionListener(this);
+		//buttons.add(b2);
 
 		b1 = new JButton("Entry1");
 		b1.setActionCommand("entry1");
-		buttons.add(b1);
-		//b1.addActionListener(this);
+		//buttons.add(b1);
 
 		b3 = new JButton("Egg1");
 		b3.setActionCommand("egg1");
-		buttons.add(b3);
-		//b3.addActionListener(this);
+		//buttons.add(b3);
 
 		b6 = new JButton("Egg2");
 		b6.setActionCommand("egg2");
-		buttons.add(b6);
-		//b3.addActionListener(this);
+		//buttons.add(b6);
 
 		b4 = new JButton("Exit1");
 		b4.setActionCommand("exit1");
-		buttons.add(b4);
-		//b3.addActionListener(this);
+		//buttons.add(b4);
 
 		b5 = new JButton("Entry2");
 		b5.setActionCommand("entry2");
-		buttons.add(b5);
-		//b3.addActionListener(this);
+		//buttons.add(b5);
 
 		b7 = new JButton("Egg3");
 		b7.setActionCommand("egg3");
-		buttons.add(b7);
-		//b3.addActionListener(this);
+		//buttons.add(b7);
 
 		b8 = new JButton("Egg4");
 		b8.setActionCommand("egg4");
-		buttons.add(b8);
-		//b3.addActionListener(this);
+		//buttons.add(b8);		//b3.addActionListener(this);
 
 		b9 = new JButton("Exit2");
 		b9.setActionCommand("exit2");
-		buttons.add(b9);
-		//b3.addActionListener(this);
+		//buttons.add(b9);
 		
-		startSeq = new JButton("Start Sequence");
-		startSeq.setActionCommand("startSeq");
-		buttons.add(startSeq);
+		startShow1 = new JButton("Start Show1");
+		startShow1.setActionCommand("startShow1");
+		buttons.add(startShow1);		
+		
+		startShow2 = new JButton("Start Show2");
+		startShow2.setActionCommand("startShow2");
+		buttons.add(startShow2);
 
 		stopSeq = new JButton("Stop Active Sequence");
 		stopSeq.setActionCommand("stopSeq");
