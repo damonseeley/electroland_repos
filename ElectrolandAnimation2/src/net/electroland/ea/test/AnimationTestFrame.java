4ba2a73ebf81891d3d0737c5629d2f6927102e78
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-17 18:37:50 +0000

    support null backgrond color

--- ElectrolandAnimation2/src/net/electroland/ea/test/AnimationTestFrame.java
+++ ElectrolandAnimation2/src/net/electroland/ea/test/AnimationTestFrame.java
@@ -28,6 +28,7 @@ public class AnimationTestFrame extends JFrame{
 
         // create an AnimationManager
         AnimationManager anim = new AnimationManager("animation.properties");
+        anim.setStageColor(Color.WHITE);
         int w = anim.getStageDimensions().width / 2;
         int h = anim.getStageDimensions().height / 2;
 
@@ -41,10 +42,11 @@ public class AnimationTestFrame extends JFrame{
         Clip two = one.addClip(anim.getContent("fastImage"), 50, 50, 100, 100, .5);
         Clip wow = two.addClip(anim.getContent("fastImage"), 50, 50, 50, 50, .5);
 
-        // second copy of the nested stuff(woo hoo!)
+        // second copy  of the nested stuff(woo hoo!)
         Clip cop = (Clip)one.clone();
         anim.addClip(cop, 0, 0, w, h, 1.0);
-        
+        wow.delete();
+
         // red box clip
         SolidColorContent c = new SolidColorContent(Color.RED);
         Clip red = anim.addClip(c, 0, 0, w, h, 0);
@@ -53,14 +55,14 @@ public class AnimationTestFrame extends JFrame{
         Change change1 = new LinearChange().alphaTo(1.0).yTo(h);
         Change change2 = new LinearChange().alphaTo(.75).xBy(-10).yBy(-10);
         Change change3 = new LinearChange().alphaTo(0.0).scaleHeight(.5).scaleWidth(.5);
-        red.queueChange(change0, 2000).queueChange(change1, 1000).delay(500).queueChange(change2, 1000).queueChange(change3, 1000);
+        red.queueChange(change0, 0).queueChange(change1, 1000).delay(500).queueChange(change2, 1000).queueChange(change3, 1000);
         Change change4 = new LinearChange().alphaTo(0.25).xTo(0);
         Change change5 = new LinearChange().alphaTo(1.0).xTo(w).yTo(0);
-        Change change6 = new LinearChange().yTo(0);
+        Change change6 = new LinearChange().scaleHeight(1.25);
         // brokenChange change6 = new LinearChange().scaleHeight(.5);
 
         one.queueChange(change4, 0).delay(500).queueChange(change5, 750);
-        wow.delay(2000).queueChange(change6, 1000);
+        one.delay(2000).queueChange(change6, 1000);
 
         while (true){
             f.getGraphics().drawImage(anim.getStage(), 0, 0, f.getWidth(), f.getHeight(), null);
