4dde829bc973e65acf6b8f1c50f1b6f98f811167
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-17 08:38:01 +0000

    first checkin

---
+++ ElectrolandAnimation2/src/net/electroland/ea/AnimationManager.java
@@ -0,0 +1,93 @@
+package net.electroland.ea;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Map;
+
+import net.electroland.ea.content.SolidColorContent;
+import net.electroland.utils.ElectrolandProperties;
+import net.electroland.utils.OptionException;
+import net.electroland.utils.ParameterMap;
+
+import org.apache.log4j.Logger;
+
+public class AnimationManager implements DeletedClipListener {
+
+    private static Logger logger = Logger.getLogger(AnimationManager.class);
+    private int fps;
+    private Clip stage;
+    private Dimension stageDimensions;
+    private Map<String, Content>protoContent;
+    private Map<String, Collection<Clip>>taggedClips;
+
+    public int getFps() {
+        return fps;
+    }
+    public void setFps(int fps) {
+        this.fps = fps;
+    }
+    public Dimension getStageDimensions() {
+        return stageDimensions;
+    }
+    public void setStageDimensions(Dimension stageDimensions) {
+        this.stageDimensions = stageDimensions;
+    }
+    public AnimationManager(String propsName)
+    {
+        config(propsName);
+    }
+    public void config(String propsName)
+    {
+        logger.info("loading " + propsName);
+        ElectrolandProperties p = new ElectrolandProperties(propsName);
+
+        this.fps = p.getRequiredInt("settings", "global", "fps");
+
+        stageDimensions = new Dimension(p.getRequiredInt("settings", "global", "width"),
+                                p.getRequiredInt("settings", "global", "height"));
+        stage = new Clip(new SolidColorContent(Color.white), 0, 0, stageDimensions.width, stageDimensions.height, 1.0);
+        // clip
+        protoContent = new Hashtable<String,Content>();
+        Map<String, ParameterMap> contentParams = p.getObjects("content");
+        for (String s : contentParams.keySet()){
+            logger.info("loading content '" + s + "'");
+            ParameterMap universalParams = contentParams.get(s);
+
+            Map<String, ParameterMap> extendedParams = null;
+            try{
+                extendedParams = p.getObjects(s);
+            }catch(OptionException e){
+                // not a problem.  There might not be any extended params.
+            }
+
+            Content content = (Content)(universalParams.getRequiredClass("class"));
+            content.config(universalParams, extendedParams);
+            protoContent.put(s, content);
+        }
+    }
+    public Clip addClip(Content c, int x, int y, int width, int height, double alpha)
+    {
+        return stage.addClip(c, x,y,width,height,alpha);
+    }
+    public Content getContent(String contentId)
+    {
+        return (Content)protoContent.get(contentId).clone();
+    }
+    public Collection<Clip> getClip(String tag)
+    {
+        return taggedClips.get(tag);
+    }
+    public BufferedImage getStage()
+    {
+        stage.processChanges();
+        return stage.getImage(new BufferedImage(stageDimensions.width,
+                                                stageDimensions.height,
+                                                BufferedImage.TYPE_INT_ARGB));
+    }
+    @Override
+    public void deleted(Clip clip) {
+    }
+}
---
+++ ElectrolandAnimation2/src/net/electroland/ea/test/AnimationTestFrame.java
@@ -0,0 +1,64 @@
+package net.electroland.ea.test;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.util.Hashtable;
+
+import javax.swing.JFrame;
+
+import net.electroland.ea.AnimationManager;
+import net.electroland.ea.Change;
+import net.electroland.ea.Clip;
+import net.electroland.ea.changes.LinearChange;
+import net.electroland.ea.content.SolidColorContent;
+
+import org.apache.log4j.Logger;
+
+public class AnimationTestFrame extends JFrame{
+
+    private static Logger logger = Logger.getLogger(AnimationTestFrame.class);
+    private static final long serialVersionUID = 1L;
+    AnimationManager anim;
+
+    public static void main(String args[])
+    {
+        // create a context
+        Hashtable<String, Object> context = new Hashtable<String, Object>();
+        // just putting some bullshit object into the context.  normally, you'd
+        // put something useful in, like "sound_manager", soundManager
+        context.put("random_rectangle", new Rectangle());
+
+        // create an AnimationManager
+        AnimationManager anim = new AnimationManager("animation.properties");
+        int w = anim.getStageDimensions().width / 2;
+        int h = anim.getStageDimensions().height / 2;
+
+        AnimationTestFrame f = new AnimationTestFrame();
+        f.setSize(anim.getStageDimensions());
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Clip one = anim.addClip(anim.getContent("slowImage"), w, h, w, h, 1.0);
+        // nested clip
+        one.addClip(anim.getContent("fastImage"), 50, 50, 100, 100, .5);
+
+        SolidColorContent c = new SolidColorContent(Color.RED);
+        Clip two = anim.addClip(c, 0, 0, w, h, .75);
+        Change change = new LinearChange().toAlpha(0.0).toLeft(w);
+        two.queueChange(change, 2000);
+        Change change2 = new LinearChange().toAlpha(0.25).toLeft(0);
+        Change change3 = new LinearChange().toAlpha(1.0).toLeft(w);
+        Change change4 = new LinearChange().scaleHeight(.5);
+        
+        one.queueChange(change2, 1000).delay(500).queueChange(change3, 750).queueChange(change4, 1000).delete();
+
+        while (true){
+            f.getGraphics().drawImage(anim.getStage(), 0, 0, f.getWidth(), f.getHeight(), null);
+            try {
+                Thread.sleep(1000/anim.getFps());
+            } catch (InterruptedException e) {
+                System.exit(0);
+            }
+        }
+    }
+}
