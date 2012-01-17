d7e06a7cec50ad50d9742a99a8b7e379a7ebaeae
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-17 18:37:05 +0000

    clearer grouping

--- ElectrolandAnimation2/src/net/electroland/ea/QueuedChange.java
+++ ElectrolandAnimation2/src/net/electroland/ea/QueuedChange.java
@@ -4,11 +4,11 @@ public class QueuedChange {
     final public static int DELETE = -1;
     final public static int DELAY = 0;
     final public static int CHANGE = 1;
+    protected int type;
     protected Change change;
     protected long duration = 0;
     protected long delay = 0;
     protected long startTime;
     protected long endTime;
-    protected int type;
     protected boolean started = false;
 }
