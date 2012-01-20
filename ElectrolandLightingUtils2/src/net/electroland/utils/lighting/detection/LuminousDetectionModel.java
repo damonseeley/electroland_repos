5ac7c5993897446677f56ef8ec7f0fbbbcfd507d
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-20 02:56:19 +0000

    first check in

---
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/LuminousDetectionModel.java
@@ -0,0 +1,29 @@
+package net.electroland.utils.lighting.detection;
+
+import net.electroland.utils.lighting.DetectionModel;
+
+public class LuminousDetectionModel implements DetectionModel {
+
+    @Override
+    public byte getValue(int[] pixels) {
+        float total = 0;
+        for (int i = 0; i < pixels.length; i++){
+
+            // separate rgb vals
+            int a = (pixels[i] >> 24 ) & 0xFF;
+            int r = (pixels[i] >> 16 ) & 0xFF;
+            int g = (pixels[i] >>  8 ) & 0xFF;
+            int b = (pixels[i]       ) & 0xFF;
+
+            if (a == 0){
+                // add zero. eg., do nothing.
+            }else{
+                double da = (a / 255.0);
+                int gy = (int)(((r + g + b)/3.0) * da);
+
+                total += gy;
+            }
+        }
+        return (byte)(total / pixels.length);
+    }
+}
