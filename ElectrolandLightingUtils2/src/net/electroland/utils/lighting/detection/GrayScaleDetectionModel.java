d7afdc0e29c5d42ce2e76dc36585a63ce004773e
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-20 02:34:47 +0000

    don't ignore alpha

--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/GrayScaleDetectionModel.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/GrayScaleDetectionModel.java
@@ -13,9 +13,10 @@ public class GrayScaleDetectionModel implements DetectionModel {
             int r = (pixels[i] >> 16) & 0xFF;
             int g = (pixels[i] >> 8 & 0xFF);
             int b = (pixels[i] & 0xFF);
+            double a = (255.0 / (pixels[i] >> 24));
 
             // http://en.wikipedia.org/wiki/Grayscale
-            int gy = (int)((.3 * r) + (.59 * g) + (.11 * b));
+            int gy = (int)(((.3 * r) + (.59 * g) + (.11 * b))*a);
 
             total += gy;
         }
