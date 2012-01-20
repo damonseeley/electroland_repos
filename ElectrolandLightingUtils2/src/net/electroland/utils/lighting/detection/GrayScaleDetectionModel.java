3cbfba3a7e044e3b81577d66a0ce1f23fdfa817f
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-01-20 02:37:38 +0000

    avoid div by zero

--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/GrayScaleDetectionModel.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/detection/GrayScaleDetectionModel.java
@@ -13,12 +13,18 @@ public class GrayScaleDetectionModel implements DetectionModel {
             int r = (pixels[i] >> 16) & 0xFF;
             int g = (pixels[i] >> 8 & 0xFF);
             int b = (pixels[i] & 0xFF);
-            double a = (255.0 / (pixels[i] >> 24));
+            int a = (pixels[i] >> 24);
+            
+            if (a == 0){
+                // add zero. eg., do nothing.
+            }else{
+                double da = (255.0 / a);
 
-            // http://en.wikipedia.org/wiki/Grayscale
-            int gy = (int)(((.3 * r) + (.59 * g) + (.11 * b))*a);
-
-            total += gy;
+                // http://en.wikipedia.org/wiki/Grayscale
+                int gy = (int)(((.3 * r) + (.59 * g) + (.11 * b)) * da);
+                
+                total += gy;
+            }
         }
         return (byte)(total / pixels.length);
     }
