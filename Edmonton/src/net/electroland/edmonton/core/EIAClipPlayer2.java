5672061031307971a8845099d4115f2ad0a8930c
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-05-14 16:58:32 +0000

    several new animations in clipplayer2

--- Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer2.java
+++ Edmonton/src/net/electroland/edmonton/core/EIAClipPlayer2.java
@@ -29,24 +29,34 @@ public class EIAClipPlayer2 extends EIAClipPlayer {
     private int bottomBar = 8;
     private int barHeight = 2;
 
-    public void blip2(double x) {
+    public void vertSixFill(double x) {
 
-        boolean doBlip = true;
-        boolean doBlipSound = true;
+        //logger.info("localStabSmall@ " + x);
+        x = findNearestLight(x+lookAhead,true);
+        int barWidth = 3;
+        Content simpleClip2 = new SolidColorContent(Color.WHITE);
+        
+        int dOff = 150; //delay offset
+        int startX = (int)(x-barWidth/2);
+        
+        Clip top1 = live.addClip(simpleClip2, (int)(x-barWidth/2+barOff*0),topBar,barWidth,barHeight, 1.0); 
+        Clip bottom1 = live.addClip(simpleClip2, (int)(x-barWidth/2+barOff*0),bottomBar,barWidth,barHeight, 1.0, dOff*1);
+        Clip top2 = live.addClip(simpleClip2, (int)(x-barWidth/2+barOff*1),topBar,barWidth,barHeight, 1.0, dOff*2); 
+        Clip bottom2 = live.addClip(simpleClip2, (int)(x-barWidth/2+barOff*1),bottomBar,barWidth,barHeight, 1.0, dOff*3); 
+        Clip top3 = live.addClip(simpleClip2, (int)(x-barWidth/2+barOff*2),topBar,barWidth,barHeight, 1.0, dOff*4); 
+        Clip bottom3 = live.addClip(simpleClip2, (int)(x-barWidth/2+barOff*2),bottomBar,barWidth,barHeight, 1.0, dOff*5); 
 
-        if (doBlip){
-            x = findNearestLight(x+lookAhead,true);
-            //orig int barWidth = 3;
-            int barWidth = 3;
-            Content simpleClip2 = new SolidColorContent(Color.WHITE);
-            Clip blip1 = live.addClip(simpleClip2, (int)x-barWidth/2,0,barWidth,16, 0.7); //set the alpha to 0.5 to get 50% brightness on creation
-            blip1.delay(150).fadeOut(400).delete();
 
-            //now play a sound
-            if (doBlipSound) {
-                //sc.playSingleChannelBlind("marimba_mid_01.wav", x, 0.5f); // the firs test, kind of "crunch" sound
-            }
-        }
+        top1.delay(350).fadeOut(350).delete();
+        bottom1.delay(350).fadeOut(350).delete();
+        top2.delay(350).fadeOut(350).delete();
+        bottom2.delay(350).fadeOut(350).delete();
+        top3.delay(350).fadeOut(350).delete();
+        bottom3.delay(350).fadeOut(350).delete();
+        
+        //now play a sound!
+        //sc.playSingleChannelBlind("lumen_entrance7.wav", x, 0.4f);
+
     }
     
 	public void entryWavePM1(double x) {
@@ -65,22 +75,56 @@ public class EIAClipPlayer2 extends EIAClipPlayer {
 		//one.delay(4000).queueChange(change6, 1000);
 		//faintSparkle.delay(500).queueChange(lightFade, 4000).delay(12000).fadeOut(2000).delete();
 	}
+	
+	public void entryWavePM2(double x) {
+		//logger.info("bigWaveAll@ " + x);
+		x = findNearestLight(x+lookAhead,true);
+		
+		Content waveImage = anim.getContent("waveImage");
+
+		int waveWidth = 24;
+		Clip waveImageClip = live.addClip(waveImage, (int)x,0,waveWidth,16, 1.0); //add it as 32px wide at the end of the stage
+		waveImageClip.zIndex = -100; // sets to far background
+
+		//fadein, wait, fadeout
+		Change waveMove = new LinearChange().xTo(-waveWidth);
+		waveImageClip.queueChange(waveMove, 3000).delete();
+		//one.delay(4000).queueChange(change6, 1000);
+		//faintSparkle.delay(500).queueChange(lightFade, 4000).delay(12000).fadeOut(2000).delete();
+	}
     
     public void boomerang(double x){
     	logger.info("boomerang");
     	
-    	int barWidth = 5;
+    	int barWidth = 10;
     	int dist = 65;
     	
         x = findNearestLight(x+lookAhead,true);
         Content simpleClip2 = new SolidColorContent(Color.WHITE);
         
-        Clip boom = live.addClip(simpleClip2, (int)x-barWidth,topBar,barWidth,barHeight*3, 1.0);
+        Clip boomerang = live.addClip(simpleClip2, (int)x-barWidth,topBar,barWidth,barHeight*3, 1.0);
         
-        Change out = new LinearChange().xTo(x-dist).scaleWidth(5.0);
-        Change back = new LinearChange().xTo(x+lookAhead*2).scaleWidth(0.2);
+        Change out = new LinearChange().xTo(x-dist).scaleWidth(1.0);
+        Change back = new LinearChange().xTo(x+lookAhead*2).scaleWidth(1.0).alphaTo(0.1);
         
-        boom.queueChange(out, 800).queueChange(back,800).fadeOut(300).delete();
+        boomerang.queueChange(out, 500).queueChange(back,500).fadeOut(100).delete();
+    }
+    
+    public void grow(double x){
+    	logger.info("boomerang");
+    	
+    	int barWidth = 3;
+    	int dist = 65;
+    	
+        x = findNearestLight(x+lookAhead,true);
+        Content simpleClip2 = new SolidColorContent(Color.WHITE);
+        
+        Clip boomerang = live.addClip(simpleClip2, (int)x-barWidth,topBar,barWidth,barHeight*3, 1.0);
+        
+        Change out = new LinearChange().xTo(x-dist).widthTo(dist);
+        Change back = new LinearChange().xTo(x+lookAhead*1).widthTo(barWidth);
+        
+        boomerang.queueChange(out,600).queueChange(back,600).fadeOut(100).delete();
     }
     
     public void randomBars(double x){
@@ -166,13 +210,33 @@ public class EIAClipPlayer2 extends EIAClipPlayer {
         Clip topBlip = live.addClip(simpleClip2, (int)x-barWidth+2,topBar,barWidth,barHeight, 1.0); 
         Clip bottomBlip = live.addClip(simpleClip2, (int)x-barWidth+2,bottomBar,barWidth,barHeight, 1.0, 300); 
 
-        topBlip.delay(500).fadeOut(500).delete();
-        bottomBlip.delay(500).fadeOut(500).delete();
+        topBlip.delay(500).fadeOut(100).delete();
+        bottomBlip.delay(500).fadeOut(100).delete();
 
         //now play a sound!
         // something bigger here
         //sc.playSingleChannelBlind("lumen_entrance7.wav", x, 0.4f);
 
+    }
+    
+    public void blip2(double x) {
+
+        boolean doBlip = true;
+        boolean doBlipSound = true;
+
+        if (doBlip){
+            x = findNearestLight(x+lookAhead,true);
+            //orig int barWidth = 3;
+            int barWidth = 3;
+            Content simpleClip2 = new SolidColorContent(Color.WHITE);
+            Clip blip1 = live.addClip(simpleClip2, (int)x-barWidth/2,0,barWidth,16, 0.8); //set the alpha to 0.5 to get 50% brightness on creation
+            blip1.delay(200).fadeOut(400).delete();
+
+            //now play a sound
+            if (doBlipSound) {
+                //sc.playSingleChannelBlind("marimba_mid_01.wav", x, 0.5f); // the firs test, kind of "crunch" sound
+            }
+        }
     }
     
 
