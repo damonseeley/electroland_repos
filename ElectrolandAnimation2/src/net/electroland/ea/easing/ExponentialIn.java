10a174fd87fa67b2132aeb5d8d1e7d6df2eaa684
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-07-12 16:44:13 +0000

    Exponential easing added.

---
+++ ElectrolandAnimation2/src/net/electroland/ea/easing/ExponentialIn.java
@@ -0,0 +1,9 @@
+package net.electroland.ea.easing;
+
+public class ExponentialIn extends EasingFunction {
+
+    @Override
+    public double valueAt(double percentComplete, double start, double finish) {
+        return (finish - start) * Math.pow(2, 10 * (percentComplete - 1)) + start;
+    }
+}
