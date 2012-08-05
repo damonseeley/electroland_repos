b60d4b2b44cbbc8aa88be75a49e2b7bcebf8b9c5
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-08-05 22:57:59 +0000

    photos.xml

--- Gateway/src/net/electroland/gateway/mediamap/PhotosHandler.java
+++ Gateway/src/net/electroland/gateway/mediamap/PhotosHandler.java
@@ -4,13 +4,16 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class NFOHandler extends DefaultHandler {
+public class PhotosHandler extends DefaultHandler {
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
-        if ("Info".equalsIgnoreCase(qName) && "name".equalsIgnoreCase(attributes.getValue("name"))){
-            System.out.println(attributes.getValue("value"));
+        if ("entry".equalsIgnoreCase(qName)){
+            String first = attributes.getValue("first");
+            String last = attributes.getValue("last");
+            String disambiguator = attributes.getValue("disambiguator");
+            String filename = attributes.getValue("filname");
         }
     }
 }
