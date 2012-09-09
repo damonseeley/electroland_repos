aeb6e19baf8ef58c554b89bf45cd9ba03aa0a8d4
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-09 19:55:30 +0000

    enabled/disable detectors and/or canvas

--- Gotham/src/net/electroland/gotham/core/ui/DisplayControlBar.java
+++ Gotham/src/net/electroland/gotham/core/ui/DisplayControlBar.java
@@ -8,6 +8,8 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import net.electroland.utils.lighting.DetectionModel;
 import net.electroland.utils.lighting.canvas.ELUPApplet;
@@ -15,7 +17,7 @@ import net.electroland.utils.lighting.detection.BlueDetectionModel;
 import net.electroland.utils.lighting.detection.GreenDetectionModel;
 import net.electroland.utils.lighting.detection.RedDetectionModel;
 
-public class DisplayControlBar extends JPanel implements ActionListener{
+public class DisplayControlBar extends JPanel implements ActionListener, ChangeListener{
 
     private static final long serialVersionUID = -626583748057983309L;
     private JCheckBox includeRendering, includeDectectors, includePresenceGrid;
@@ -25,7 +27,7 @@ public class DisplayControlBar extends JPanel implements ActionListener{
     public DisplayControlBar(){
         includeRendering    = new JCheckBox("canvas", true);
         includeDectectors   = new JCheckBox("detectors", true);
-        includePresenceGrid = new JCheckBox("presence grid", true);
+        includePresenceGrid = new JCheckBox("presence grid", true); includePresenceGrid.setEnabled(false);
         detectorColors            = new JComboBox(new DetectionModel[]{null, new RedDetectionModel(), new BlueDetectionModel(), new GreenDetectionModel()});
         this.add(new JLabel(" Display:"));
         this.add(detectorColors);
@@ -34,6 +36,8 @@ public class DisplayControlBar extends JPanel implements ActionListener{
         this.add(includePresenceGrid);
         this.listeners = new Vector<ELUPApplet>();
         detectorColors.addActionListener(this);
+        includeRendering.addChangeListener(this);
+        includeDectectors.addChangeListener(this);
     }
 
     public void addListener(ELUPApplet applet){
@@ -65,6 +69,20 @@ public class DisplayControlBar extends JPanel implements ActionListener{
                 }else{
                     a.showAll();
                 }
+            }
+        }
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent evt) {
+        if (evt.getSource() == includeRendering) {
+            for (ELUPApplet a : listeners){
+                a.setShowRendering(includeRendering.isSelected());
+            }
+        } else if (evt.getSource() == includeDectectors) {
+            for (ELUPApplet a : listeners){
+                a.setShowDetectors(includeDectectors.isSelected());
+                detectorColors.setEnabled(includeDectectors.isSelected());
             }
         }
     }
