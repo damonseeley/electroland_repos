f7e421e5f60beeef8a534e001b42de75670cdcc9
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-09 15:02:35 +0000

    testing some color cycling

--- Gotham/src/net/electroland/gotham/processing/East.java
+++ Gotham/src/net/electroland/gotham/processing/East.java
@@ -1,11 +1,13 @@
 package net.electroland.gotham.processing;
 
+import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.geom.Point2D;
 import java.util.logging.Logger;
 
 import net.electroland.ea.EasingFunction;
 import net.electroland.ea.easing.QuinticInOut;
+import net.electroland.ea.easing.SinusoidalInOut;
 import net.electroland.utils.lighting.canvas.ELUPApplet;
 
 public class East extends ELUPApplet {
@@ -19,22 +21,25 @@ public class East extends ELUPApplet {
     private boolean isGrowing = true;
     private long startTime = -1;
     private float smallRadius, largeRadius;
+    private Color nextColor, currentColor = Color.white;
 
-    // the period that the circle expands/contrats
-    final static long DURATION_MILLIS = 1000;
+    // the period that the circle expands/contracts
+    final static long DURATION_MILLIS = 3000;
     // the easing function for expanding/contracting
     private EasingFunction ef = new QuinticInOut();
+    private EasingFunction cf = new SinusoidalInOut();
 
     @Override
     public void setup() {
         // syncArea is the area of the screen that will be synced to the lights.
         syncArea = this.getSyncArea();
-        // our circle's center will be the middel of the sync area.
+        // our square's center will be the middle of the sync area.
         center = new Point2D.Double(syncArea.x + .5 * syncArea.width, 
                                     syncArea.y + .5 * syncArea.height);
         // it will beat between these radii
         smallRadius = .05f * syncArea.height;
         largeRadius = .8f * syncArea.height;
+        nextColor = randomColor();
     }
 
     @Override
@@ -49,6 +54,8 @@ public class East extends ELUPApplet {
         if (System.currentTimeMillis() - startTime > DURATION_MILLIS){
             isGrowing = !isGrowing;
             startTime = System.currentTimeMillis();
+            currentColor = nextColor;
+            nextColor = randomColor();
         }
 
         // calculate the current radius based on time.
@@ -61,8 +68,19 @@ public class East extends ELUPApplet {
         }
 
         // paint a circle
-        color(255);
-        fill(255);
+        color(cf.valueAt(percentComplete, currentColor.getRed(), nextColor.getRed()), 
+              cf.valueAt(percentComplete, currentColor.getGreen(), nextColor.getGreen()), 
+              cf.valueAt(percentComplete, currentColor.getBlue(), nextColor.getBlue()));
+
+        fill(cf.valueAt(percentComplete, currentColor.getRed(), nextColor.getRed()), 
+                cf.valueAt(percentComplete, currentColor.getGreen(), nextColor.getGreen()), 
+                cf.valueAt(percentComplete, currentColor.getBlue(), nextColor.getBlue()));
+
         rect((float)center.getX() - .5f * radius, (float)center.getY() - .5f * radius, radius, radius);
     }
+
+    public Color randomColor() {
+        return new Color((int)(Math.random() * 255), (int)(Math.random() * 255), (int)(Math.random() * 255));
+    }
+
 }
