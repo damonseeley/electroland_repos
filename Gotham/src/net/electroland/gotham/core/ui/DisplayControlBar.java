c99a1a7591ced0389bd5d9b2e9a77ef5a6499ce5
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-14 23:42:29 +0000

    native libs

--- Gotham/src/net/electroland/gotham/core/ui/DisplayControlBar.java
+++ Gotham/src/net/electroland/gotham/core/ui/DisplayControlBar.java
@@ -21,6 +21,7 @@ import net.electroland.utils.lighting.canvas.ELUPApplet;
 import net.electroland.utils.lighting.detection.BlueDetectionModel;
 import net.electroland.utils.lighting.detection.GreenDetectionModel;
 import net.electroland.utils.lighting.detection.RedDetectionModel;
+import processing.core.PApplet;
 
 public class DisplayControlBar extends JPanel implements ActionListener, ChangeListener{
 
