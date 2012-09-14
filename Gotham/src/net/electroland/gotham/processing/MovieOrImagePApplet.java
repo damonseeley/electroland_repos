a1cd33c165c491960a25ace1840e310719075d2b
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-14 07:07:49 +0000

    movies

--- Gotham/src/net/electroland/gotham/processing/MovieOrImagePApplet.java
+++ Gotham/src/net/electroland/gotham/processing/MovieOrImagePApplet.java
@@ -3,34 +3,36 @@ package net.electroland.gotham.processing;
 import java.io.File;
 
 import processing.core.PImage;
-
+import processing.video.*;
 
 public class MovieOrImagePApplet extends GothamPApplet {
 
     private static final long serialVersionUID = 1L;
     private PImage image;
-//    private Movie movie;
+    private Movie movie;
 
     @Override
     public void drawELUContent() {
         // based on the file name either draw an Image or render a movie.
         if (image != null){
             this.image(image, 0, 0, this.getSyncArea().width, this.getSyncArea().height);
-        }// else if (movie != null) {
-            // render movie here
-        //}
+        } else if (movie != null) {
+            this.image(movie, 0, 0, this.getSyncArea().width, this.getSyncArea().height);
+        }
     }
 
     @Override
     public void fileReceived(File file){
         String name = file.getAbsolutePath();
         System.out.println("loading " + name);
-        if (name.toLowerCase().endsWith(".mov")){
+        if (name.toLowerCase().endsWith(".mov") || name.toLowerCase().endsWith(".mp4")){
+            movie = new Movie(this, name);
+            movie.play();
             image = null;
-            // load movie here
+            System.out.println("loading movie " + movie);
         } else if (name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".png")) {
-            // movie = null;
             image = loadImage(file.getAbsolutePath());
+            movie = null;
         }
     }
 }
