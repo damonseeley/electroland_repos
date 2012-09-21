dff7318a10783531d75ccf1be028ca9102e11719
mkontopo <mkontopo@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-21 06:14:53 +0000

    Updated East_BlurTest to use internal animation libs instead of Ani Removed Ani from libraries and from build path Applet loads with stripes on screen already (uniform widths) Remaining stripes are of random widths now.

--- Gotham/src/net/electroland/gotham/processing/assets/Stripe.java
+++ Gotham/src/net/electroland/gotham/processing/assets/Stripe.java
@@ -1,39 +1,66 @@
 package net.electroland.gotham.processing.assets;
-import net.electroland.gotham.processing.East_BlurTest;
 import java.awt.Dimension;
 import processing.core.PApplet;
-import de.looksgood.ani.Ani;
+import net.electroland.ea.EasingFunction;
+import net.electroland.ea.easing.Linear;
+import net.electroland.gotham.processing.East_BlurTest;
 
 public class Stripe {
-	public float xpos;
-	private float h; //the hue of this Stripe
-	Ani vel;
-	private float target;
+
+	private EasingFunction ef;
+	public float begin;
+	public static float target;
+	private float prevMillis;
+	private float percentComplete;
+	public float timeAcross; // Number of seconds needed to traverse the sync area
+	
 	PApplet p;
-	public static float w = 100; // with of a stripe
-	public static float hw = w * 0.5f; // half width
-	private static boolean highlight = true;
+	private float xpos;
+	private float h; //the hue of this Stripe
+	private float w; // with of a stripe
+	private float hw; // half width
+	public float dist;
 
 	public Stripe(PApplet p, Dimension d) {
 		this.p = p;
-		xpos = -hw; //start offscreen
-		target = d.width + hw; //end offscreen
+		ef = new Linear();
+		w = 30 + (float)Math.random()*120;
+		hw = w * 0.5f;
+		begin = -hw; //start offscreen
+		target = d.width + 150; //end offscreen
 		h = p.random(360);
-		vel = new Ani(this, East_BlurTest.rate, "xpos", target, Ani.LINEAR); //the tween
+		prevMillis = p.millis(); //Start our timer
+		dist = target - begin;
+		timeAcross = East_BlurTest.randomSpeeds ? 30.0f + (float)Math.random()*90 : 60.0f * (dist/d.width);
 	}
-
-	public static void setMouseHighlight(boolean b){
-		highlight = b ? true : false;
+	//Overloaded Constructor. Only used to fill the screen with Stripe on startup.
+	public Stripe(PApplet p, Dimension d, int inc){
+		this(p, d);
+		w = d.width/6;
+		hw = w * 0.5f;
+		begin = (-hw) + inc * w;
+		dist = target - begin;
+		timeAcross = East_BlurTest.randomSpeeds ? 30.0f + (float)Math.random()*90 : 60.0f * (dist/d.width);
 	}
+	
 	public void run() {
-		float s = highlight ? PApplet.constrain(mouseDist(), 0, 90) : 90;
-		p.fill(p.color(h, s, 90));
-		p.rect(xpos, p.height / 2, w, p.height);	
+		float elapsed = p.millis()-prevMillis;
+		if(elapsed < timeAcross*1000)
+			percentComplete = elapsed / (timeAcross*1000);
+		
+		xpos = ef.valueAt(percentComplete, begin, target);
+		p.fill(p.color(h, 90, 90), 200);
+		p.rect(xpos, p.height / 2, w, p.height+50);	
 	}
-	public boolean kill() {
-		return xpos >= target;
+	public boolean isOffScreen() {
+		//It doesn't exactly reach the  final target position, dpending on the tween duration vs. framerate.
+		return xpos >= target-50; 
 	}
-	private float mouseDist() {
-		return Math.abs(p.mouseX-xpos);
+	//Only used if we want different sized Stripes to file in perfectly one after the other.
+	public float getSpawnRate() {
+		// (this stripe's width : total distance to travel) * time to get to the
+		// other side
+		return (w / dist) * (timeAcross * 1000);
 	}
+	
 }
