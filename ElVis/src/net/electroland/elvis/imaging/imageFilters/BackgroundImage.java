4eddeeed809c9579e1a6ab09728ed0904b526dfb
emendelowitz <emendelowitz@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-21 23:14:30 +0000

    updated background model to use 32 bit float instead of 8 bit unsigned int.  COMPLETALLY UNTESTED

--- ElVis/src/net/electroland/elvis/imaging/imageFilters/BackgroundImage.java
+++ ElVis/src/net/electroland/elvis/imaging/imageFilters/BackgroundImage.java
@@ -1,19 +1,22 @@
 package net.electroland.elvis.imaging.imageFilters;
 
+import static com.googlecode.javacv.cpp.opencv_core.IPL_DEPTH_32F;
 import static com.googlecode.javacv.cpp.opencv_core.cvAddWeighted;
+import static com.googlecode.javacv.cpp.opencv_core.cvCopy;
 import static com.googlecode.javacv.cpp.opencv_core.cvCreateImage;
 import static com.googlecode.javacv.cpp.opencv_core.cvGetSize;
-import static com.googlecode.javacv.cpp.opencv_core.cvCopy;
+import static com.googlecode.javacv.cpp.opencv_highgui.cvConvertImage;
 import net.electroland.elvis.util.ElProps;
 import net.electroland.elvis.util.parameters.DoubleParameter;
 
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 
-
 public class BackgroundImage extends Filter {
 
-
+	IplImage internalModel; 
+	IplImage internalSrc; 
+	
 	int initialFrameSkip = 0; // some cameras have a lot of noise at startup (or fade in light iSight)  don't want to use bad background to start
 
 	public DoubleParameter adaptionParameter;
@@ -69,13 +72,19 @@ public class BackgroundImage extends Filter {
 		if(initialFrameSkip-- > 0) return null;
 		if(dst == null)  {
 			dst = cvCreateImage(cvGetSize(bi), bi.depth(), bi.nChannels());
+			internalSrc = cvCreateImage(cvGetSize(bi), IPL_DEPTH_32F, bi.nChannels());
+			internalModel = cvCreateImage(cvGetSize(bi), IPL_DEPTH_32F, bi.nChannels());
+			cvConvertImage(bi, internalModel,0);
 			cvCopy(bi, dst);
 			return dst;
 		} else { 
 			double adaptation =adaptionParameter.getDoubleValue();
 			if(adaptation == 0) 	return dst; // don't bother processing just use static background
 			double memory = 1.0 - adaptation;
-			cvAddWeighted(dst, memory, bi, adaptation, 0, dst);
+			cvConvertImage(bi, internalSrc,0);
+			cvAddWeighted(internalModel, memory, internalSrc, adaptation, 0, internalModel);
+			cvConvertImage(internalModel, dst,0);
+			
 
 		}
 		return dst;
