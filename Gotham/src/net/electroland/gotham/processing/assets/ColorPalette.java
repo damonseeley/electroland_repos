7484feb3eeeec67af7e9f909734559480c0cef76
mkontopo <mkontopo@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-25 21:27:50 +0000

    Added a menu to switch the color palette while the applet is running. To change swatch .jpg's, simply put the .jpg in "depends/images" and update the props file

--- Gotham/src/net/electroland/gotham/processing/assets/ColorPalette.java
+++ Gotham/src/net/electroland/gotham/processing/assets/ColorPalette.java
@@ -3,30 +3,36 @@ package net.electroland.gotham.processing.assets;
 import java.util.List;
 import net.electroland.gotham.core.GothamConductor;
 import net.electroland.utils.ElectrolandProperties;
-import net.electroland.gotham.processing.EastBlurTest;
 import processing.core.PImage;
 import processing.core.PApplet;
 
 
 public class ColorPalette {
 	private static ElectrolandProperties props = GothamConductor.props;
+	PApplet p;
 	private int[] colors;
+	private static int numSwatches;
 	
 	public ColorPalette(PApplet p) {
+		this.p = p;
+	}
+
+	public int[] getPalette(int n) {
 		List<String> fileNames = props.getOptionalList("wall", "East", "colorPalettes");
+		numSwatches = fileNames.size();
 		colors = new int[15]; //A swatch has 15 colors
 		int num = 0;
-		String s = fileNames.get(EastBlurTest.selector);
-		System.out.println(s);
+		String s = fileNames.get(n);
 		PImage thisImage = p.loadImage("/../Gotham/depends/images/"+s);
 
 		for (int i = 10; i < thisImage.width; i += 20) {
 			colors[num] = thisImage.get(i, thisImage.height / 2);
 			num++;
 		}
-	}
-
-	public int[] getPalette() {
+		
 		return colors;
+	}
+	public static int getNumSwatches(){
+		return numSwatches;
 	}
 }
