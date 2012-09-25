234dda55949f98e337f888bac46dd7381c62443b
mkontopo <mkontopo@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-25 21:38:58 +0000

    Updated to accommodate swatches of varying sizes. Just make sure the swatch jpg width is in increments of 20. Added a swatch of width 100 to test.

--- Gotham/src/net/electroland/gotham/processing/assets/ColorPalette.java
+++ Gotham/src/net/electroland/gotham/processing/assets/ColorPalette.java
@@ -12,6 +12,7 @@ public class ColorPalette {
 	PApplet p;
 	private int[] colors;
 	private static int numSwatches;
+	private static int numColors;
 	
 	public ColorPalette(PApplet p) {
 		this.p = p;
@@ -20,11 +21,12 @@ public class ColorPalette {
 	public int[] getPalette(int n) {
 		List<String> fileNames = props.getOptionalList("wall", "East", "colorPalettes");
 		numSwatches = fileNames.size();
-		colors = new int[15]; //A swatch has 15 colors
+		
 		int num = 0;
 		String s = fileNames.get(n);
 		PImage thisImage = p.loadImage("/../Gotham/depends/images/"+s);
-
+		numColors = (int)(thisImage.width/20);
+		colors = new int[numColors]; //Divide swatch with by 20 to get how many colors are int he file. Each square is 20x20 pixels
 		for (int i = 10; i < thisImage.width; i += 20) {
 			colors[num] = thisImage.get(i, thisImage.height / 2);
 			num++;
@@ -32,6 +34,13 @@ public class ColorPalette {
 		
 		return colors;
 	}
+	
+	//Returns the number of colors in the current swatch
+	public static int getNumColors(){
+		return numColors;
+	}
+	
+	//Returns the number of jpgs in the props file
 	public static int getNumSwatches(){
 		return numSwatches;
 	}
