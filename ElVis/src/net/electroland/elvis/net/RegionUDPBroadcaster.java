0efded72d605f9de5c9bdbc2a5756534c51b0e2b
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-26 23:09:32 +0000

    spelling

---
+++ ElVis/src/net/electroland/elvis/net/RegionUDPBroadcaster.java
@@ -0,0 +1,31 @@
+package net.electroland.elvis.net;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Vector;
+
+import net.electroland.elvis.regions.PolyRegion;
+import net.electroland.elvis.regions.PolyRegionResults;
+
+
+public class RegionUDPBroadcaster extends UDPBroadcaster {
+
+
+	public RegionUDPBroadcaster(int port) throws SocketException, UnknownHostException {
+		this("localhost", port);
+	}
+	public RegionUDPBroadcaster(String address, int port) throws SocketException, UnknownHostException {
+		super(address, port);
+	}
+	
+	public void updateRegions(Vector<PolyRegion> regions) {
+		if((regions == null) || (regions.isEmpty())) return;
+		StringAppender.TrivialAppender restuls = new StringAppender.TrivialAppender();
+		restuls.setString(PolyRegionResults.buildString(regions));
+		this.send(restuls);
+		
+	}
+	
+	
+
+}
