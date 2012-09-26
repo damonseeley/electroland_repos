d7b50aff3f1bab9d5c5b1ff2a8cfd10e90e15973
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-26 22:35:45 +0000

    removed deprecation warning.

--- Gotham/src/net/electroland/gotham/processing/DoNothing.java
+++ Gotham/src/net/electroland/gotham/processing/DoNothing.java
@@ -1,17 +1,8 @@
 package net.electroland.gotham.processing;
 
-import java.awt.Dimension;
 import java.util.ArrayList;
-import net.electroland.gotham.core.GothamConductor;
-import net.electroland.gotham.processing.assets.Stripe;
-import net.electroland.gotham.processing.assets.FastBlur;
-import net.electroland.utils.ElectrolandProperties;
 
-import controlP5.ControlP5;
-import controlP5.ControlWindow;
-import controlP5.Controller;
-import controlP5.Knob;
-import controlP5.Toggle;
+import net.electroland.gotham.processing.assets.Stripe;
 
 import org.apache.log4j.Logger;
 
@@ -21,7 +12,6 @@ public class DoNothing extends GothamPApplet {
 	public static float scaler; // A scaler value to upsample or downsample the duration of the tween	
 	private static final long serialVersionUID = 1L;
 	static Logger logger = Logger.getLogger(GothamPApplet.class);
-	private Dimension syncArea;
 
 	public float scalerAmt;
 	public float blurAmt;
@@ -32,7 +22,6 @@ public class DoNothing extends GothamPApplet {
 
 	@Override
 	public void setup() {
-		syncArea = this.getSyncArea();
 		
 	}
 
