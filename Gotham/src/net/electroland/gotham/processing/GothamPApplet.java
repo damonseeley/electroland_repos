f4b28f03ea6c020725188aa06c3edc1a048452ca
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-26 23:13:49 +0000

    ignore key events.

--- Gotham/src/net/electroland/gotham/processing/GothamPApplet.java
+++ Gotham/src/net/electroland/gotham/processing/GothamPApplet.java
@@ -8,10 +8,20 @@ import net.electroland.gotham.core.Person;
 import net.electroland.gotham.core.Room;
 import net.electroland.utils.lighting.canvas.ELUPApplet;
 
+import org.apache.log4j.Logger;
+
+import processing.event.KeyEvent;
+
 abstract public class GothamPApplet extends ELUPApplet {
 
+    static Logger logger = Logger.getLogger(GothamPApplet.class);
     private static final long serialVersionUID = 8991292715035010606L;
     private Room room;
+
+    @Override
+    public void handleKeyEvent(KeyEvent e){
+        logger.debug("Ignoring key events in GothamPApplet.");
+    }
 
     public void handle(GridData d) {
         synchronized(room){
