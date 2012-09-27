3430656c7178a7302918d491aed75fe2ce2f8ee1
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-27 16:54:57 +0000

    basic tests

---
+++ Gotham/src/net/electroland/gotham/processing/StripeIxTest.java
@@ -0,0 +1,74 @@
+package net.electroland.gotham.processing;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+//import net.electroland.gotham.core.GothamConductor;
+import net.electroland.gotham.processing.assets.*;
+//import net.electroland.utils.ElectrolandProperties;
+import org.apache.log4j.Logger;
+import controlP5.ControlEvent;
+
+public class StripeIxTest extends GothamPApplet {
+
+	private static final long serialVersionUID = 1L;
+	static Logger logger = Logger.getLogger(GothamPApplet.class);
+	private Dimension syncArea;
+	//private int nStripes; // Num Stripes that begin on screen.
+	
+	private int selector = 0; // Which color swatch from the props file to use.
+	
+	private long interval = 3000;
+	private Timer timer;
+
+	ArrayList<StripeSimple> stripes;
+
+	ColorPalette cp;
+
+	@Override
+	public void setup() {
+		System.out.println("hello");
+		syncArea = this.getSyncArea();
+		colorMode(HSB, 360, 100, 100);
+
+		stripes = new ArrayList<StripeSimple>();
+
+		//nStripes = props.getOptionalInt("wall", "East", "initialStripes");
+
+		cp = new ColorPalette(this);
+		cp.createNewPalette(0);
+		timer = new Timer(interval);
+		timer.start();
+
+		stripes.add(new StripeSimple(this, syncArea, (int)Math.random()*this.syncArea.width));
+	}
+
+	@Override
+	public void drawELUContent() {
+
+		background(color(0, 0, 0));
+
+		// Handle Stripes
+		for (int i = stripes.size() - 1; i >= 0; i--) {
+			StripeSimple s = stripes.get(i);
+			s.run();
+			if(i!=0)
+			
+			if (s.isOffScreen())
+				stripes.remove(i);
+		}
+
+		// Timing Controls for each new Stripe
+		if (timer.isFinished()) {
+			int xloc = (int)(Math.random()*this.syncArea.width);
+			logger.info("creating new stripe at: " + xloc + " where syncarea.width = " + this.syncArea.width);
+			stripes.add(new StripeSimple(this, syncArea, xloc));
+			timer.reset((long)(1000.0 + (Math.random()*5))); //Randomize the timer each time. Result: stripes of diff widths
+		}
+	
+
+		loadPixels();
+		updatePixels();
+	}
+
+	
+}
---
+++ Gotham/src/net/electroland/gotham/processing/assets/StripeSimple.java
@@ -0,0 +1,104 @@
+package net.electroland.gotham.processing.assets;
+
+import java.awt.Dimension;
+import processing.core.PApplet;
+import net.electroland.ea.EasingFunction;
+import net.electroland.ea.easing.Linear;
+import net.electroland.gotham.core.GothamConductor;
+import net.electroland.gotham.processing.GothamPApplet;
+import net.electroland.utils.ElectrolandProperties;
+import org.apache.log4j.Logger;
+
+public class StripeSimple {
+
+	static Logger logger = Logger.getLogger(GothamPApplet.class);
+	private ElectrolandProperties props = GothamConductor.props;
+	private Dimension d;
+
+	public static float scalerAmt;
+	public static boolean randomSpeeds;
+	public static float rScaler;
+	public static float spawnScaler;
+	
+	private EasingFunction ef;
+	public float begin;
+	public float target;
+	private float prevMillis;
+	private float percentComplete;
+	private float baseTime;
+	public float timeAcross; // Number of seconds needed to traverse the sync
+								// area, based off defaultTime
+
+	PApplet p;
+	private float xpos;
+	int stripeColor;
+	private float w; // with of a stripe
+	private float hw; // half width
+	public float dist;
+	private int pDirection, direction;
+	public static boolean changing = false;
+
+	public StripeSimple(PApplet p, Dimension d, int x) {
+		this.p = p;
+		this.d = d;
+		xpos = x;
+		ef = new Linear();
+		w = (30 + (float) Math.random()*120);
+		hw = w * 0.5f;
+
+		stripeColor = ColorPalette.getRandomColor();
+	}
+
+
+	public void run() {
+
+		p.rectMode(PApplet.CENTER);
+		p.fill(stripeColor);
+		p.rect(xpos, p.height / 2, w, p.height + 50);
+
+	}
+
+	public boolean isOffScreen() {
+		if (scalerAmt > 0)
+			return xpos >= target;
+		else
+			return xpos <= target;
+	}
+
+	// Only used if we want different sized Stripes to file in perfectly one
+	// after the other.
+	public float getSpawnRate() {
+		// (this stripe's width : total distance to travel) * time to get to the
+		// other side
+		return ((w / dist) * (timeAcross * 1000));
+	}
+	
+	private void resetDirection(int dir) {
+		begin = xpos;
+		percentComplete = 0;
+		if (dir > 0) {
+			target = d.width + 200;
+		} else {
+			target = -100 - hw;
+		}
+		dist = Math.abs(target - begin);
+		timeAcross = randomSpeeds ? (baseTime + p.random(
+				-rScaler, rScaler))
+				* (dist / d.width) : baseTime * (dist / d.width);
+		
+	}
+	
+	public static void setScalerAmt(float amt){
+		scalerAmt = amt;
+	}
+	public static void setUseRandomSpeeds(float rs){
+		randomSpeeds = rs > 0 ? true : false;
+	}
+	public static void setRandomScaler(float rscaler){
+		rScaler = rscaler;
+	}
+	public static void setSpawnScaler(float ss){
+		spawnScaler = ss;
+	}
+	
+}
