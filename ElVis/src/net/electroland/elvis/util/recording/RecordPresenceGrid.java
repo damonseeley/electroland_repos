c34b17ec713a3e0981a93e571fc16656c018e98f
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-28 05:17:30 +0000

    a little cleaner

--- ElVis/src/net/electroland/elvis/util/recording/RecordPresenceGrid.java
+++ ElVis/src/net/electroland/elvis/util/recording/RecordPresenceGrid.java
@@ -11,48 +11,29 @@ import net.electroland.elvis.net.PresenceGridUDPClient;
 
 public class RecordPresenceGrid extends PresenceGridUDPClient {
 
-    private BufferedWriter output;
-    private StringBuilder buffer;
+    private FileRecorder recorder;
+
+    public RecordPresenceGrid(int port) throws SocketException {
+        super(port);
+    }
 
     public static void main(String args[]) throws Exception {
         if (args.length != 2){
             System.out.println("Usage: RecordPresenceGrid [port] [filename]");
         }else{
             RecordPresenceGrid r = new RecordPresenceGrid(new Integer(args[0]));
-            r.output = new BufferedWriter(new FileWriter(new File(args[1])));
-            r.buffer = new StringBuilder();
+            r.recorder = new FileRecorder(args[1]);
         }
-    }
-
-    public RecordPresenceGrid(int port) throws SocketException {
-        super(port);
     }
 
     @Override
     public void handle(GridData t) {
-        if (output != null){
-
-            buffer.setLength(0);
-            buffer.append(System.currentTimeMillis());
-            buffer.append(':');
-            t.buildString(buffer);
-
+        if (recorder != null){
             try {
-
-                output.write(buffer.toString());
-                output.newLine();
-                output.flush();
-
+                recorder.recored(t);
             } catch (IOException e) {
                 e.printStackTrace();
-                System.exit(-1);
-            }finally{
-
-                try {
-                    output.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                recorder.close();
             }
         }
     }
