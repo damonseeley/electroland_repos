1cac46e477162f67cae4ef5348238165f2ece322
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-09-28 05:17:30 +0000

    a little cleaner

---
+++ ElVis/src/net/electroland/elvis/util/recording/FileRecorder.java
@@ -0,0 +1,41 @@
+package net.electroland.elvis.util.recording;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import net.electroland.elvis.net.StringAppender;
+
+public class FileRecorder {
+
+    private BufferedWriter output;
+    private StringBuilder buffer;
+
+    public FileRecorder(String filename) throws IOException {
+        output = new BufferedWriter(new FileWriter(new File(filename)));
+        buffer = new StringBuilder();
+    }
+
+    public void recored(StringAppender a) throws IOException {
+        if (output != null){
+
+            buffer.setLength(0);
+            buffer.append(System.currentTimeMillis());
+            buffer.append(':');
+            a.buildString(buffer);
+
+                output.write(buffer.toString());
+                output.newLine();
+                output.flush();
+        }
+    }
+
+    public void close() {
+        try {
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- ElVis/src/net/electroland/elvis/util/recording/RecordTracks.java
+++ ElVis/src/net/electroland/elvis/util/recording/RecordTracks.java
@@ -8,52 +8,34 @@ import java.net.SocketException;
 
 import net.electroland.elvis.blobtracking.BaseTrack;
 import net.electroland.elvis.blobtracking.TrackResults;
+import net.electroland.elvis.net.GridData;
 import net.electroland.elvis.net.TrackUDPClient;
 
 public class RecordTracks extends TrackUDPClient {
 
-    private BufferedWriter output;
-    private StringBuilder buffer;
+    private FileRecorder recorder;
+
+    public RecordTracks(int port) throws SocketException {
+        super(port);
+    }
 
     public static void main(String args[]) throws Exception {
         if (args.length != 2){
             System.out.println("Usage: RecordPresenceGrid [port] [filename]");
         }else{
             RecordTracks r = new RecordTracks(new Integer(args[0]));
-            r.output = new BufferedWriter(new FileWriter(new File(args[1])));
-            r.buffer = new StringBuilder();
+            r.recorder = new FileRecorder(args[1]);
         }
-    }
-
-    public RecordTracks(int port) throws SocketException {
-        super(port);
     }
 
     @Override
     public void handle(TrackResults<BaseTrack> t) {
-        if (output != null){
-
-            buffer.setLength(0);
-            buffer.append(System.currentTimeMillis());
-            buffer.append(':');
-            t.buildString(buffer);
-
+        if (recorder != null){
             try {
-
-                output.write(buffer.toString());
-                output.newLine();
-                output.flush();
-
+                recorder.recored(t);
             } catch (IOException e) {
                 e.printStackTrace();
-                System.exit(-1);
-            }finally{
-
-                try {
-                    output.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                recorder.close();
             }
         }
     }
