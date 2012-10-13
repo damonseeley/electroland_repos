841967bd67b6aff5bd2e7d717a0529bfe8bda5e9
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-10-13 05:44:15 +0000

    more refactoring.  what fuck head wrote this code.

--- Gotham/src/net/electroland/gotham/processing/MetaBalls.java
+++ Gotham/src/net/electroland/gotham/processing/MetaBalls.java
@@ -5,12 +5,11 @@ import processing.core.PVector;
 /**
  * Adapted from http://www.openprocessing.org/sketch/13250
  */
-
 public class MetaBalls extends GothamPApplet {
 
     private static final long serialVersionUID = 7554567212859904536L;
 
-    final int NUM_BALLS         = 6;
+    final int   NUM_BALLS       = 6;
     final float FRICTION        = 0.999f;
     final float MOUSE_REPEL     = 60f;
     final float MAX_VEL         = 5;
@@ -19,34 +18,34 @@ public class MetaBalls extends GothamPApplet {
     final float THRESH_2        = 5.5f;
     final float THRESH_3        = 6;
     final float THRESH_4        = 7;
+    final int   MIN_RADIUS      = 90;
+    final int   MAX_RADIUS      = 140;
 
     // TODO: should cycle hue and saturation from an image file
-    private int hue               = 0;
-    private int saturation        = 100;
+    private int hue             = 0;
+    private int saturation      = 100;
 
     // might be nice to clean these up by putting them in a Metaball object
     private float[] mbRadius    = new float[NUM_BALLS];
     private PVector[] mbPos     = new PVector[NUM_BALLS];
     private PVector[] mbVel     = new PVector[NUM_BALLS];
 
-    private PVector center      = new PVector(0,0); // need to understand this.
-
     @Override
     public void setup(){
-        colorMode(HSB,360,100,100);
-        for(int i=0; i< NUM_BALLS; i++) {
-          mbPos[i] = new PVector(random(0,this.getSyncArea().width),random(0,getSyncArea().height));
-          mbVel[i] = new PVector(random(-1,1),random(-1,1));
-          mbRadius[i] = random(90,140);
+        colorMode(HSB, 360, 100, 100);
+        for(int i = 0; i < NUM_BALLS; i++) {
+          mbPos[i]    = new PVector(random(0, this.getSyncArea().width),random(0, getSyncArea().height));
+          mbVel[i]    = new PVector(random(-1, 1),random(-1, 1));
+          mbRadius[i] = random(MIN_RADIUS, MAX_RADIUS);
         }
     }
 
     @Override
     public void drawELUContent() {
 
-        center.set(0,0,0);
+        PVector center = new PVector(0, 0, 0);
         //update meta ball positions
-        for(int i=0;i<NUM_BALLS;i++) {
+        for(int i=0; i < NUM_BALLS; i++) {
           // we'll replace mouse with vision
 //          if(mousePressed) {
 //            PVector m = new PVector();
@@ -58,54 +57,57 @@ public class MetaBalls extends GothamPApplet {
 //            mbVel[i].add(repel);
 //          }
 
-          center.add(mbPos[i]);
-          mbVel[i].mult(FRICTION);
-          mbVel[i].limit(MAX_VEL);
+            center.add(mbPos[i]);
+            mbVel[i].mult(FRICTION);
+            mbVel[i].limit(MAX_VEL);
         }
 
         center.div(NUM_BALLS);
 
-        for(int i=0;i<NUM_BALLS;i++) {
-
-          // gravity to center
-          PVector c = PVector.sub(center,mbPos[i]);
-          c.normalize();
-          c.mult(COHESION_WEIGHT);
-          mbVel[i].add(c);
-
-          mbPos[i].add(mbVel[i]);
-
-          // wall bouncing
-          if(mbPos[i].x > this.getSyncArea().width) {
-            mbPos[i].x = this.getSyncArea().width;
-            mbVel[i].x *= -1.0;
-          }
-          if(mbPos[i].x < 0) {
-            mbPos[i].x = 0;
-            mbVel[i].x *= -1.0;
-          }
-          if(mbPos[i].y > this.getSyncArea().height) {
-            mbPos[i].y = this.getSyncArea().height;
-            mbVel[i].y *= -1.0;
-          }
-          if(mbPos[i].y < 0) {
-            mbPos[i].y = 0;
-            mbVel[i].y *= -1.0;
-          }
+        for(int i = 0; i < NUM_BALLS; i++) {
+            // gravity to center
+            PVector c = PVector.sub(center, mbPos[i]);
+            c.normalize();
+            c.mult(COHESION_WEIGHT);
+            mbVel[i].add(c);
+            mbPos[i].add(mbVel[i]);
+    
+              // simple bounce when beyond bounds.  only accomplishes 90 or 180
+              // degree turns.
+            if(mbPos[i].x > this.getSyncArea().width) {
+                mbPos[i].x = this.getSyncArea().width;
+                mbVel[i].x = -mbVel[i].x;
+            }
+            if(mbPos[i].x < 0) {
+                mbPos[i].x = 0;
+                mbVel[i].x = -mbVel[i].x;
+            }
+            if(mbPos[i].y > this.getSyncArea().height) {
+                mbPos[i].y = this.getSyncArea().height;
+                mbVel[i].y = -mbVel[i].y;
+            }
+            if(mbPos[i].y < 0) {
+                mbPos[i].y = 0;
+                mbVel[i].y = -mbVel[i].y;
+            }
         }
 
         // render
         background(0);
         hue++; // cycle hue
         float sum;
-        for(int i=0; i<width; i++) {
-          for(int j=0; j<height; j++) {
-            sum = 0;
-            for(int m=0; m<NUM_BALLS; m++) {
-              sum += mbRadius[m] / sqrt(sq(i-mbPos[m].x) + sq(j-mbPos[m].y));
+        
+        // this thing is compositing every single pixel on the screen.
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                sum = 0;
+                // times every ball.  doesn't seem right.  should be able to just reset the entire
+                // grid and THEN composite the balls in a separate pass.
+                for(int m = 0; m < NUM_BALLS; m++) {
+                  sum += mbRadius[m] / sqrt(sq(i-mbPos[m].x) + sq(j-mbPos[m].y));
+                }
+                set(i, j, color(hue % 360, saturation, (sum * sum * sum) / 3));
             }
-            set(i,j,color(hue%360, saturation, (sum*sum*sum)/3));
-          }
         }
     }
 }
