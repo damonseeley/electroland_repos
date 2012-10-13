69d3afa2cf6b1b66566c9f6236bd03599cca85e8
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-10-13 05:11:44 +0000

    more refactoring

--- Gotham/src/net/electroland/gotham/processing/MetaBalls.java
+++ Gotham/src/net/electroland/gotham/processing/MetaBalls.java
@@ -20,55 +20,56 @@ public class MetaBalls extends GothamPApplet {
     final float THRESH_3        = 6;
     final float THRESH_4        = 7;
 
+    // TODO: should cycle hue and saturation from an image file
+    private int hue               = 360;
+    private int saturation        = 100;
+
+    // might be nice to clean these up by putting them in a Metaball object
     private float[] mbRadius    = new float[NUM_BALLS];
-    private float mDistance;
     private PVector[] mbPos     = new PVector[NUM_BALLS];
     private PVector[] mbVel     = new PVector[NUM_BALLS];
-    private float sum           = 0.0f;
-    private PVector m           = new PVector();
-    private PVector c           = new PVector();
-    private PVector repel       = new PVector();
-    private PVector centre      = new PVector(0,0);
 
-    boolean started = false;
+    private PVector centre      = new PVector(0,0); // need to understand this.
+//  private PVector m           = new PVector(); // mouse
+//  private PVector repel       = new PVector(); // mouse repel
+
+    @Override
+    public void setup(){
+        colorMode(HSB,360,100,100);
+        for(int i=0; i< NUM_BALLS; i++) {
+          mbPos[i] = new PVector(random(0,this.getSyncArea().width),random(0,getSyncArea().height));
+          mbVel[i] = new PVector(random(-1,1),random(-1,1));
+          mbRadius[i] = random(90,140);
+        }
+    }
+
     @Override
     public void drawELUContent() {
 
-        if (!started){ // setup is flaking out.
-
-            colorMode(HSB,360,100,100);
-            for(int i=0; i< NUM_BALLS; i++) {
-              mbPos[i] = new PVector(random(0,this.getSyncArea().width),random(0,getSyncArea().height));
-              mbVel[i] = new PVector(random(-1,1),random(-1,1));
-              mbRadius[i] = random(90,140);
-            }
-            started = true;
-        }
-
         centre.set(0,0,0);
         //update meta ball positions
-        for(int i=0;i<NUM_BALLS;i++) {
-          // we'll replace mouse with vision
-          if(mousePressed) {
-            m.set(mouseX,mouseY,0);
-            mDistance = PVector.dist(mbPos[i],m);
-            repel = PVector.sub(mbPos[i],m);
-            repel.normalize();
-            repel.mult(MOUSE_REPEL/(mDistance*mDistance));
-            mbVel[i].add(repel);
-          }
-
-          centre.add(mbPos[i]);
-          mbVel[i].mult(FRICTION);
-          mbVel[i].limit(MAX_VEL);
-        }
+//        for(int i=0;i<NUM_BALLS;i++) {
+//          // we'll replace mouse with vision
+//          if(mousePressed) {
+//            m.set(mouseX,mouseY,0);
+//            float mDistance = PVector.dist(mbPos[i],m);
+//            repel = PVector.sub(mbPos[i],m);
+//            repel.normalize();
+//            repel.mult(MOUSE_REPEL/(mDistance*mDistance));
+//            mbVel[i].add(repel);
+//          }
+//
+//          centre.add(mbPos[i]);
+//          mbVel[i].mult(FRICTION);
+//          mbVel[i].limit(MAX_VEL);
+//        }
 
         centre.div(NUM_BALLS);
 
         for(int i=0;i<NUM_BALLS;i++) {
 
           // gravity to center
-          c = PVector.sub(centre,mbPos[i]);
+          PVector c = PVector.sub(centre,mbPos[i]);
           c.normalize();
           c.mult(COHESION_WEIGHT);
           mbVel[i].add(c);
@@ -96,13 +97,14 @@ public class MetaBalls extends GothamPApplet {
 
         // render
         background(0);
+        float sum;
         for(int i=0; i<width; i++) {
           for(int j=0; j<height; j++) {
             sum = 0;
             for(int m=0; m<NUM_BALLS; m++) {
               sum += mbRadius[m] / sqrt(sq(i-mbPos[m].x) + sq(j-mbPos[m].y));
             }
-            set(i,j,color(360,100,(sum*sum*sum)/3));
+            set(i,j,color(hue, saturation, (sum*sum*sum)/3));
           }
         }
     }
