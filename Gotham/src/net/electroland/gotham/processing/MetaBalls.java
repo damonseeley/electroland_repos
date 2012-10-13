604d9cf81282d1eac1da95e7ec79a05d10428c5f
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-10-13 04:24:37 +0000

    some refactoring for clarity.

--- Gotham/src/net/electroland/gotham/processing/MetaBalls.java
+++ Gotham/src/net/electroland/gotham/processing/MetaBalls.java
@@ -2,39 +2,39 @@ package net.electroland.gotham.processing;
 
 import processing.core.PVector;
 
+/**
+ * Adapted from http://www.openprocessing.org/sketch/13250
+ */
 
 public class MetaBalls extends GothamPApplet {
 
     private static final long serialVersionUID = 7554567212859904536L;
 
-    /**
-     * Adapted from http://www.openprocessing.org/sketch/13250
-     */
-    final int NUM_BALLS = 6;
-    PVector[] mbPos = new PVector[NUM_BALLS];
-    PVector[] mbVel = new PVector[NUM_BALLS];
-    float[] mbRadius = new float[NUM_BALLS];
-    float sum = 0.0f;
-    final float FRICTION = 0.999f;
-    final float MOUSE_REPEL = 60f;
-    final float MAX_VEL = 5;
+    final int NUM_BALLS         = 6;
+    final float FRICTION        = 0.999f; // higher = less friction
+    final float MOUSE_REPEL     = 60f;
+    final float MAX_VEL         = 5;
     final float COHESION_WEIGHT = 0.05f;
-    final float THRESH_1 = 5;
-    final float THRESH_2 = 5.5f;
-    final float THRESH_3 = 6;
-    final float THRESH_4 = 7;
-    PVector u = new PVector();
-    PVector m = new PVector();
-    PVector c = new PVector();
-    PVector repel = new PVector();
-    float mDistance;
-    PVector centre = new PVector(0,0);
+    final float THRESH_1        = 5;
+    final float THRESH_2        = 5.5f;
+    final float THRESH_3        = 6;
+    final float THRESH_4        = 7;
+
+    private float[] mbRadius    = new float[NUM_BALLS];
+    private float mDistance;
+    private float sum           = 0.0f;
+    private PVector[] mbPos     = new PVector[NUM_BALLS];
+    private PVector[] mbVel     = new PVector[NUM_BALLS];
+    private PVector m           = new PVector();
+    private PVector c           = new PVector();
+    private PVector repel       = new PVector();
+    private PVector centre      = new PVector(0,0);
 
     boolean started = false;
     @Override
     public void drawELUContent() {
 
-        if (!started){
+        if (!started){ // setup is flaking out.
 
             colorMode(HSB,360,100,100);
             //size(400, 400, P2D);
@@ -47,11 +47,11 @@ public class MetaBalls extends GothamPApplet {
 
             started = true;
         }
-        //println(frameRate);
+
         centre.set(0,0,0);
         //update meta ball positions
         for(int i=0;i<NUM_BALLS;i++) {
-          //mouse?
+          // when we want people to interact, we'll use this (not mouse)
           if(mousePressed) {
             m.set(mouseX,mouseY,0);
             mDistance = PVector.dist(mbPos[i],m);
@@ -60,27 +60,25 @@ public class MetaBalls extends GothamPApplet {
             repel.mult(MOUSE_REPEL/(mDistance*mDistance));
             mbVel[i].add(repel);
           }
-       
+
           centre.add(mbPos[i]);
-       
           mbVel[i].mult(FRICTION);
-       
           mbVel[i].limit(MAX_VEL);
         }
-       
+
         centre.div(NUM_BALLS);
-       
+
         for(int i=0;i<NUM_BALLS;i++) {
-       
-          //tendancy to go to centre
+
+          // gravity to center
           c = PVector.sub(centre,mbPos[i]);
           c.normalize();
           c.mult(COHESION_WEIGHT);
           mbVel[i].add(c);
-       
+
           mbPos[i].add(mbVel[i]);
-       
-          //bounce
+
+          // wall bouncing
           if(mbPos[i].x > width) {
             mbPos[i].x = width;
             mbVel[i].x *= -1.0;
@@ -98,7 +96,8 @@ public class MetaBalls extends GothamPApplet {
             mbVel[i].y *= -1.0;
           }
         }
-        //draw stuff
+
+        // render
         background(0);
         for(int i=0; i<width; i++) {
           for(int j=0; j<height; j++) {
@@ -106,35 +105,8 @@ public class MetaBalls extends GothamPApplet {
             for(int m=0; m<NUM_BALLS; m++) {
               sum += mbRadius[m] / sqrt(sq(i-mbPos[m].x) + sq(j-mbPos[m].y));
             }
-            switch(key) {
-            case '4':
-              //Irish flag
-              if(sum >= THRESH_1 && sum <= THRESH_2) {
-                set(i,j,color(155,100,62)); //Green
-              }
-              else if(sum > THRESH_2 && sum <= THRESH_3) {
-                set(i,j,color(0,0,100)); //White
-              }
-              else if( sum > THRESH_3 && sum <= THRESH_4) {
-                set(i,j,color(30,100,96)); //Gold
-              }
-              break;
-            case '3':
-              //Psychadelic
-              set(i,j,color(sum*sum*sum/2,100,255));
-              break;
-            case '2':
-              //White
-              set(i,j,color(0,0,sum*sum*sum/2));
-              break;
-            case '1':
-            default :
-              //Red     
-              set(i,j,color(360,100,(sum*sum*sum)/3));
-              break;
-            }
+            set(i,j,color(360,100,(sum*sum*sum)/3)); // modulate only brightness
           }
         }
-
     }
 }
