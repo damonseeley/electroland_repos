daceb555b911eba9c28e86f49762a627ee08354d
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-10-16 05:03:33 +0000

    first pass mouse reactivity

--- Gotham/src/net/electroland/gotham/processing/MetaBalls.java
+++ Gotham/src/net/electroland/gotham/processing/MetaBalls.java
@@ -1,6 +1,8 @@
 package net.electroland.gotham.processing;
 
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +15,20 @@ public class MetaBalls extends GothamPApplet {
 
     private static final long serialVersionUID = 7554567212859904536L;
 
+    // we want the lava to stay on screen
+    final Rectangle BOUNDARY    = new Rectangle(60,60,580,390);
+
     final int   COARSNESS       = 5;
     final int   NUM_BALLS       = 3;
     final int   MIN_RADIUS      = 200;    // initial ball radius min
     final int   MAX_RADIUS      = 400;   // initial ball radius max
 
-    final float FRICTION        = 1.0f;  // higher = less
+    final float FRICTION        =   .75f;  // higher = less
+    final float P_FRICTION      =  1.0f;
     final float MAX_VEL         = 15;    // higher = faster
-    final float COHESION_WEIGHT = 0.05f;  // higher = more
+    final float COHESION =   .05f;  // higher = more
+    final float P_COHESION      = -COHESION;
+
 
     // TODO: should cycle hue and saturation from an image file
     private int hue             = 0;
@@ -29,6 +37,8 @@ public class MetaBalls extends GothamPApplet {
 
     // might be nice to clean these up by putting them in a Collection of Metaballs.
     private List<Metaball> balls;
+    private Point mainPresence;
+
 
     @Override
     public void setup(){
@@ -50,7 +60,7 @@ public class MetaBalls extends GothamPApplet {
         //update meta ball positions
         for (Metaball ball : balls){
             center.add(ball.position);
-            ball.velocity.mult(FRICTION);
+            ball.velocity.mult(mainPresence != null ? P_FRICTION : FRICTION);
             ball.velocity.limit(MAX_VEL);
         }
 
@@ -60,7 +70,7 @@ public class MetaBalls extends GothamPApplet {
 
             PVector c = PVector.sub(center, ball.position);
             c.normalize();
-            c.mult(COHESION_WEIGHT);
+            c.mult(mainPresence != null ? P_COHESION : COHESION);
             ball.velocity.add(c);
             ball.position.add(ball.velocity);
 
@@ -68,11 +78,8 @@ public class MetaBalls extends GothamPApplet {
         }
 
         // render
-        hue++; // cheaply cycle hue
+        //hue++; // cheaply cycle hue
 
-        // given that computation goes up with square of dimensions, it would
-        // make sense to do something like determine the boundaries of the
-        // detectors and only render that range.
         noStroke();
         for(int i = 0; i < this.getSyncArea().width; i+= COARSNESS) {
             for(int j = 0; j < this.getSyncArea().height; j+= COARSNESS) {
@@ -82,14 +89,33 @@ public class MetaBalls extends GothamPApplet {
                     ball.pixelImpact = ball.radius / sqrt(sq(i - ball.position.x) + sq(j - ball.position.y));
                     sum += ball.pixelImpact;
                 }
-                // TODO: need to calculate hue in here as well
-                // hue should be the number above (being added to sum) as a coefficient blending the
-                // current balls hue into the overall hue of the pixel
-                //set(i, j, color(hue % 360, saturation, (sum * sum * sum) / 3));
                 fill(hue % 360, saturation, (sum * sum * sum) / 3);
                 this.rect(i, j, COARSNESS, COARSNESS);
             }
         }
+
+        // show where the mouse was clicked
+        if (mainPresence != null){
+            stroke(0, 0, 255);
+            line(mainPresence.x - 10, mainPresence.y, mainPresence.x + 10, mainPresence.y);
+            line(mainPresence.x, mainPresence.y - 10, mainPresence.x, mainPresence.y + 10);
+        }
+    }
+
+
+    Point down;
+    boolean on;
+    public void mousePressed() {
+        mainPresence = new Point(mouseX, mouseY);
+    }
+
+    public void mouseDragged(){
+        mainPresence = new Point(mouseX, mouseY);
+    }
+
+    public void mouseReleased(){
+        mainPresence = new Point(mouseX, mouseY);
+        mainPresence = null;
     }
 
     class Metaball {
@@ -97,7 +123,6 @@ public class MetaBalls extends GothamPApplet {
         float radius;
         PVector position;
         PVector velocity;
-        int hue; // can't quite implement per ball hue yet.
         Dimension area;
         float pixelImpact;
 
@@ -106,32 +131,27 @@ public class MetaBalls extends GothamPApplet {
             position  = new PVector(random(0, initArea.width),random(0, initArea.height));
             velocity  = new PVector(random(-1, 1),random(-1, 1));
             radius    = random(MIN_RADIUS, MAX_RADIUS);
-            hue       = 0;
         }
 
         public void checkBounds(){
             // simple bounce when beyond bounds.  only accomplishes 90 or 180
             // degree turns.
-            if(position.x > area.width) {
-                position.x = area.width;
+            if(position.x > BOUNDARY.x + BOUNDARY.width) {
+                position.x = BOUNDARY.x + BOUNDARY.width;
                 velocity.x = -velocity.x;
             }
-            if(position.x < 0) {
-                position.x = 0;
+            if(position.x < BOUNDARY.x) {
+                position.x = BOUNDARY.x;
                 velocity.x = -velocity.x;
             }
-            if(position.y > area.height) {
-                position.y = area.height;
+            if(position.y > BOUNDARY.y + BOUNDARY.height) {
+                position.y = BOUNDARY.y + BOUNDARY.height;
                 velocity.y = -velocity.y;
             }
-            if(position.y < 0) {
-                position.y = 0;
+            if(position.y < BOUNDARY.y) {
+                position.y = BOUNDARY.y;
                 velocity.y = -velocity.y;
             }
         }
-    }
-    
-    class MetaballGroup {
-        
     }
 }
