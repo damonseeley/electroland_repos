0c9a6d074c2557c78c75c43c3edb766712b70f45
emendelowitz <emendelowitz@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-10-20 19:55:32 +0000

    added comments, refactored camera creation into function

--- ElVis/src/net/electroland/elvis/blobktracking/core/ElVisServer.java
+++ ElVis/src/net/electroland/elvis/blobktracking/core/ElVisServer.java
@@ -10,6 +10,7 @@ import net.electroland.elvis.blobktracking.ui.Console;
 import net.electroland.elvis.blobktracking.ui.PresenceDetectorKeyListener;
 import net.electroland.elvis.blobtracking.BlobTracker;
 import net.electroland.elvis.blobtracking.TrackListener;
+import net.electroland.elvis.imaging.acquisition.openCV.OpenCVCam;
 import net.electroland.elvis.net.ImageServer;
 import net.electroland.elvis.net.TrackUDPBroadcaster;
 import net.electroland.elvis.util.CameraFactory;
@@ -77,30 +78,9 @@ public class ElVisServer implements TimeOutListener {
 		blobTracker.presenceDetector.thresh.getParameter(0).setValue(props.getProperty("threshold", 100.0));
 		blobTracker.presenceDetector.background.getParameter(0).setValue(props.getProperty("adaptation", .001));
 
-		try {
 
-			//			blobTracker.setSourceStream(props.getProperty("camera", ImagePanel.FLY_SRC));
-			String cameraSrcString = props.getProperty("camera", CameraFactory.FLY_SRC);
-			blobTracker.setSourceStream(cameraSrcString);
-			if(cameraSrcString.equals(CameraFactory.FLY_SRC)) {
-				long cameraTimeOutDuration = props.getProperty("cameraTimeOutDuration", 2000);
-				if(cameraTimeOutDuration > 0) {
-					logger.info("camera timeout duration " + cameraTimeOutDuration);
-					TimeOutMonitorThread tom = new TimeOutMonitorThread(cameraTimeOutDuration);
-					tom.addTimeOutListener(this);
-					tom.start();
-				} else {
-					logger.info("not monitoring camera time outs, cameraTimeOutDuration <= 0");
-				}
-			}
-
-
-		} catch (IOException e) {
-			logger.error(e.toString());
-			e.printStackTrace();
-		}
-
-
+		setupCamera();
+		
 		blobTracker.presenceDetector.start();
 
 		if(props.getProperty("runImageServer", true)) {
@@ -115,7 +95,31 @@ public class ElVisServer implements TimeOutListener {
 
 
 	}
+	protected void setupCamera() {
+		try {
 
+			String cameraSrcString = props.getProperty("camera", CameraFactory.FLY_SRC);
+			blobTracker.setSourceStream(cameraSrcString);
+			if(cameraSrcString.equals(CameraFactory.FLY_SRC)) {
+				long cameraTimeOutDuration = props.getProperty("cameraTimeOutDuration", 2000);
+				if(cameraTimeOutDuration > 0) {
+					logger.info("camera timeout duration " + cameraTimeOutDuration);
+					TimeOutMonitorThread tom = new TimeOutMonitorThread(cameraTimeOutDuration);
+					tom.addTimeOutListener(this);
+					((OpenCVCam) blobTracker.getSourceStream()).setTimeOutMonitorThread(tom);
+					tom.start();
+				} else {
+					logger.info("not monitoring camera time outs, cameraTimeOutDuration <= 0");
+				}
+			}
+
+
+		} catch (IOException e) {
+			logger.error(e.toString());
+			e.printStackTrace();
+		}
+		
+	}
 
 
 
@@ -163,25 +167,11 @@ public class ElVisServer implements TimeOutListener {
 	public void monitorTimedOut(TimeOutMonitorThread sender) {
 		logger.error("Camera timed out");
 		if(props.getProperty("cameraTimeOutRestart", false)) {
-			logger.warn("attempting to restart camera");
+			// replace everything in this if statement with exit(0)
 			sender.stopRunning();
+			logger.warn("attempting to restart camera");
 			blobTracker.getSourceStream().stopRunningForce();
-			String cameraSrcString = props.getProperty("camera", CameraFactory.FLY_SRC);
-			long cameraTimeOutDuration = props.getProperty("cameraTimeOutDuration", 2000);
-			if(cameraTimeOutDuration > 0) {
-				logger.info("camera timeout duration " + cameraTimeOutDuration);
-				TimeOutMonitorThread tom = new TimeOutMonitorThread(cameraTimeOutDuration);
-				tom.addTimeOutListener(this);
-				tom.start();
-			}
-
-			try {
-				blobTracker.setSourceStream(cameraSrcString);
-				logger.warn("camera re-started");
-			} catch (IOException e) {
-				logger.error("untable to restart camera");
-				logger.error(e.toString());
-			}
+			setupCamera();
 		} else {
 			logger.warn("camera restart not requested");			
 		}
