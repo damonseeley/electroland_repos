36c1deab9b4424e8e971edd55145f4e57aa36b0c
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-10-21 01:42:55 +0000

    properly printing presence grids

--- Gotham/src/net/electroland/gotham/processing/Metaballs3.java
+++ Gotham/src/net/electroland/gotham/processing/Metaballs3.java
@@ -1,6 +1,5 @@
 package net.electroland.gotham.processing;
 
-import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
@@ -9,16 +8,23 @@ import java.util.List;
 
 import net.electroland.elvis.blobtracking.BaseTrack;
 import net.electroland.elvis.net.GridData;
+import net.electroland.gotham.processing.assets.FastBlur;
 import processing.core.PVector;
 
 @SuppressWarnings("serial")
 public class Metaballs3 extends GothamPApplet {
 
-    public static final int NONE                 = 0;
-    public static final int MOUSE                 = 1;
-    public static final int TRACK                 = 2;
-
+    public static final int NONE                    = 0;
+    public static final int MOUSE                   = 1;
+    public static final int TRACK                   = 2;
+    public static final int GRID_DEFAULT            = 0;
+    public static final int GRID_EDIT_TOPLEFT       = 1;
+    public static final int GRID_EDIT_DIM           = 2;
+    public static final int GRID_EDIT_INSET         = 3;
+    public static final int GRID_EDIT_CANVAS_DIM    = 4;
+    
     private int mode = TRACK; // set the reaction mode
+    private int presenceMode = GRID_DEFAULT;
 
     public static final float FRICTION            = .999f;  // higher = less .999
     public static final float MAX_VEL             = 1.0f;  // base velocity with no interaction or tracks higher = faster was 0.75
@@ -31,16 +37,21 @@ public class Metaballs3 extends GothamPApplet {
     // ball scale
     public static final float ELLIPSE_SCALE       = 5.0f;   // percent 2.0-2.4 then 3.5 for most testting
     public static final int ELLIPSE_ALPHA       = 150;   // value/255
-
+    // presence scale
+    public static final float DILATE            = 6.0f;
+    
     // don't touch:
     public static final float SQUISHINESS         = 50;     // higher = more
     public static final float MIN_VEL             = .75f;   // higher = faster
 
     private List <MetaballGroup>groups;
 
-    private Point mainPresence;
-
     private GridData gridData;
+    // specifies the section of the incoming grid that we want to subset
+    Rectangle grid = new Rectangle(2, 4, 68, 21);
+    // specifies how the grid will be translated and scaled to the canvas
+    Rectangle gridOnCanvas = new Rectangle(80, 70, 0, 0);
+
     private List<BaseTrack>trackData;
 
     @Override
@@ -102,22 +113,25 @@ public class Metaballs3 extends GothamPApplet {
 
             for (Metaball ball : group.balls){
 
+                boolean runningAway = false;
                 // mousePressed = proxy for presence grid repelling
                 if(mode == MOUSE && mousePressed) {
                     ball.repell(new PVector(mouseX, mouseY, 0), REPELL_FORCE);
                     ball.spaceCheck(groups);
+                    runningAway = true;
 
                 } else if (mode == TRACK && trackData != null) {
                     synchronized(trackData){
-                    	for (BaseTrack track : trackData){
-                    		ball.repell(new PVector(track.x, track.y, 0), REPELL_FORCE);
-                    	}
+                        runningAway = trackData.size() > 0;
+                        for (BaseTrack track : trackData){
+                            ball.repell(new PVector(track.x, track.y, 0), REPELL_FORCE);
+                        }
                     }
                 }
 
                 center.add(ball.position);
                 ball.velocity.mult(FRICTION);
-                ball.velocity.limit(mainPresence != null ? MAX_RUN_VEL : MAX_VEL);
+                ball.velocity.limit(runningAway ? MAX_RUN_VEL : MAX_VEL);
                 if (ball.velocity.mag() < MIN_VEL)
                     ball.velocity.setMag(MIN_VEL);
             }
@@ -135,20 +149,17 @@ public class Metaballs3 extends GothamPApplet {
             group.checkBounds();
         }
 
-        fill(color(128, 0, 255),127); //fill with purple
-        
+        // fill the whole area with purple
+        fill(color(128, 0, 255), 127);
+        rect(0, 0, width, height);
 
-		//fill(color(255, 255, 255),127); //fill with a light alpha white
-		rect(0,0,width,height); //fill the whole area
-
-        
         // render each group's bounding box
         for (MetaballGroup group : groups){
             this.stroke(255,255,255);
             this.noFill();
             this.rect(group.range.x, group.range.y, group.range.width, group.range.height);
         }
-        
+
         // render groups of balls
         for (MetaballGroup group : groups){
             for (Metaball ball : group.balls){
@@ -157,69 +168,71 @@ public class Metaballs3 extends GothamPApplet {
                 this.ellipse(ball.position.x, ball.position.y, ball.width(), ball.height());
             }
         }
-        // blur the whole thing
-        //filter(BLUR, 3);
 
         // presence
         if (mode == MOUSE && mousePressed){
             fill(10, 200, 255);
-            //ellipse(mouseX, mouseY, 60, 40);
             ellipse(mouseX, mouseY, 120, 120);
-        //} else if (mode == TRACK && trackData !=null) {
         } else if (mode == TRACK && gridData !=null) {
 
-//            synchronized(trackData){
-//                for (BaseTrack track : trackData){
-//                    fill(255, 255, 255);
-//                    int blobSize = 80;
-//                    ellipse(track.x, track.y, blobSize, blobSize);
-//                }
-//            }
-            
-            /**  IF YOU ACTIVATE THIS CODE, make sure to change (mode == TRACK && trackData != null) (above) to gridData != null */
-            int insetLeft = 80;
-            int insetTop = 70;
-            double dilate = 6.0;
+            fill(color(0, 0, 50), 8); //fill with a light alpha white
+            rect(0, 0, this.getSyncArea().width, this.getSyncArea().height); //fill the whole area
 
-            fill(color(0, 0, 50),8); //fill with a light alpha white
-            rect(0,0,this.getSyncArea().width,this.getSyncArea().height); //fill the whole area
+            if (gridData != null){
 
-            synchronized(gridData){
-                if (gridData != null){
+                synchronized(gridData){
 
-                	
-                	int gridXStart = 2; //was 2 10/18
-                    int gridXMax = 70;
-                    int gridYStart = 4; // test start inset on top was 4 on 10/18
-                    int gridYMax = 25; //all of the height was 25 10/18
-                    
-                    int hShift = -0;
+                    gridOnCanvas = new Rectangle(80, 70, 
+                            this.getSyncArea().width - 160, 
+                            this.getSyncArea().height - 140);
+                    float cellWidth = gridOnCanvas.width / (float)gridData.width;
+                    float cellHeight = gridOnCanvas.height / (float)gridData.height;
 
-                    int cellWidth = (this.getSyncArea().width-(insetLeft*2))/(gridXMax-gridXStart);
-                    int cellHeight = (this.getSyncArea().height-(insetTop*2))/(gridYMax-gridYStart);
+                    if (presenceMode != GRID_DEFAULT){
+                        stroke(255);
+                    }else{
+                        noStroke();
+                        fill(255, 255, 255, 127);
+                    }
 
-                    for(int y = gridYStart; y < gridYMax; y++) {
-                        for(int x = gridXStart; x < gridXMax; x++) {
-                            if (gridData.getValue(x, y) > 0){
-                                fill(color(255, 255, 255));
-                                //fill(color(20, 240, 255));
-                                ellipse(this.getSyncArea().width-(insetLeft*2)+hShift-y*cellHeight+insetLeft, this.getSyncArea().height-(insetTop*2)-x*cellWidth+insetTop, (int)(cellHeight*dilate), (int)(cellWidth*dilate)); //rotated
-
+                    for (int x = 0; x < gridData.width; x++){
+                        for (int y = 0; y < gridData.height; y++){
+                            if (presenceMode != GRID_DEFAULT){
+                                if (gridData.getValue(x, y) != (byte)0){
+                                    fill(255);
+                                }else{
+                                    noFill();
+                                }
+                                this.rect(gridOnCanvas.x + (x * cellWidth), 
+                                          gridOnCanvas.y + (y * cellHeight), 
+                                          cellWidth, 
+                                          cellHeight);
+                            } else {
+                                if (gridData.getValue(x, y) != (byte)0){
+                                    this.ellipse(gridOnCanvas.x + (x * cellWidth), 
+                                                 gridOnCanvas.y + (y * cellHeight), 
+                                                 cellWidth * DILATE, 
+                                                 cellHeight * DILATE);
+                                }
                             }
                         }
                     }
+
+                    // render the presences
                 }
             }
         }
-/*
+
         loadPixels();
         FastBlur.performBlur(pixels, width, height, floor(18));
         updatePixels();
-*/
-    }
 
-    Rectangle grid;
-    Rectangle gridOnCanvas;
+    }
+ 
+    @Override
+    public void keyPressed() {
+        System.out.println(keyCode);
+    }
 
     @Override
     public void handle(GridData srcData) {
@@ -228,9 +241,15 @@ public class Metaballs3 extends GothamPApplet {
         } else {
             synchronized(gridData){
 
+                // copy the original source, so we don't accidentally change
+                // the source for other clients using this data.
+                StringBuilder sb = new StringBuilder();
+                srcData.buildString(sb);
+                srcData = new GridData(sb.toString());
+
                 srcData = subset(srcData, grid);
 
-                srcData = rotate(srcData, -90, new Point(grid.width / 2, grid.height / 2));
+                srcData = counterClockwise(srcData);
 
                 srcData = flipHorizontal(srcData);
 
@@ -239,10 +258,28 @@ public class Metaballs3 extends GothamPApplet {
         }
     }
 
-    public GridData subset(GridData in, Rectangle boundary){
+    public static void print(GridData d){
+        for (int i = 0; i < d.data.length; i++){
+            System.out.print(d.data[i] + " ");
+            if ((i + 1) % d.width == 0)
+                System.out.println();
+        }
+        System.out.println();
+    }
+    
+    /**
+     * returns new GridData where data is a subset of the original based on
+     * a Rectangular boundary.
+     * 
+     * @param in
+     * @param boundary
+     * @return
+     */
+    public static GridData subset(GridData in, Rectangle boundary){
+
         byte[] target = new byte[boundary.width * boundary.height];
-        for (int y = 0; y < boundary.height; y++){
-            System.arraycopy(in.data, ((y + boundary.y - 1) * in.height) + (boundary.x - 1), target, y * boundary.width, boundary.width);
+        for (int y = 0; y < boundary.height; y++) {
+            System.arraycopy(in.data, ((y + boundary.y) * in.width) + (boundary.x), target, y * boundary.width, boundary.width);
         }
         in.data  = target;
         in.height = boundary.height;
@@ -250,19 +287,39 @@ public class Metaballs3 extends GothamPApplet {
         return in;
     }
 
-    public GridData rotate(GridData in, float degrees, Point center){
-    	/**
-        float[] pt = {track.x, track.y};
-        AffineTransform.getRotateInstance(Math.toRadians(degrees), center.x, center.y).transform(pt, 0, pt, 0, 1);
-        track.x = pt[0];
-        track.y = pt[1];
-        */
-        return null;
+    public static GridData counterClockwise(GridData in){
+
+        // switch height for width and vice versa
+        byte[] rotated = new byte[in.data.length];
+        int i = 0;
+
+        for (int x = in.width - 1; x >= 0; x--){
+            for (int y = 0; y < in.height; y++){
+                rotated[i++] = in.data[y * in.width + x];
+            }
+        }
+        int w = in.width;
+        in.width = in.height;
+        in.height = w;
+        in.data = rotated;
+
+        return in;
     }
 
-    public GridData flipHorizontal(GridData in){
-    
-        return null;
+    public static GridData flipHorizontal(GridData in){
+        int center = in.width / 2;
+        byte buffer;
+
+        for (int y = 0; y < in.height; y++){
+            for (int x = 0; x < center; x++){
+                int leftIndex = y * in.width + x;
+                int rightIndex = (y + 1) * in.width - x - 1;
+                buffer = in.data[leftIndex];
+                in.data[leftIndex] = in.data[rightIndex];
+                in.data[rightIndex] = buffer;
+            }
+        }
+        return in;
     }
 
     @Override
