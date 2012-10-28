41828794feed7bd3a2874186399ace8ab677c3c1
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-10-28 23:59:37 +0000

    first checkin

---
+++ ElVis/install/run.bat
@@ -0,0 +1,1 @@
+java -classpath ./depends;javacpp.jar;javacv-windows-x86_64.jar;javacv-windows-x86.jar;javacv.jar;JMyron.jar;log4j-1.2.9.jar;miglayout15-swing.jar;ELVIS.jar; net.electroland.elvis.blobktracking.core.ElVisServer
