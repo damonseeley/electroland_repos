b7abacd27025e99686dd97ffa45edf2aee831ddb
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-11-27 08:08:01 +0000

    more scaffolding.

--- EIO2/src/net/electroland/eio/devices/Device.java
+++ EIO2/src/net/electroland/eio/devices/Device.java
@@ -1,5 +1,15 @@
 package net.electroland.eio.devices;
 
+import java.util.Map;
+
+import net.electroland.eio.Value;
+import net.electroland.utils.ParameterMap;
+
 public abstract class Device {
 
+    public Device(ParameterMap params){}
+
+    abstract public Map<Channel, Value> read();
+
+    abstract public void write(Map<Channel, Value> values);
 }
