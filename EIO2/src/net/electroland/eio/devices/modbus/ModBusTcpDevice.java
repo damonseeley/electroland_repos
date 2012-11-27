35d1441caeb1f03cf66267eab6bcf890d384d43b
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2012-11-27 08:08:01 +0000

    more scaffolding.

--- EIO2/src/net/electroland/eio/devices/modbus/ModBusTcpDevice.java
+++ EIO2/src/net/electroland/eio/devices/modbus/ModBusTcpDevice.java
@@ -1,7 +1,28 @@
 package net.electroland.eio.devices.modbus;
 
+import java.util.Map;
+
+import net.electroland.eio.Value;
+import net.electroland.eio.devices.Channel;
 import net.electroland.eio.devices.Device;
+import net.electroland.utils.ParameterMap;
 
 public class ModBusTcpDevice extends Device {
 
+    public ModBusTcpDevice(ParameterMap params) {
+        super(params);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Map<Channel, Value> read() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void write(Map<Channel, Value> values) {
+        // TODO Auto-generated method stub
+        
+    }
 }
