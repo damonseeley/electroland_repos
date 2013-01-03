f9d3521bb63ac78e8b918f7ccb1984fcb0015130
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-01-03 04:51:55 +0000

    forgotten check in

--- EIO2/src/net/electroland/eio/devices/Channel.java
+++ EIO2/src/net/electroland/eio/devices/Channel.java
@@ -6,7 +6,14 @@ import net.electroland.eio.filters.Filter;
 import net.electroland.utils.ParameterMap;
 
 public abstract class Channel {
+
     public String id;
+
     protected List<Filter>filters;
+
     public Channel(ParameterMap p){}
+
+    public boolean isSuspect(){
+        return false;
+    }
 }
