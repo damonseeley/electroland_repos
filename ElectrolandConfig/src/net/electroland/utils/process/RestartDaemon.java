1d82a8b4249ecc48d73467cae94a8ace23b70a83
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-01-04 05:04:28 +0000

    first checkin

---
+++ ElectrolandConfig/src/net/electroland/utils/process/RestartDaemon.java
@@ -0,0 +1,100 @@
+package net.electroland.utils.process;
+
+import java.awt.BorderLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class RestartDaemon extends JFrame implements ProcessExitedListener, WindowListener {
+
+    private ProcessItem running;
+    private JCheckBox state;
+    private String rootDir, batFileName;
+    private long pollRate;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        if (args.length != 3){
+            System.out.println("Usage: RestartDaemon [rootDir] [batFileName] [pollRate]");
+        }else{
+            RestartDaemon daemon = new RestartDaemon();
+
+            daemon.rootDir     = args[0];
+            daemon.batFileName = args[1];
+            daemon.pollRate    = Long.parseLong(args[2]);
+
+            daemon.setTitle(daemon.rootDir + " " + daemon.batFileName);
+            daemon.state = new JCheckBox("Restart if process dies?");
+            daemon.setLayout(new BorderLayout());
+            daemon.getContentPane().add(daemon.state, BorderLayout.CENTER);
+            daemon.setSize(250,100);
+            daemon.setVisible(true);
+            daemon.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            daemon.addWindowListener(daemon);
+            daemon.run();
+        }
+    }
+
+    public void run(){
+        running = ProcessUtil.run(readBat(batFileName, 
+                                  rootDir), new File(rootDir), this, pollRate);
+    }
+
+    public static String readBat(String filename, String rootDir){
+        if (filename == null){
+            return null;
+        }else{
+            File bat = new File(rootDir, filename);
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new FileReader(bat));
+                String command = br.readLine();
+                br.close();
+                return command;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void exited(ProcessItem ded) {
+        if (state.isSelected()){
+            run();
+        }
+    }
+
+    @Override
+    public void windowClosing(WindowEvent arg0) {
+        state.setSelected(false);
+        ProcessUtil.kill(running);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent arg0) {}
+
+    @Override
+    public void windowActivated(WindowEvent arg0) {}
+
+    @Override
+    public void windowDeactivated(WindowEvent arg0) {}
+
+    @Override
+    public void windowDeiconified(WindowEvent arg0) {}
+
+    @Override
+    public void windowIconified(WindowEvent arg0) {}
+
+    @Override
+    public void windowOpened(WindowEvent arg0) {}
+}
