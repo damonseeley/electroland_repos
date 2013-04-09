8ae9807eeee395b970851f36b7f833f1ab43139b
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-09 19:04:37 +0000

    support for multiple filtered values.

--- EIO2/src/net/electroland/eio/filters/Filter.java
+++ EIO2/src/net/electroland/eio/filters/Filter.java
@@ -3,7 +3,16 @@ package net.electroland.eio.filters;
 import net.electroland.eio.Value;
 import net.electroland.utils.ParameterMap;
 
-public interface Filter {
-    public void configure(ParameterMap map);
-    public void filter(Value in);
+abstract public class Filter {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    abstract public void configure(ParameterMap map);
+    abstract public void filter(Value in);
 }
