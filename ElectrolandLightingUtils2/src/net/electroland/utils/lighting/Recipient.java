79e1b9ede73fb6ce5e70beea0b81701f8059b63d
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-11 18:56:27 +0000

    fixed whitespace problems.

--- ElectrolandLightingUtils2/src/net/electroland/utils/lighting/Recipient.java
+++ ElectrolandLightingUtils2/src/net/electroland/utils/lighting/Recipient.java
@@ -5,59 +5,63 @@ import net.electroland.utils.ParameterMap;
 
 
 abstract public class Recipient {
-	
-	private String name;	
-	private CanvasDetector[] channels;
-	
-	public void setName(String name){
-		this.name = name;
-	}
-	
-	public String getName(){
-		return name;
-	}
 
-	protected void sync(){
-		
-		Byte[] b = new Byte[channels.length];
-		
-		for (int i = 0; i < channels.length; i++)
-		{
-			if (channels[i] != null){
-				b[i] = channels[i].latestState;
-			}
-		}
+    private String name;
+    private CanvasDetector[] channels;
 
-		send(b);
-	}
+    public void setName(String name){
+        this.name = name;
+    }
 
-	public CanvasDetector[] getChannels()
-	{
-		return channels;
-	}
-	public void setChannels(CanvasDetector[] channels)
-	{
-		this.channels = channels;
-	}
-	
-	// configure
-	abstract public void configure(ParameterMap params) throws OptionException;
-	
-	// configure
-	abstract public void map(int channel, CanvasDetector cd) throws OptionException;
-	
-	// send all "on" values
-	abstract public void allOn();
-	
-	// send all "off" values
-	abstract public void allOff();
+    public String getName(){
+        return name;
+    }
 
-	// protocol specific handling
-	abstract public void send(Byte[] b);
+    /** 
+     * Takes the latest value from each ChannelDetectors and sends it to the
+     * lights. Assumes ELUCanvas.sync(int[] pixels) was called first. 
+     */
+    protected void sync(){
 
-	/**
-	 * print debugging info.
-	 */
-	abstract public void debug();
+        Byte[] b = new Byte[channels.length];
+        
+        for (int i = 0; i < channels.length; i++)
+        {
+            if (channels[i] != null){
+                b[i] = channels[i].latestState;
+            }
+        }
+
+        send(b);
+    }
+
+    public CanvasDetector[] getChannels()
+    {
+        return channels;
+    }
+    public void setChannels(CanvasDetector[] channels)
+    {
+        this.channels = channels;
+    }
+
+    // configure
+    abstract public void configure(ParameterMap params) throws OptionException;
+
+    // configure
+    abstract public void map(int channel, CanvasDetector cd) throws OptionException;
+
+    // send all "on" values
+    abstract public void allOn();
+
+    // send all "off" values
+    abstract public void allOff();
+
+    // protocol specific handling
+    abstract public void send(Byte[] b);
+
+    /**
+     * print debugging info.
+     */
+    abstract public void debug();
 
 }
