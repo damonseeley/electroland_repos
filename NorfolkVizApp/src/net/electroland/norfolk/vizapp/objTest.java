d54cbb3278f338c892c7fa4a80833b390aca0978
dglicksman <dglicksman@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-22 23:53:19 +0000

    Test commit to try to get .obj files uploaded

---
+++ NorfolkVizApp/src/net/electroland/norfolk/vizapp/objTest.java
@@ -0,0 +1,39 @@
+package net.electroland.norfolk.vizapp;
+import processing.core.*;
+import remixlab.proscene.*;
+//import saito.objloader.*;
+
+
+public class objTest extends PApplet {
+	
+	/**
+	 * Load and Display an OBJ Shape. 
+	 * 
+	 * The loadShape() command is used to read simple SVG (Scalable Vector Graphics)
+	 * files and OBJ (Object) files into a Processing sketch. This example loads an
+	 * OBJ file of a rocket and displays it to the screen. 
+	 */
+
+
+	PShape rocket;
+
+	float ry;
+	  
+	public void setup() {
+	  size(640, 360, P3D);
+	    
+	  rocket = loadShape("/depends/models/sculpture.obj");
+	}
+
+	public void draw() {
+	  background(0);
+	  lights();
+	  
+	  translate(width/2, height/2 + 100, -200);
+	  rotateZ(PI);
+	  rotateY(ry);
+	  shape(rocket);
+	  
+	  ry += 0.02;
+	}
+}
