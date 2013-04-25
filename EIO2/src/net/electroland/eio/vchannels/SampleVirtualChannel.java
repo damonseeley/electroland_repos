5b24fdd9ca72c3b808654ea4f34c5547e420e083
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-25 07:36:30 +0000

    added config method for VirtualChannels

--- EIO2/src/net/electroland/eio/vchannels/SampleVirtualChannel.java
+++ EIO2/src/net/electroland/eio/vchannels/SampleVirtualChannel.java
@@ -3,8 +3,14 @@ package net.electroland.eio.vchannels;
 import net.electroland.eio.Value;
 import net.electroland.eio.ValueSet;
 import net.electroland.eio.VirtualChannel;
+import net.electroland.utils.ParameterMap;
 
 public class SampleVirtualChannel extends VirtualChannel {
+
+    @Override
+    public void configure(ParameterMap params) {
+        // TODO Auto-generated method stub
+    }
 
     @Override
     public Value read(ValueSet inputValues) {
