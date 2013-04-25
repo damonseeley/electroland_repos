dc8d1ff5f78e8872acf69c43283c96477826fb5f
dglicksman <dglicksman@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-25 21:23:58 +0000

    Hopefully the last of the vague, sweeping-changes updates. Basic functionality in place: -All lights individually addressable for color and triggerState -Additive blend mode for light beams -Human participants and sensor beam models toggleable via p and b keys -Camera Clipping fixed -Zoom enabled via , and . keys -Randomize all light colors (just for show) with r key

--- NorfolkVizApp/src/net/electroland/norfolk/vizapp/NorfolkVizSketch.java
+++ NorfolkVizApp/src/net/electroland/norfolk/vizapp/NorfolkVizSketch.java
@@ -1,58 +1,90 @@
 package net.electroland.norfolk.vizapp;
+import java.awt.Color;
 import processing.core.*;
 import remixlab.proscene.*;
 import saito.objloader.*;
-import shapes3d.*;
+import java.util.Random;
 
 
 public class NorfolkVizSketch extends PApplet {
 	Scene scene;
 
 	//define a new class for LightObjects
+	//
 	class LightObject {
-	  OBJModel lightModel, participantModel, sensorBeamModel;
-	  boolean triggerState;
-	  PImage lightTexture;
-	  int intensity, lightColor, prevLightColor, fadeOnTime, fadeOffTime;
-	  LightObject(boolean passedTriggerState, OBJModel passedLightModel, PImage passedLightTexture, OBJModel passedParticipantModel, OBJModel passedSensorBeamModel, int passedIntensity, int passedLightColor, int passedFadeOnTime, int passedFadeOffTime) {
-		triggerState = passedTriggerState;
-		lightModel = passedLightModel;
-		lightTexture = passedLightTexture;
-	    participantModel = passedParticipantModel;
-	    sensorBeamModel = passedSensorBeamModel;
-	    intensity = passedIntensity;
-	    lightColor = passedLightColor;
-	    fadeOnTime = passedFadeOnTime;
-	    fadeOffTime = passedFadeOffTime;
-	  }
+		OBJModel lightModel, participantModel, sensorBeamModel;
+		boolean triggerState;
+		int lightColor;
+		PImage lightTexture, lightMask;
+		double lightIntensity;
+		LightObject(OBJModel passedLightModel, OBJModel passedParticipantModel, OBJModel passedSensorBeamModel) {
+			triggerState = false;
+			lightModel = passedLightModel;
+			participantModel = passedParticipantModel;
+			sensorBeamModel = passedSensorBeamModel;
+			lightTexture = createImage(128, 128, RGB);
+			lightMask = loadImage("../depends/models/lightGrad.jpg");
+			lightIntensity = 0.5;
+			lightColor = color(255,255,0);
+			lightTexture.loadPixels();
+			for (int i = 0; i < lightTexture.pixels.length; i++) {
+				lightTexture.pixels[i] = lightColor; 
+			}
+			lightTexture.mask(lightMask);
+			lightTexture.updatePixels();
+			lightModel.setTexture(lightTexture);
+			lightModel.enableTexture();
+		}
+	  
+	  //setLightColor makes a new PImage texture for each light
+	  //Intensity will usually be 1.0, but exists in case we want that functionality later 
+		public void setLightColor(String nameOfLight, Color lightColor) {
+			int r = Math.min(255, (int) (lightColor.getRed() * lightIntensity));
+			int g = Math.min(255, (int) (lightColor.getGreen() * lightIntensity));
+			int b = Math.min(255, (int) (lightColor.getBlue() * lightIntensity));
+			lightTexture.loadPixels();
+			for (int i = 0; i < lightTexture.pixels.length; i++) {
+				lightTexture.pixels[i] = color(r,g,b); 
+			}
+			lightTexture.mask(lightMask);
+			lightTexture.updatePixels();		  
+		}
+	  
+		public void setSensorState(String sensorName, Boolean isOn) {
+			triggerState = isOn;
+			System.err.println("Trigger " + sensorName + "is tripped high: " + triggerState);
+
+		}
+
 	}
 
-	//declare display flags
+	//declare and set some display flags
 	boolean showParticipants = true;
 	boolean showSensorBeams = true;
 	boolean showEnvironment = true;
+	boolean bTexture = true;
+	boolean bStroke = false;
+	int zoomLevel = 1200;
 
 	//declare all OBJModels
-	OBJModel sculpture, environment, modelpB01, modelpB02, modelpB03, modelpF01, modelpF02, modelpF03, modelpF05, modelpF06, modelpF08, modelpF09, modelpF10, modelpF11, modelpF12, modelsB01, modelsB02, modelsB03, modelsF01, modelsF02, modelsF03, modelsF05, modelsF06, modelsF08, modelsF09, modelsF10, modelsF11, modelsF12, modelsT01;
+	OBJModel sculpture, environment, volumeB01, volumeB02, volumeB03, volumeC01A, volumeC01B, volumeC02A, volumeC02B, volumeC03A, volumeC03B, volumeF01, volumeF02, volumeF03, volumeF05, volumeF06, volumeF08, volumeF09, volumeF10, volumeF11, volumeF12, volumeL01, volumeL02, volumeV01, volumeV02, volumeV03, volumeV04, modelpB01, modelpB02, modelpB03, modelpF01, modelpF02, modelpF03, modelpF05, modelpF06, modelpF08, modelpF09, modelpF10, modelpF11, modelpF12, modelsB01, modelsB02, modelsB03, modelsF01, modelsF02, modelsF03, modelsF05, modelsF06, modelsF08, modelsF09, modelsF10, modelsF11, modelsF12, modelsT01;
 
-	//declare all lightVolumes
-	OBJModel volumeB01, volumeB02, volumeB03, volumeC01A, volumeC01B, volumeC02A, volumeC02B, volumeC03A, volumeC03B, volumeF01, volumeF02, volumeF03, volumeF05, volumeF06, volumeF08, volumeF09, volumeF10, volumeF11, volumeF12, volumeL01, volumeL02, volumeV01, volumeV02, volumeV03, volumeV04;
-	
-	//declare all texture PImages
-	PImage texB01, texB02, texB03, texC01A, texC01B, texC02A, texC02B, texC03A, texC03B, texF01, texF02, texF03, texF05, texF06, texF08, texF09, texF10, texF11, texF12, texL01, texL02, texV01, texV02, texV03, texV04;
 	
 	//Declare an array to hold all the LightObjects
+	//This will be a hashmap soon
 	LightObject[] allLightObjects = new LightObject[25];
 
 	public void setup()
 	{
-	  
-	  //Scene instantiation
+	  //Scene/proscene instantiation
 	  size(800, 600, P3D);
 	  scene = new Scene(this);
 	  scene.setGridIsDrawn(false);
 	  scene.setAxisIsDrawn(false);
-	  scene.setRadius(600);
+	  scene.setRadius(2000);
+	  scene.camera().setPosition(new PVector(0, 50, zoomLevel));
+	  scene.disableKeyboardHandling();
+	  
 	  
 	  //Register a CAD Camera profile and name it "CAD_CAM"
 	  scene.registerCameraProfile(new CameraProfile(scene, "CAD_CAM", CameraProfile.Mode.CAD));
@@ -122,60 +154,32 @@ public class NorfolkVizSketch extends PApplet {
 	  modelsF11 = new OBJModel(this, "../depends/models/sF11.obj", "absolute", TRIANGLES);
 	  modelsF12 = new OBJModel(this, "../depends/models/sF12.obj", "absolute", TRIANGLES);
 	  
-	  //Define textures
-	  texB01 = loadImage("../depends/models/lightGrad.jpg");
-	  texB02 = loadImage("../depends/models/lightGrad.jpg");
-	  texB03 = loadImage("../depends/models/lightGrad.jpg");
-	  texC01A = loadImage("../depends/models/lightGrad.jpg");
-	  texC01B = loadImage("../depends/models/lightGrad.jpg");
-	  texC02A = loadImage("../depends/models/lightGrad.jpg");
-	  texC02B = loadImage("../depends/models/lightGrad.jpg");
-	  texC03A = loadImage("../depends/models/lightGrad.jpg");
-	  texC03B = loadImage("../depends/models/lightGrad.jpg");
-	  texF01 = loadImage("../depends/models/lightGrad.jpg");
-	  texF02 = loadImage("../depends/models/lightGrad.jpg");
-	  texF03 = loadImage("../depends/models/lightGrad.jpg");
-	  texF05 = loadImage("../depends/models/lightGrad.jpg");
-	  texF06 = loadImage("../depends/models/lightGrad.jpg");
-	  texF08 = loadImage("../depends/models/lightGrad.jpg");
-	  texF09 = loadImage("../depends/models/lightGrad.jpg");
-	  texF10 = loadImage("../depends/models/lightGrad.jpg");
-	  texF11 = loadImage("../depends/models/lightGrad.jpg");
-	  texF12 = loadImage("../depends/models/lightGrad.jpg");
-	  texL01 = loadImage("../depends/models/lightGrad.jpg");
-	  texL02 = loadImage("../depends/models/lightGrad.jpg");
-	  texV01 = loadImage("../depends/models/lightGrad.jpg");
-	  texV02 = loadImage("../depends/models/lightGrad.jpg");
-	  texV03 = loadImage("../depends/models/lightGrad.jpg");
-	  texV04 = loadImage("../depends/models/lightGrad.jpg");
-	  
-	  
 	  //initialize LightObjects
-	  LightObject B01 = new LightObject(false, volumeB01, texB01, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject B02 = new LightObject(false, volumeB02, texB02, modelpB02, modelsB02, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject B03 = new LightObject(false, volumeB03, texB03, modelpB03, modelsB03, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject C01A = new LightObject(false, volumeC01A, texC01A, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject C01B = new LightObject(false, volumeC01B, texC01B, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject C02A = new LightObject(false, volumeC02A, texC02A, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject C02B = new LightObject(false, volumeC02B, texC02B, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject C03A = new LightObject(false, volumeC03A, texC03A, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject C03B = new LightObject(false, volumeC03B, texC03B, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F01 = new LightObject(false, volumeF01, texF01, modelpF01, modelsF01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F02 = new LightObject(false, volumeF02, texF02, modelpF02, modelsF02, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F03 = new LightObject(false, volumeF03, texF03, modelpF03, modelsF03, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F05 = new LightObject(false, volumeF05, texF05, modelpF05, modelsF05, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F06 = new LightObject(false, volumeF06, texF06, modelpF06, modelsF06, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F08 = new LightObject(false, volumeF08, texF08, modelpF08, modelsF08, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F09 = new LightObject(false, volumeF09, texF09, modelpF09, modelsF09, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F10 = new LightObject(false, volumeF10, texF10, modelpF10, modelsF10, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F11 = new LightObject(false, volumeF11, texF11, modelpF11, modelsF11, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject F12 = new LightObject(false, volumeF12, texF12, modelpF12, modelsF12, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject L01 = new LightObject(false, volumeL01, texL01, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject L02 = new LightObject(false, volumeL02, texL02, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject V01 = new LightObject(false, volumeV01, texV01, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject V02 = new LightObject(false, volumeV02, texV02, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject V03 = new LightObject(false, volumeV03, texV03, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
-	  LightObject V04 = new LightObject(false, volumeV04, texV04, modelpB01, modelsB01, 255, color(255, 255, 255), 2000, 2000);
+	  LightObject B01 = new LightObject(volumeB01, modelpB01, modelsB01);
+	  LightObject B02 = new LightObject(volumeB02, modelpB02, modelsB02);
+	  LightObject B03 = new LightObject(volumeB03, modelpB03, modelsB03);
+	  LightObject C01A = new LightObject(volumeC01A, modelpB01, modelsB01);
+	  LightObject C01B = new LightObject(volumeC01B, modelpB01, modelsB01);
+	  LightObject C02A = new LightObject(volumeC02A, modelpB01, modelsB01);
+	  LightObject C02B = new LightObject(volumeC02B, modelpB01, modelsB01);
+	  LightObject C03A = new LightObject(volumeC03A, modelpB01, modelsB01);
+	  LightObject C03B = new LightObject(volumeC03B, modelpB01, modelsB01);
+	  LightObject F01 = new LightObject(volumeF01, modelpF01, modelsF01);
+	  LightObject F02 = new LightObject(volumeF02, modelpF02, modelsF02);
+	  LightObject F03 = new LightObject(volumeF03, modelpF03, modelsF03);
+	  LightObject F05 = new LightObject(volumeF05, modelpF05, modelsF05);
+	  LightObject F06 = new LightObject(volumeF06, modelpF06, modelsF06);
+	  LightObject F08 = new LightObject(volumeF08, modelpF08, modelsF08);
+	  LightObject F09 = new LightObject(volumeF09, modelpF09, modelsF09);
+	  LightObject F10 = new LightObject(volumeF10, modelpF10, modelsF10);
+	  LightObject F11 = new LightObject(volumeF11, modelpF11, modelsF11);
+	  LightObject F12 = new LightObject(volumeF12, modelpF12, modelsF12);
+	  LightObject L01 = new LightObject(volumeL01, modelpB01, modelsB01);
+	  LightObject L02 = new LightObject(volumeL02, modelpB01, modelsB01);
+	  LightObject V01 = new LightObject(volumeV01, modelpB01, modelsB01);
+	  LightObject V02 = new LightObject(volumeV02, modelpB01, modelsB01);
+	  LightObject V03 = new LightObject(volumeV03, modelpB01, modelsB01);
+	  LightObject V04 = new LightObject(volumeV04, modelpB01, modelsB01);
 	  
 
 	  //build the array of all light objects, and initialize them
@@ -212,34 +216,138 @@ public class NorfolkVizSketch extends PApplet {
 	}
 
 
-
 	public void draw()
 	{
-	    background(129);
-	    lights();
-	    
-	    blendMode(NORMAL);
-	    sculpture.draw();
-	    
-	    if (showEnvironment == true) environment.draw();
-	    //iterate through allLightObjects and draw
-	    for (int i = 0; i < allLightObjects.length; i = i+1) {
-	    	blendMode(NORMAL);
-	    	if (showParticipants == true) allLightObjects[i].participantModel.draw();
-	    	if (showSensorBeams == true) allLightObjects[i].sensorBeamModel.draw();
-	    	//blendMode(SCREEN);
-	        if (allLightObjects[i].intensity > 0) {
-	        	allLightObjects[i].lightModel.setTexture(texV04);
-	        	allLightObjects[i].lightModel.enableMaterial();
-	        	allLightObjects[i].lightModel.enableTexture();
-	        	allLightObjects[i].lightModel.draw();
-	      }
-	      
-	    }
-	    
+		background(129);
+		lights();
+
+		blendMode(BLEND);
+		sculpture.draw();
+
+		if (showEnvironment == true) environment.draw();
+		//iterate through allLightObjects and draw
+		for (int i = 0; i < allLightObjects.length; i = i+1) {
+			if (showParticipants == true) {
+				if (allLightObjects[i].triggerState == true) allLightObjects[i].participantModel.draw();
+			}
+			if (showSensorBeams == true) {
+				if (allLightObjects[i].triggerState == true) allLightObjects[i].sensorBeamModel.draw();
+			}
+		}
+		blendMode(ADD);
+		for (int i = 0; i < allLightObjects.length; i = i+1) {
+			allLightObjects[i].lightModel.draw();
+		}
 
 	}
+	
+	public void keyPressed()
+	{
+		if(key == ',') {
+			zoomLevel += 100;
+			scene.camera().setPosition(new PVector(0, 50, zoomLevel));
+		}
+		
+		if(key == '.') {
+			zoomLevel -= 100;
+			scene.camera().setPosition(new PVector(0, 50, zoomLevel));
+		}
+		
+		if(key == 'r') {
+			Random rand = new Random();
+			for (int i = 0; i < allLightObjects.length; i = i+1) {
+				float rRand = rand.nextInt(255);
+				float gRand = rand.nextInt(255);
+				float bRand = rand.nextInt(255);
+				allLightObjects[i].lightTexture.loadPixels();
+				for (int ii = 0; ii < allLightObjects[i].lightTexture.pixels.length; ii++) {
+					allLightObjects[i].lightTexture.pixels[ii] = color(rRand,gRand,bRand); 
+				}
+				allLightObjects[i].lightTexture.mask(allLightObjects[i].lightMask);
+				allLightObjects[i].lightTexture.updatePixels();
+			}
+		}
+			
+		if(key == 'p') {
+			if(!showParticipants) {
+	            showParticipants = true;
+	        } 
+	        else {
+	        	showParticipants = false;
+	        }
 
-	boolean bTexture = true;
-	boolean bStroke = false;
+		}
+		
+		if(key == 'b') {
+			if(!showSensorBeams) {
+				showSensorBeams = true;
+	        } 
+	        else {
+	        	showSensorBeams = false;
+	        }
+
+		}
+		
+		if(key == 'e') {
+			if(!showEnvironment) {
+				showEnvironment = true;
+	        } 
+	        else {
+	        	showEnvironment = false;
+	        }
+
+		}
+			
+		if(key == '1') {
+			if(!allLightObjects[1].triggerState) {
+				allLightObjects[1].triggerState = true;
+	        } 
+	        else {
+	        	allLightObjects[1].triggerState = false;
+	        }
+
+		}
+		if(key == '2') {
+			if(!allLightObjects[2].triggerState) {
+				allLightObjects[2].triggerState = true;
+	        } 
+	        else {
+	        	allLightObjects[2].triggerState = false;
+	        }
+
+		}
+			
+		if(key == '3') {
+			if(!allLightObjects[9].triggerState) {
+				allLightObjects[9].triggerState = true;
+	        } 
+	        else {
+	        	allLightObjects[9].triggerState = false;
+	        }
+
+		}
+			
+		if(key == '4') {
+			if(!allLightObjects[10].triggerState) {
+				allLightObjects[10].triggerState = true;
+	        } 
+	        else {
+	        	allLightObjects[10].triggerState = false;
+	        }
+
+		}
+		
+		if(key == '5') {
+			if(!allLightObjects[11].triggerState) {
+				allLightObjects[11].triggerState = true;
+	        } 
+	        else {
+	        	allLightObjects[11].triggerState = false;
+	        }
+
+		}
+			
+
+			
+	}
 }
