348d2209c8e5731fa28614892db529aae6c264d4
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-26 04:49:07 +0000

    latest build to dist.

--- ElectrolandConfig/install/run.bat
+++ ElectrolandConfig/install/run.bat
@@ -1,1 +1,1 @@
-java -classpath EL-Config.jar; net.electroland.utils.process.RestartDaemon C:\\Users\\Electroland\\Desktop\\ElVis run.bat 1000
+java -classpath EL-Config.jar; net.electroland.utils.process.ProcessManager
