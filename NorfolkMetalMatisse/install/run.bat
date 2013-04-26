301448a18bc317415c6acfad81890de1895cefa3
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-26 17:19:46 +0000

    tested startup harness

--- NorfolkMetalMatisse/install/run.bat
+++ NorfolkMetalMatisse/install/run.bat
@@ -1,1 +1,1 @@
-java -classpath EL-Config.jar; net.electroland.utils.process.ProcessManager
+java -classpath ./depends;./libraries/log4j-1.2.15.jar;./libraries/EL-Config.jar net.electroland.utils.process.ProcessManager
