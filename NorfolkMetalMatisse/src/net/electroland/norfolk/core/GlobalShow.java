02212446bb0836e61c03fcf0e25a2686af9a261b
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-04-27 01:18:08 +0000

    globalshows every 60 minutes.

---
+++ NorfolkMetalMatisse/src/net/electroland/norfolk/core/GlobalShow.java
@@ -0,0 +1,47 @@
+package net.electroland.norfolk.core;
+
+import org.apache.log4j.Logger;
+
+public class GlobalShow implements Runnable {
+
+    private static Logger logger = Logger.getLogger(GlobalShow.class);
+    private ClipPlayer player;
+    private long nextPlayTime = 0;
+    private Thread thread;
+
+    public GlobalShow(ClipPlayer player){
+        this.player = player;
+    }
+
+    public void run(){
+        while (thread != null){
+            if (System.currentTimeMillis() > nextPlayTime){
+
+                /***  play whateve you want here!  Pick from a random list, ***/
+                /***  or whatever.                                          ***/
+                logger.info("playing 60 seconds show...");
+                player.play("sweepWhiteDown");
+
+                /**************************************************************/
+                nextPlayTime = System.currentTimeMillis() + 60 * 60 * 1000; // an hour from now.
+            }
+            try {
+                Thread.sleep(1000); // poll every second instead of sleeping for 60,
+                                    // so we can more easily turn this thread off.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop(){
+        thread = null;
+    }
+
+    public void start(){
+        if (thread == null){
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+}
