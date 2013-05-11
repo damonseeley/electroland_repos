d591def4aa1f56576bdaa429be749504d980c153
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-05-11 21:11:33 +0000

    load singlets and triplets.

--- NorfolkMetalMatisse/src/net/electroland/norfolk/core/CueManager.java
+++ NorfolkMetalMatisse/src/net/electroland/norfolk/core/CueManager.java
@@ -1,13 +1,21 @@
 package net.electroland.norfolk.core;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 
 import net.electroland.utils.ElectrolandProperties;
 
 public class CueManager {
 
     public Collection<Cue> load(ElectrolandProperties props){
-        return Collections.<Cue>emptyList();
+        ArrayList<Cue> cues = new ArrayList<Cue>();
+
+        // singlets
+        cues.add(new SingletCue(props.getParams("cues", "singlet")));
+
+        // triplets
+        cues.add(new TripletCue(props.getParams("cues", "triplet")));
+
+        return cues;
     }
 }
