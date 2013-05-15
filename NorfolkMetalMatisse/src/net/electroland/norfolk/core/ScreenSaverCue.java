0020065a54629962a39f67362132c37d45ba0f1e
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-05-15 06:02:27 +0000

    screensaver working

--- NorfolkMetalMatisse/src/net/electroland/norfolk/core/ScreenSaverCue.java
+++ NorfolkMetalMatisse/src/net/electroland/norfolk/core/ScreenSaverCue.java
@@ -1,23 +1,49 @@
 package net.electroland.norfolk.core;
 
+import java.util.List;
+import java.util.Random;
+
 import net.electroland.utils.ParameterMap;
 
 public class ScreenSaverCue extends Cue {
 
+    boolean isSaving = false, firsttime = true;
+    int timeout, fadeout;
+    private List<String>shows;
+
     public ScreenSaverCue(ParameterMap p) {
         super(p);
-        // TODO Auto-generated constructor stub
+        timeout = p.getRequiredInt("timeout");
+        fadeout = p.getRequiredInt("fadeout");
+        shows   = p.getRequiredList("cues");
     }
 
     @Override
     public void fire(EventMetaData meta, ClipPlayer cp) {
-        // TODO Auto-generated method stub
-
+        if (isSaving){
+            if (firsttime){
+                firsttime = false;
+                cp.play(shows.get(new Random().nextInt(shows.size())));
+            }
+            cp.enterScreensaverMode(fadeout);
+        }else{
+            cp.exitScreensaverMode(fadeout);
+        }
     }
 
     @Override
     public boolean ready(EventMetaData meta) {
-        // TODO Auto-generated method stub
-        return false;
+
+        boolean everythingInactive = System.currentTimeMillis() - meta.getTimeOfLastNonScreenSaverCue() > timeout;
+
+        if (everythingInactive && !isSaving){
+            isSaving = true;
+            return true;
+        }else if (!everythingInactive && isSaving){
+            isSaving = false;
+            return true;
+        }else{
+            return false;
+        }
     }
 }
