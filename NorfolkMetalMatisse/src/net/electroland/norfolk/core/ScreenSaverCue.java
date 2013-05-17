3ce13a77f519cfa711a0bbeb2e81c6cf9d3340fd
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-05-17 22:28:36 +0000

    removed $cues

--- NorfolkMetalMatisse/src/net/electroland/norfolk/core/ScreenSaverCue.java
+++ NorfolkMetalMatisse/src/net/electroland/norfolk/core/ScreenSaverCue.java
@@ -1,31 +1,22 @@
 package net.electroland.norfolk.core;
 
-import java.util.List;
-import java.util.Random;
-
 import net.electroland.utils.ParameterMap;
 
 public class ScreenSaverCue extends Cue {
 
-    private boolean isSaving = false, firsttime = true;
+    private boolean isSaving = false;
     private int timeout, fadeout;
-    private List<String>shows;
     private Cue[] exceptions;
 
     public ScreenSaverCue(ParameterMap p) {
         super(p);
         timeout = p.getRequiredInt("timeout");
         fadeout = p.getRequiredInt("fadeout");
-        shows   = p.getRequiredList("cues");
     }
 
     @Override
     public void fire(EventMetaData meta, ClipPlayer cp) {
         if (isSaving){
-            if (firsttime){
-                firsttime = false;
-                cp.play(shows.get(new Random().nextInt(shows.size())));
-            }
             cp.enterScreensaverMode(fadeout);
         }else{
             cp.exitScreensaverMode(fadeout);
@@ -34,7 +25,7 @@ public class ScreenSaverCue extends Cue {
 
     @Override
     public boolean ready(EventMetaData meta) {
-        // TODO: excluding screenSaver, bigShow, train
+
         boolean everythingInactive = meta.getTimeSinceLastCueExcluding(exceptions) > timeout;
 
         if (everythingInactive && !isSaving){
