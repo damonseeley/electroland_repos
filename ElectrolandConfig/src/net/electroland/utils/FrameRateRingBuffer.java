d8115041a16b960b83f01fa8be1eda74ffc05168
bradley <bradley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-06-04 04:04:01 +0000

    more efficient frame rate calculation.

--- ElectrolandConfig/src/net/electroland/utils/FrameRateRingBuffer.java
+++ ElectrolandConfig/src/net/electroland/utils/FrameRateRingBuffer.java
@@ -1,76 +1,84 @@
 package net.electroland.utils;
 
+/**
+ * Use this to measure FPS.
+ * 
+ * Pick a capacity. Then call markFrame() everytime you render a frame. Call
+ * getFPS() to access the average framerate at any time.
+ * 
+ * @author bradley
+ */
+// TODO: What about make this a subclass of Thread, that takes care of all
+//       sleep calculations. Then markFrame() will be superfluous.
 public class FrameRateRingBuffer {
 
-	long[] times;
-	int pointer;
-	long last;
-	boolean firstPass = true;
+    int[]   samples;
+    int     sampleIndex;
+    long    lastSampleTime;
+    int     total;
+    boolean firstPass = true;
 
-	//  test
-	public static void main(String args[])
-	{
-		FrameRateRingBuffer r = new FrameRateRingBuffer(10);
-		for (int i = 0; i < 100; i++){
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			r.markFrame();
-			System.out.println(r);
-			System.out.println(r.getFPS());
-		}
-	}
-	
-	public FrameRateRingBuffer(int expectedFrameRate, int seconds)
-	{
-		times = new long[expectedFrameRate * seconds];
-		last = System.currentTimeMillis();		
-	}
-	
-	
-	public FrameRateRingBuffer(int capacity){
-		if (capacity < 1){
-			throw new RuntimeException("capacity must be greater than 0.");
-		}
-		times = new long[capacity];
-		last = System.currentTimeMillis();
-	}
-	
-	public void markFrame()
-	{
-		synchronized(times){
-			long curr = System.currentTimeMillis();
-			times[pointer++] = curr - last;
-			last = curr;
-			if (pointer == times.length){
-				pointer = 0;
-				firstPass = false;
-			}
-		}
-	}
-	
-	public double getFPS()
-	{
-		synchronized(times){
-			long total = 0;
-			for (long t : times){
-				total += t;
-			}
-			return 1000 / 
-					(total / 
-						(firstPass ? (double)pointer : (double)times.length));
-		}
-	}
+    //  test
+    public static void main(String args[])
+    {
+        // will take 3.3 seconds
+        FrameRateRingBuffer r = new FrameRateRingBuffer(10);
+        for (int i = 0; i < 100; i++){
+            try {
+                Thread.sleep(33);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            r.markFrame();
+        }
+        System.out.println(r);
+    }
 
-	public String toString(){
-		StringBuffer sb = new StringBuffer("StringBuffer[fps=");
-		sb.append(this.getFPS()).append(", times=[");
-		for (long t : times){
-			sb.append(t).append(',');
-		}
-		sb.append("]]");
-		return sb.toString();
-	}
+    public FrameRateRingBuffer(int expectedFrameRate, int seconds)
+    {
+        samples = new int[expectedFrameRate * seconds];
+        lastSampleTime = System.currentTimeMillis();
+    }
+
+    public FrameRateRingBuffer(int capacity){
+        if (capacity < 1){
+            throw new RuntimeException("capacity must be greater than 0.");
+        }
+        samples = new int[capacity];
+        lastSampleTime = System.currentTimeMillis();
+    }
+
+    public void markFrame()
+    {
+        synchronized(samples){
+            long curr = System.currentTimeMillis();
+            total -= samples[sampleIndex];
+            samples[sampleIndex] = (int)(curr - lastSampleTime);
+            total += samples[sampleIndex++];
+            lastSampleTime = curr;
+            if (sampleIndex == samples.length){
+                sampleIndex = 0;
+                firstPass = false;
+            }
+        }
+    }
+
+    public int getFPS()
+    {
+        int denominator = firstPass ? sampleIndex : samples.length;
+        if (denominator == 0){
+            return -1;
+        }
+        return 1000 / (total / denominator);
+    }
+
+    public String toString(){
+        StringBuffer sb = new StringBuffer("StringBuffer[fps=");
+        sb.append(this.getFPS()).append(", times=[");
+        for (long t : samples){
+            sb.append(t).append(',');
+        }
+        sb.append("]]");
+        return sb.toString();
+    }
 }
