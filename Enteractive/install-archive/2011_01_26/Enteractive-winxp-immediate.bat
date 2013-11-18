2ccfe31af689a77ebb6a989f24bf20c726fa7f5b
dseeley <dseeley@38cd337d-7a19-44a1-8cca-49443be78b0d>
2013-11-18 20:04:03 +0000

    initial import of 2011_01_26 install archive

---
+++ Enteractive/install-archive/2011_01_26/Enteractive-winxp-immediate.bat
@@ -0,0 +1,2 @@
+# start project on WinXP
+java -classpath ./depends;Enteractive.jar;libraries/ELU.jar;libraries/core.jar;libraries/javaosc_ELmod.jar;libraries/junit-4.6.jar;libraries/log4j-1.2.15.jar;libraries/miglayout15-swing.jar;libraries/SCSoundControl.jar net.electroland.enteractive.core.EnteractiveMain
